39aa8af974debefb032129067e514e82c40f649e
wuyong <245593358@qq.com>
2018-07-18 11:54:03 +0800

    kernel class

---
+++ FWFramework/Framework/Kernel/FWLog.h
@@ -0,0 +1,174 @@
+/*!
+ @header     FWLog.h
+ @indexgroup FWFramework
+ @brief      日志记录
+ @author     wuyong
+ @copyright  Copyright © 2018年 wuyong.site. All rights reserved.
+ @updated    2018-05-11
+ */
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - Macro
+
+/*!
+ @brief 记录日志内部宏
+ 
+ @parseOnly
+ @param type 日志方法名
+ @param format 日志格式，同NSLog
+ */
+#define FWLogType_( type, format, ... ) \
+    [FWLog type:(@"(%@ #%d %s) " format), [@(__FILE__) lastPathComponent], __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__];
+
+/*!
+ @brief 记录详细日志
+ 
+ @param format 日志格式，同NSLog
+ */
+#define FWLogVerbose( format, ... ) \
+    FWLogType_( verbose, format, ##__VA_ARGS__ );
+
+/*!
+ @brief 记录调试日志
+ 
+ @param format 日志格式，同NSLog
+ */
+#define FWLogDebug( format, ... ) \
+    FWLogType_( debug, format, ##__VA_ARGS__ );
+
+/*!
+ @brief 记录信息日志
+ 
+ @param format 日志格式，同NSLog
+ */
+#define FWLogInfo( format, ... ) \
+    FWLogType_( info, format, ##__VA_ARGS__ );
+
+/*!
+ @brief 记录警告日志
+ 
+ @param format 日志格式，同NSLog
+ */
+#define FWLogWarn( format, ... ) \
+    FWLogType_( warn, format, ##__VA_ARGS__ );
+
+/*!
+ @brief 记录错误日志
+ 
+ @param format 日志格式，同NSLog
+ */
+#define FWLogError( format, ... ) \
+    FWLogType_( error, format, ##__VA_ARGS__ );
+
+#pragma mark - FWLog
+
+/*!
+ @brief 日志类型定义
+ 
+ @const FWLogTypeError 错误类型，0...00001
+ @const FWLogTypeWarn 警告类型，0...00010
+ @const FWLogTypeInfo 信息类型，0...00100
+ @const FWLogTypeDebug 调试类型，0...01000
+ @const FWLogTypeVerbose 详细类型，0...10000
+ */
+typedef NS_OPTIONS(NSUInteger, FWLogType) {
+    FWLogTypeError   = 1 << 0,
+    FWLogTypeWarn    = 1 << 1,
+    FWLogTypeInfo    = 1 << 2,
+    FWLogTypeDebug   = 1 << 3,
+    FWLogTypeVerbose = 1 << 4,
+};
+
+/*!
+ @brief 日志级别定义
+ 
+ @const FWLogLevelOff 关闭日志，0...00000
+ @const FWLogLevelError 错误以上级别，0...00001
+ @const FWLogLevelWarn 警告以上级别，0...00011
+ @const FWLogLevelInfo 信息以上级别，0...00111
+ @const FWLogLevelDebug 调试以上级别，0...01111
+ @const FWLogLevelVerbose 详细以上级别，0...11111
+ @const FWLogLevelAll 所有级别，1...11111
+ */
+typedef NS_ENUM(NSUInteger, FWLogLevel) {
+    FWLogLevelOff     = 0,
+    FWLogLevelError   = FWLogTypeError,
+    FWLogLevelWarn    = FWLogLevelError | FWLogTypeWarn,
+    FWLogLevelInfo    = FWLogLevelWarn  | FWLogTypeInfo,
+    FWLogLevelDebug   = FWLogLevelInfo  | FWLogTypeDebug,
+    FWLogLevelVerbose = FWLogLevelDebug | FWLogTypeVerbose,
+    FWLogLevelAll     = NSUIntegerMax,
+};
+
+NS_ASSUME_NONNULL_BEGIN
+
+/*!
+ @brief 日志记录类。支持设置全局日志级别和自定义FWLogPlugin插件
+ */
+@interface FWLog : NSObject
+
+/*!
+ @brief 设置全局日志级别
+
+ @param level 日志级别
+ */
++ (void)setLevel:(FWLogLevel)level;
+
+/*!
+ @brief 详细日志
+
+ @param format 日志格式，同NSLog
+ */
++ (void)verbose:(NSString *)format, ...;
+
+/*!
+ @brief 调试日志
+ 
+ @param format 日志格式，同NSLog
+ */
++ (void)debug:(NSString *)format, ...;
+
+/*!
+ @brief 信息日志
+ 
+ @param format 日志格式，同NSLog
+ */
++ (void)info:(NSString *)format, ...;
+
+/*!
+ @brief 警告日志
+ 
+ @param format 日志格式，同NSLog
+ */
++ (void)warn:(NSString *)format, ...;
+
+/*!
+ @brief 错误日志
+ 
+ @param format 日志格式，同NSLog
+ */
++ (void)error:(NSString *)format, ...;
+
+@end
+
+#pragma mark - FWLogPlugin
+
+/*!
+ @brief 日志插件协议
+ */
+@protocol FWLogPlugin <NSObject>
+
+@required
+
+/*!
+ @brief 记录日志协议方法
+
+ @param type 日志类型
+ @param message 日志消息
+ */
+- (void)fwLog:(FWLogType)type withMessage:(NSString *)message;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FWFramework/Framework/Kernel/FWMessage.h
@@ -0,0 +1,377 @@
+/*!
+ @header     FWMessage.h
+ @indexgroup FWFramework
+ @brief      点对点消息、广播通知管理器
+ @author     wuyong
+ @copyright  Copyright © 2018年 wuyong.site. All rights reserved.
+ @updated    2018-05-16
+ */
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - Macro
+
+/*!
+ @brief 定义类点对点消息
+ 
+ @param name 消息名称
+ */
+#define FWMessage( name ) \
+    @property (nonatomic, readonly) NSString * name; \
+    - (NSString *)name; \
+    + (NSString *)name;
+
+/*!
+ @brief 定义类点对点消息实现
+ 
+ @param name 消息名称
+ */
+#define FWDefMessage( name ) \
+    @dynamic name; \
+    - (NSString *)name { return [[self class] name]; } \
+    + (NSString *)name { return [NSString stringWithFormat:@"%@.%@.%s", @"message", NSStringFromClass([self class]), #name]; }
+
+/*!
+ @brief 定义类广播通知
+ 
+ @param name 通知名称
+ */
+#define FWNotification( name ) \
+    @property (nonatomic, readonly) NSString * name; \
+    - (NSString *)name; \
+    + (NSString *)name;
+
+/*!
+ @brief 定义类广播通知实现
+ 
+ @param name 通知名称
+ */
+#define FWDefNotification( name ) \
+    @dynamic name; \
+    - (NSString *)name { return [[self class] name]; } \
+    + (NSString *)name { return [NSString stringWithFormat:@"%@.%@.%s", @"notification", NSStringFromClass([self class]), #name]; }
+
+#pragma mark - NSObject+FWMessage
+
+/*!
+ @brief 点对点消息分类
+ */
+@interface NSObject (FWMessage)
+
+#pragma mark - Observer
+
+/*!
+ @brief 监听某个点对点消息，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name  消息名称
+ @param block 消息句柄
+ */
+- (void)fwObserveMessage:(NSString *)name block:(void (^)(NSNotification *notification))block;
+
+/*!
+ @brief 监听某个指定对象点对点消息，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name   消息名称
+ @param object 消息对象，值为nil时表示所有
+ @param block  消息句柄
+ */
+- (void)fwObserveMessage:(NSString *)name object:(id)object block:(void (^)(NSNotification *notification))block;
+
+/*!
+ @brief 监听某个点对点消息，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name   消息名称
+ @param target 消息目标
+ @param action 目标动作，参数为通知对象
+ */
+- (void)fwObserveMessage:(NSString *)name target:(id)target action:(SEL)action;
+
+/*!
+ @brief 监听某个指定对象点对点消息，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name   消息名称
+ @param object 消息对象，值为nil时表示所有
+ @param target 消息目标
+ @param action 目标动作，参数为通知对象
+ */
+- (void)fwObserveMessage:(NSString *)name object:(id)object target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个点对点消息指定监听
+ 
+ @param name   消息名称
+ @param target 消息目标
+ @param action 目标动作
+ */
+- (void)fwUnobserveMessage:(NSString *)name target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个指定对象点对点消息指定监听
+ 
+ @param name   消息名称
+ @param object 消息对象，值为nil时表示所有
+ @param target 消息目标
+ @param action 目标动作
+ */
+- (void)fwUnobserveMessage:(NSString *)name object:(id)object target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个点对点消息所有监听
+ 
+ @param name 消息名称
+ */
+- (void)fwUnobserveMessage:(NSString *)name;
+
+/*!
+ @brief 手工移除某个指定对象点对点消息所有监听
+ 
+ @param name   消息名称
+ @param object 消息对象，值为nil时表示所有
+ */
+- (void)fwUnobserveMessage:(NSString *)name object:(id)object;
+
+/*!
+ @brief 手工移除所有点对点消息监听
+ */
+- (void)fwUnobserveAllMessages;
+
+#pragma mark - Subject
+
+/*!
+ @brief 发送类点对点消息
+ 
+ @param name 消息名称
+ @param receiver 消息接收者
+ */
++ (void)fwSendMessage:(NSString *)name toReceiver:(id)receiver;
+
+/*!
+ @brief 发送类点对点消息，附带对象
+ 
+ @param name   消息名称
+ @param object 消息对象
+ @param receiver 消息接收者
+ */
++ (void)fwSendMessage:(NSString *)name object:(id)object toReceiver:(id)receiver;
+
+/*!
+ @brief 发送类点对点消息，附带对象和用户信息
+ 
+ @param name     消息名称
+ @param object   消息对象
+ @param userInfo 用户信息
+ @param receiver 消息接收者
+ */
++ (void)fwSendMessage:(NSString *)name object:(id)object userInfo:(NSDictionary *)userInfo toReceiver:(id)receiver;
+
+/*!
+ @brief 发送点对点消息
+ 
+ @param name 消息名称
+ @param receiver 消息接收者
+ */
+- (void)fwSendMessage:(NSString *)name toReceiver:(id)receiver;
+
+/*!
+ @brief 发送点对点消息，附带对象
+ 
+ @param name   消息名称
+ @param object 消息对象
+ @param receiver 消息接收者
+ */
+- (void)fwSendMessage:(NSString *)name object:(id)object toReceiver:(id)receiver;
+
+/*!
+ @brief 发送点对点消息，附带对象和用户信息
+ 
+ @param name     消息名称
+ @param object   消息对象
+ @param userInfo 用户信息
+ @param receiver 消息接收者
+ */
+- (void)fwSendMessage:(NSString *)name object:(id)object userInfo:(NSDictionary *)userInfo toReceiver:(id)receiver;
+
+@end
+
+#pragma mark - NSObject+FWNotification
+
+/*!
+ @brief 广播通知分类
+ */
+@interface NSObject (FWNotification)
+
+#pragma mark - Observer
+
+/*!
+ @brief 监听某个广播通知，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name  通知名称
+ @param block 通知句柄
+ */
+- (void)fwObserveNotification:(NSString *)name block:(void (^)(NSNotification *notification))block;
+
+/*!
+ @brief 监听某个指定对象广播通知，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name   通知名称
+ @param object 通知对象，值为nil时表示所有
+ @param block  通知句柄
+ */
+- (void)fwObserveNotification:(NSString *)name object:(id)object block:(void (^)(NSNotification *notification))block;
+
+/*!
+ @brief 监听某个广播通知，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name   通知名称
+ @param target 通知目标
+ @param action 目标动作，参数为通知对象
+ */
+- (void)fwObserveNotification:(NSString *)name target:(id)target action:(SEL)action;
+
+/*!
+ @brief 监听某个指定对象广播通知，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param name   通知名称
+ @param object 通知对象，值为nil时表示所有
+ @param target 通知目标
+ @param action 目标动作，参数为通知对象
+ */
+- (void)fwObserveNotification:(NSString *)name object:(id)object target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个广播通知指定监听
+ 
+ @param name   通知名称
+ @param target 通知目标
+ @param action 目标动作
+ */
+- (void)fwUnobserveNotification:(NSString *)name target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个指定对象广播通知指定监听
+ 
+ @param name   通知名称
+ @param object 通知对象，值为nil时表示所有
+ @param target 通知目标
+ @param action 目标动作
+ */
+- (void)fwUnobserveNotification:(NSString *)name object:(id)object target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个广播通知所有监听
+ 
+ @param name 通知名称
+ */
+- (void)fwUnobserveNotification:(NSString *)name;
+
+/*!
+ @brief 手工移除某个指定对象广播通知所有监听
+ 
+ @param name   通知名称
+ @param object 通知对象，值为nil时表示所有
+ */
+- (void)fwUnobserveNotification:(NSString *)name object:(id)object;
+
+/*!
+ @brief 手工移除所有广播通知监听
+ */
+- (void)fwUnobserveAllNotifications;
+
+#pragma mark - Subject
+
+/*!
+ @brief 发送广播通知
+ 
+ @param name 通知名称
+ */
++ (void)fwPostNotification:(NSString *)name;
+
+/*!
+ @brief 发送广播通知，附带对象
+ 
+ @param name   通知名称
+ @param object 通知对象
+ */
++ (void)fwPostNotification:(NSString *)name object:(id)object;
+
+/*!
+ @brief 发送广播通知，附带对象和用户信息
+ 
+ @param name     通知名称
+ @param object   通知对象
+ @param userInfo 用户信息
+ */
++ (void)fwPostNotification:(NSString *)name object:(id)object userInfo:(NSDictionary *)userInfo;
+
+/*!
+ @brief 发送广播通知
+ 
+ @param name 通知名称
+ */
+- (void)fwPostNotification:(NSString *)name;
+
+/*!
+ @brief 发送广播通知，附带对象
+ 
+ @param name   通知名称
+ @param object 通知对象
+ */
+- (void)fwPostNotification:(NSString *)name object:(id)object;
+
+/*!
+ @brief 发送广播通知，附带对象和用户信息
+ 
+ @param name     通知名称
+ @param object   通知对象
+ @param userInfo 用户信息
+ */
+- (void)fwPostNotification:(NSString *)name object:(id)object userInfo:(NSDictionary *)userInfo;
+
+@end
+
+#pragma mark - NSObject+FWKvo
+
+/*!
+ @brief KVO属性监听分类
+ */
+@interface NSObject (FWKvo)
+
+/*!
+ @brief 监听对象某个属性，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param property 属性名称
+ @param block    目标句柄，block参数依次为object、优化的change字典(不含NSNull)
+ */
+- (void)fwObserveProperty:(NSString *)property block:(void (^)(id object, NSDictionary *change))block;
+
+/*!
+ @brief 监听对象某个属性，对象释放时自动移除监听，添加多次执行多次
+ 
+ @param property 属性名称
+ @param target   目标对象
+ @param action   目标动作，action参数依次为object、优化的change字典(不含NSNull)
+ */
+- (void)fwObserveProperty:(NSString *)property target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个属性指定监听
+ 
+ @param property 属性名称
+ @param target   目标对象，值为nil时移除所有对象(同UIControl)
+ @param action   目标动作，值为nil时移除所有动作(同UIControl)
+ */
+- (void)fwUnobserveProperty:(NSString *)property target:(id)target action:(SEL)action;
+
+/*!
+ @brief 手工移除某个属性所有监听
+ 
+ @param property 属性名称
+ */
+- (void)fwUnobserveProperty:(NSString *)property;
+
+/*!
+ @brief 手工移除所有属性所有监听
+ */
+- (void)fwUnobserveAllProperties;
+
+@end
