80e9a9b7adebee4669e93a5f330933f582823f68
wuyong <245593358@qq.com>
2018-09-29 17:42:58 +0800

    nsobject block

--- FWFramework/FWFramework/Extend/Foundation/NSObject+FWBlock.m
+++ FWFramework/FWFramework/Extend/Foundation/NSObject+FWBlock.m
@@ -11,4 +11,71 @@
 
 @implementation NSObject (FWBlock)
 
++ (id)fwPerformBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay
+{
+    return [NSObject fwPerformBlock:block onQueue:dispatch_get_main_queue() afterDelay:delay];
+}
+
++ (id)fwPerformBlockInBackground:(void (^)(void))block afterDelay:(NSTimeInterval)delay
+{
+    return [NSObject fwPerformBlock:block onQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0) afterDelay:delay];
+}
+
++ (id)fwPerformBlock:(void (^)(void))block onQueue:(dispatch_queue_t)queue afterDelay:(NSTimeInterval)delay
+{
+    NSParameterAssert(block != nil);
+    
+    __block BOOL cancelled = NO;
+
+    void (^wrapper)(BOOL) = ^(BOOL cancel) {
+        if (cancel) {
+            cancelled = YES;
+            return;
+        }
+        if (!cancelled) block();
+    };
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (uint64_t)(NSEC_PER_SEC * delay)), queue, ^{ wrapper(NO); });
+    
+    return [wrapper copy];
+}
+
++ (void)fwCancelBlock:(id)block
+{
+    NSParameterAssert(block != nil);
+    void (^wrapper)(BOOL) = block;
+    wrapper(YES);
+}
+
+- (id)fwPerformBlock:(void (^)(id obj))block afterDelay:(NSTimeInterval)delay
+{
+    return [self fwPerformBlock:block onQueue:dispatch_get_main_queue() afterDelay:delay];
+}
+
+- (id)fwPerformBlockInBackground:(void (^)(id obj))block afterDelay:(NSTimeInterval)delay
+{
+    return [self fwPerformBlock:block onQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0) afterDelay:delay];
+}
+
+- (id)fwPerformBlock:(void (^)(id obj))block onQueue:(dispatch_queue_t)queue afterDelay:(NSTimeInterval)delay
+{
+    NSParameterAssert(block != nil);
+    
+    __block BOOL cancelled = NO;
+    
+    void (^wrapper)(BOOL) = ^(BOOL cancel) {
+        if (cancel) {
+            cancelled = YES;
+            return;
+        }
+        if (!cancelled) block(self);
+    };
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (uint64_t)(NSEC_PER_SEC * delay)), queue, ^{
+        wrapper(NO);
+    });
+    
+    return [wrapper copy];
+}
+
 @end
