e4f2ac6101c897d892b1309072f5c4f608a782eb
wuyong <245593358@qq.com>
2018-10-11 15:05:04 +0800

    优化push pop

--- FWFramework/Framework/Category/UIKit/UIViewController+FWBar.h
+++ FWFramework/Framework/Category/UIKit/UIViewController+FWBar.h
@@ -68,10 +68,10 @@
 
 #pragma mark - Action
 
-// 打开页面。如果导航栏存在，则调用push；否则调用present
+// 打开页面。1.如果打开导航栏，则调用present；2.否则如果导航栏存在，则调用push；3.否则调用present
 - (void)fwOnOpen:(UIViewController *)viewController;
 
-// 关闭页面。如果导航栏存在，则调用pop；否则调用dismiss
+// 关闭页面。1.如果导航栏不存在，则调用dismiss；2.否则如果已是导航栏底部，则调用dismiss；3.否则调用pop
 - (void)fwOnClose;
 
 @end
