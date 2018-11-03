caa1b1713ca36f1e7348b30e636651f866224b19
wuyong <245593358@qq.com>
2018-11-03 23:27:55 +0800

    uipagecontrol

---
+++ FWFramework/Framework/Category/UIKit/UIPageControl+FWFramework.h
@@ -0,0 +1,20 @@
+/*!
+ @header     UIPageControl+FWFramework.h
+ @indexgroup FWFramework
+ @brief      UIPageControl+FWFramework
+ @author     wuyong
+ @copyright  Copyright © 2018 wuyong.site. All rights reserved.
+ @updated    2018/11/3
+ */
+
+#import <UIKit/UIKit.h>
+
+/*!
+ @brief UIPageControl分类
+ */
+@interface UIPageControl (FWFramework)
+
+// 自定义圆点大小
+@property (nonatomic, assign) CGSize fwIndicatorSize;
+
+@end
