a00fa358b6b29650a4a90a036b8bcacc1b0ee72d
wuyong <245593358@qq.com>
2018-12-13 14:29:45 +0800

    banner

---
+++ Example/Example/Classes/Controller/Test/FWFramework/TestBannerViewController.m
@@ -0,0 +1,69 @@
+//
+//  TestBannerViewController.m
+//  Example
+//
+//  Created by wuyong on 2018/12/13.
+//  Copyright © 2018 wuyong.site. All rights reserved.
+//
+
+#import "TestBannerViewController.h"
+#import "SDCycleScrollView.h"
+#import "M80AttributedLabel.h"
+
+@interface TestBannerViewController () <SDCycleScrollViewDelegate>
+
+@end
+
+@implementation TestBannerViewController
+
+- (void)renderView
+{
+    SDCycleScrollView *cycleView = [SDCycleScrollView new];
+    cycleView.autoScroll = YES;
+    cycleView.autoScrollTimeInterval = 6;
+    cycleView.placeholderImage = [UIImage imageNamed:@"public_picture"];
+    cycleView.bannerImageViewContentMode = UIViewContentModeScaleAspectFill;
+    cycleView.pageControlDotSize = CGSizeMake(6, 6);
+    cycleView.pageDotColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+    cycleView.currentPageDotColor = [UIColor whiteColor];
+    [self.view addSubview:cycleView];
+    [cycleView fwPinEdgeToSuperview:NSLayoutAttributeTop];
+    [cycleView fwPinEdgeToSuperview:NSLayoutAttributeLeft];
+    [cycleView fwSetDimension:NSLayoutAttributeWidth toSize:FWScreenWidth];
+    [cycleView fwSetDimension:NSLayoutAttributeHeight toSize:135];
+    
+    NSMutableArray *imageUrls = [NSMutableArray array];
+    [imageUrls addObject:@"http://e.hiphotos.baidu.com/image/h%3D300/sign=0e95c82fa90f4bfb93d09854334e788f/10dfa9ec8a136327ee4765839c8fa0ec09fac7dc.jpg"];
+    [imageUrls addObject:@"not_exist.jpg"];
+    [imageUrls addObject:@"http://e.hiphotos.baidu.com/image/h%3D300/sign=0e95c82fa90f4bfb93d09854334e788f/10dfa9ec8a136327ee4765839c8fa0ec09fac7dc.jpg"];
+    cycleView.imageURLStringsGroup = [imageUrls copy];
+    
+    M80AttributedLabel *label = [M80AttributedLabel new];
+    label.font = [UIFont systemFontOfSize:15];
+    label.textColor = [UIColor fwColorWithHex:0x111111];
+    label.textAlignment = kCTTextAlignmentCenter;
+    [self.view addSubview:label];
+    [label fwPinEdgeToSuperview:NSLayoutAttributeLeft];
+    [label fwPinEdgeToSuperview:NSLayoutAttributeRight];
+    [label fwPinEdge:NSLayoutAttributeTop toEdge:NSLayoutAttributeBottom ofView:cycleView withOffset:10];
+    [label fwSetDimension:NSLayoutAttributeHeight toSize:30];
+    
+    [label setText:@"文本 "];
+    UIView *labelView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    labelView.backgroundColor = [UIColor redColor];
+    [labelView fwSetCornerRadius:15];
+    [label appendView:labelView margin:UIEdgeInsetsZero alignment:M80ImageAlignmentCenter];
+    [label appendText:@" "];
+    UIImage *image = [UIImage fwImageWithColor:[UIColor blueColor] size:CGSizeMake(30, 30)];
+    [label appendImage:image maxSize:image.size margin:UIEdgeInsetsZero alignment:M80ImageAlignmentCenter];
+    [label appendText:@" 结束"];
+}
+
+#pragma mark - SDCycleScrollViewDelegate
+
+- (void)cycleScrollView:(SDCycleScrollView *)cycleScrollView didSelectItemAtIndex:(NSInteger)index
+{
+    FWLogDebug(@"index: %@", index);
+}
+
+@end
