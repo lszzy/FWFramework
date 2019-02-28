976258ea0e40d47786f96998b1115dc184955c1d
wuyong <245593358@qq.com>
2019-02-28 16:02:54 +0800

    fix

--- FWFramework/Framework/Category/UIKit/UINavigationController+FWBar.h
+++ FWFramework/Framework/Category/UIKit/UINavigationController+FWBar.h
@@ -10,14 +10,14 @@
 #import <UIKit/UIKit.h>
 
 /*!
- @brief 优化导航栏转场动画突兀的问题，默认关闭。全局启用后各个ViewController管理自己的导航栏样式
+ @brief 优化导航栏转场动画闪烁的问题，默认关闭。全局启用后各个ViewController管理自己的导航栏样式，在viewDidLoad或viewViewAppear中设置即可
  @discussion 方案1：自己实现UINavigationController管理器；方案2：将原有导航栏设置透明，每个控制器添加一个NavigationBar充当导航栏；方案3：转场开始隐藏原有导航栏并添加假的NavigationBar，转场结束后还原。此处采用方案3。更多介绍：https://tech.meituan.com/2018/10/25/navigation-transition-solution-and-best-practice-in-meituan.html
  
  @see https://github.com/MoZhouqi/KMNavigationBarTransition
  */
 @interface UINavigationController (FWBar)
 
-// 全局启用转场NavigationBar。启用后各个ViewController管理自己的导航栏样式
+// 全局启用转场NavigationBar。启用后各个ViewController管理自己的导航栏样式，在viewDidLoad或viewViewAppear中设置即可
 + (void)fwEnableTransitionNavigationBar;
 
 // 自定义转场过程中containerView的背景色，默认白色
@@ -30,7 +30,7 @@
  */
 @interface UIViewController (FWBarTransition)
 
-// 如果iOS11+有滚动视图时转场动画不正常，指定此视图即可
+// 如果iOS11+有滚动视图时转场动画不正常，可指定此视图；也可设置滚动视图的contentInsetAdjustmentBehavior为Never
 @property (nonatomic, weak) UIScrollView *fwTransitionScrollView;
 
 @end
