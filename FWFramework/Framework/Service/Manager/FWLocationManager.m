723f9d7d94d4769150eaed3d99279af2fb7bbc98
wuyong <245593358@qq.com>
2019-03-19 12:08:36 +0800

    location

--- FWFramework/Framework/Service/Manager/FWLocationManager.m
+++ FWFramework/Framework/Service/Manager/FWLocationManager.m
@@ -10,6 +10,7 @@
 
 NSString *const FWLocationUpdatedNotification = @"FWLocationUpdatedNotification";
 NSString *const FWLocationFailedNotification = @"FWLocationFailedNotification";
+NSString *const FWHeadingUpdatedNotification = @"FWHeadingUpdatedNotification";
 
 @interface FWLocationManager () <CLLocationManagerDelegate>
 
@@ -32,24 +33,58 @@ NSString *const FWLocationFailedNotification = @"FWLocationFailedNotification";
     self = [super init];
     if (self) {
         _locationManager = [[CLLocationManager alloc] init];
-        _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         _locationManager.delegate = self;
-        if ([_locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
-            [_locationManager requestWhenInUseAuthorization];
-        }
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        // _locationManager.distanceFilter = 50;
     }
     return self;
+}
+
+#pragma mark - Accessor
+
+- (void)setHeadingEnabled:(BOOL)headingEnabled
+{
+    // 不支持方向时，设置无效
+    if (headingEnabled && ![CLLocationManager headingAvailable]) {
+        headingEnabled = NO;
+    }
+    _headingEnabled = headingEnabled;
 }
 
 #pragma mark - Public
 
 - (void)startNotifier
 {
+    if (self.alwaysLocation) {
+        if ([self.locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
+            [self.locationManager requestAlwaysAuthorization];
+        }
+        if (@available(iOS 9.0, *)) {
+            [self.locationManager setAllowsBackgroundLocationUpdates:YES];
+        }
+    } else {
+        if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+            [self.locationManager requestWhenInUseAuthorization];
+        }
+    }
+    
     [self.locationManager startUpdatingLocation];
+    if (self.headingEnabled) {
+        [self.locationManager startUpdatingHeading];
+    }
 }
 
 - (void)stopNotifier
 {
+    if (self.alwaysLocation) {
+        if (@available(iOS 9.0, *)) {
+            [self.locationManager setAllowsBackgroundLocationUpdates:NO];
+        }
+    }
+    
+    if (self.headingEnabled) {
+        [self.locationManager stopUpdatingHeading];
+    }
     [self.locationManager stopUpdatingLocation];
 }
 
@@ -67,6 +102,18 @@ NSString *const FWLocationFailedNotification = @"FWLocationFailedNotification";
     if (oldLocation) [userInfo setObject:oldLocation forKey:NSKeyValueChangeOldKey];
     if (newLocation) [userInfo setObject:newLocation forKey:NSKeyValueChangeNewKey];
     [[NSNotificationCenter defaultCenter] postNotificationName:FWLocationUpdatedNotification object:self userInfo:userInfo.copy];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
+{
+    CLHeading *oldHeading = _heading;
+    _heading = newHeading;
+    
+    // 发送方向改变通知
+    NSMutableDictionary *userInfo = [[NSMutableDictionary alloc] init];
+    if (oldHeading) [userInfo setObject:oldHeading forKey:NSKeyValueChangeOldKey];
+    if (newHeading) [userInfo setObject:newHeading forKey:NSKeyValueChangeNewKey];
+    [[NSNotificationCenter defaultCenter] postNotificationName:FWHeadingUpdatedNotification object:self userInfo:userInfo.copy];
 }
 
 - (void)locationManager:(CLLocationManager *)manager
