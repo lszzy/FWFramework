359b9d315dee13c0fa1cbacb996d1c104d62da59
wuyong <lingshizhuangzi@gmail.com>
2019-05-07 00:24:01 +0800

    fix

--- Example/Example/Classes/Controller/Test/FWFramework/TestDrawerViewController.m
+++ Example/Example/Classes/Controller/Test/FWFramework/TestDrawerViewController.m
@@ -9,68 +9,7 @@
 #import "TestDrawerViewController.h"
 #import <objc/runtime.h>
 
-#define TotalHeight (FWScreenHeight - FWStatusBarHeight - FWNavigationBarHeight)
-#define TopHeight (TotalHeight / 4 * 3)
-#define BottomHeight (TotalHeight / 4)
-
-@interface UIPanGestureRecognizer (Test)
-
-@end
-
-@implementation UIPanGestureRecognizer (Test)
-
-- (CGFloat)fwDrawerViewStartPosition
-{
-    return [objc_getAssociatedObject(self, @selector(fwDrawerViewStartPosition)) doubleValue];
-}
-
-- (void)setFwDrawerViewStartPosition:(CGFloat)position
-{
-    objc_setAssociatedObject(self, @selector(fwDrawerViewStartPosition), @(position), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (void)fwDrawerView:(UIView *)view
-           direction:(UISwipeGestureRecognizerDirection)direction
-        fromPosition:(CGFloat)fromPosition
-          toPosition:(CGFloat)toPosition
-      kickbackHeight:(CGFloat)kickbackHeight
-{
-    UIScrollView *scrollView = [view isKindOfClass:[UIScrollView class]] ? (UIScrollView *)view : nil;
-    
-    if (self.state == UIGestureRecognizerStateBegan) {
-        self.fwDrawerViewStartPosition = view.frame.origin.y;
-    }
-    
-    CGFloat transition = [self translationInView:view.superview].y;
-    [self setTranslation:CGPointZero inView:view.superview];
-    
-    CGFloat target = view.frame.origin.y - scrollView.contentOffset.y + transition;
-    if (target < fromPosition) {
-        view.fwY = fromPosition;
-        scrollView.fwContentOffsetY = fromPosition - target;
-    } else {
-        view.fwY = target;
-        scrollView.fwContentOffsetY = 0;
-    }
-        
-    if (self.state == UIGestureRecognizerStateEnded) {
-        [UIView animateWithDuration:0.1 animations:^{
-            CGFloat baselineY;
-            if (self.fwDrawerViewStartPosition == fromPosition) {
-                baselineY = fromPosition + kickbackHeight;
-            } else {
-                baselineY = toPosition - kickbackHeight;
-            }
-            if (view.fwY < baselineY) {
-                view.fwY = fromPosition;
-            } else {
-                view.fwY = toPosition;
-            }
-        } completion:^(BOOL finished) {}];
-    }
-}
-
-@end
+#define ViewHeight (FWScreenHeight - FWStatusBarHeight - FWNavigationBarHeight)
 
 @interface TestDrawerViewController () <UIScrollViewDelegate>
 
@@ -85,14 +24,13 @@
 {
     self.view.backgroundColor = [UIColor brownColor];
     
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, TopHeight, self.view.fwWidth, TotalHeight)];
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, ViewHeight / 4 * 3, self.view.fwWidth, ViewHeight)];
     _scrollView = scrollView;
     [scrollView fwContentInsetNever];
     scrollView.delegate = self;
     scrollView.showsHorizontalScrollIndicator = NO;
     scrollView.showsVerticalScrollIndicator = NO;
     scrollView.backgroundColor = [UIColor whiteColor];
-    // 预留TopHeight空白，否则contentView拉不上来
     scrollView.contentSize = CGSizeMake(self.view.fwWidth, 1000);
     [self.view addSubview:scrollView];
     
@@ -120,7 +58,7 @@
 
 - (void)scrollViewPanAction:(UIPanGestureRecognizer *)gesture
 {
-    [gesture fwDrawerView:self.scrollView direction:UISwipeGestureRecognizerDirectionUp fromPosition:0 toPosition:TopHeight kickbackHeight:25];
+    [gesture fwDrawerView:self.scrollView fromPosition:0 toPosition:ViewHeight / 4 * 3 kickbackHeight:25];
 }
 
 @end
--- FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.h
+++ FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.h
@@ -17,4 +17,18 @@
 // 当前滑动方向，失败返回0
 - (UISwipeGestureRecognizerDirection)fwSwipeDirection;
 
+/*!
+ @brief 抽屉拖拽效果，在action中调用即可
+ 
+ @param view 抽屉视图，默认为self.view
+ @param fromPosition 相对于view父视图的起始originY位置
+ @param toPosition 相对于view父视图的目标originY位置
+ @param kickbackHeight 回弹高度，拖拽小于该高度执行回弹
+ @return 当前状态停留位置
+ */
+- (CGFloat)fwDrawerView:(UIView *)view
+           fromPosition:(CGFloat)fromPosition
+             toPosition:(CGFloat)toPosition
+         kickbackHeight:(CGFloat)kickbackHeight;
+
 @end
