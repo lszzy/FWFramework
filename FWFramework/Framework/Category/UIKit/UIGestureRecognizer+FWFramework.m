1dbc517234026dcdd38031692f77dfbfdf8ac682
wuyong <lingshizhuangzi@gmail.com>
2019-05-07 00:24:01 +0800

    fix

--- FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.m
+++ FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.m
@@ -8,6 +8,7 @@
  */
 
 #import "UIGestureRecognizer+FWFramework.h"
+#import <objc/runtime.h>
 
 @implementation UIPanGestureRecognizer (FWFramework)
 
@@ -23,6 +24,46 @@
         return UISwipeGestureRecognizerDirectionRight;
     }
     return 0;
+}
+
+- (CGFloat)fwDrawerView:(UIView *)view
+           fromPosition:(CGFloat)fromPosition
+             toPosition:(CGFloat)toPosition
+         kickbackHeight:(CGFloat)kickbackHeight
+{
+    // 默认self.view
+    if (!view) {
+        view = self.view;
+    }
+    
+    // 拖动开始时记录起始位置
+    if (self.state == UIGestureRecognizerStateBegan) {
+        objc_setAssociatedObject(self, _cmd, @(view.frame.origin.y), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    
+    // 记录并清空相对父视图的移动距离
+    CGPoint transition = [self translationInView:view.superview];
+    [self setTranslation:CGPointZero inView:view.superview];
+    
+    // 视图跟随拖动移动指定距离
+    CGFloat targetY = view.frame.origin.y + transition.y;
+    if (targetY < fromPosition) {
+        targetY = fromPosition;
+    }
+    view.frame = CGRectMake(view.frame.origin.x, targetY, view.frame.size.width, view.frame.size.height);
+    
+    // 拖动结束时停留指定位置
+    if (self.state == UIGestureRecognizerStateEnded) {
+        CGFloat beganY = [objc_getAssociatedObject(self, _cmd) doubleValue];
+        CGFloat baselineY = (beganY == fromPosition) ? (fromPosition + kickbackHeight) : (toPosition - kickbackHeight);
+        targetY = view.frame.origin.y < baselineY ? fromPosition : toPosition;
+        
+        // 执行动画移动到指定位置
+        [UIView animateWithDuration:0.2 animations:^{
+            view.frame = CGRectMake(view.frame.origin.x, targetY, view.frame.size.width, view.frame.size.height);
+        } completion:nil];
+    }
+    return targetY;
 }
 
 @end
