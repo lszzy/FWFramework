c02af0c5fe9f4db2b0e16f570e583f67ac41c7ff
wuyong <lingshizhuangzi@gmail.com>
2019-05-09 13:08:59 +0800

    fix

--- Example/Example/Classes/Controller/Test/FWFramework/TestDrawerViewController.m
+++ Example/Example/Classes/Controller/Test/FWFramework/TestDrawerViewController.m
@@ -10,10 +10,7 @@
 
 #define ViewHeight (FWScreenHeight - FWStatusBarHeight - FWNavigationBarHeight)
 
-@interface TestDrawerViewController () <UIScrollViewDelegate>
-
-@property (nonatomic, weak) UIScrollView *scrollView;
-@property (nonatomic, weak) UIView *contentView;
+@interface TestDrawerViewController ()
 
 @end
 
@@ -23,10 +20,14 @@
 {
     self.view.backgroundColor = [UIColor brownColor];
     
+    [self renderViewUp];
+    [self renderViewDown];
+}
+
+- (void)renderViewUp
+{
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, ViewHeight / 4 * 3, self.view.fwWidth, ViewHeight)];
-    _scrollView = scrollView;
     [scrollView fwContentInsetNever];
-    scrollView.delegate = self;
     scrollView.showsHorizontalScrollIndicator = NO;
     scrollView.showsVerticalScrollIndicator = NO;
     scrollView.backgroundColor = [UIColor whiteColor];
@@ -34,7 +35,6 @@
     [self.view addSubview:scrollView];
     
     UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.fwWidth, 2000)];
-    _contentView = contentView;
     UILabel *topLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.fwWidth, 50)];
     topLabel.textAlignment = NSTextAlignmentCenter;
     topLabel.text = @"I am top";
@@ -49,15 +49,59 @@
     [contentView addSubview:bottomLabel];
     [scrollView addSubview:contentView];
     
+    scrollView.bounces = NO;
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] init];
     FWWeakifySelf();
-    [scrollView fwDrawerPosition:0 bottomPosition:ViewHeight / 4 * 3 kickbackHeight:25 callback:^(CGFloat position) {
+    [panGesture fwDrawerView:scrollView topPosition:0 bottomPosition:ViewHeight / 4 * 3 kickbackHeight:25 callback:^(CGFloat position) {
         FWStrongifySelf();
+        [self.view bringSubviewToFront:scrollView];
         if (position == 0) {
             [self.navigationController.navigationBar fwSetBackgroundColor:[UIColor brownColor]];
         } else {
             [self.navigationController.navigationBar fwSetBackgroundColor:[UIColor fwColorWithHex:0xFFDA00]];
         }
     }];
+    [scrollView addGestureRecognizer:panGesture];
+}
+
+- (void)renderViewDown
+{
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, -ViewHeight / 4 * 3, self.view.fwWidth, ViewHeight)];
+    [scrollView fwContentInsetNever];
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.showsVerticalScrollIndicator = NO;
+    scrollView.backgroundColor = [UIColor redColor];
+    scrollView.contentSize = CGSizeMake(self.view.fwWidth, 2000);
+    [self.view addSubview:scrollView];
+    
+    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.fwWidth, 2000)];
+    UILabel *topLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.fwWidth, 50)];
+    topLabel.textAlignment = NSTextAlignmentCenter;
+    topLabel.text = @"I am top";
+    [contentView addSubview:topLabel];
+    UILabel *middleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 975, self.view.fwWidth, 50)];
+    middleLabel.textAlignment = NSTextAlignmentCenter;
+    middleLabel.text = @"I am middle";
+    [contentView addSubview:middleLabel];
+    UILabel *bottomLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 1950, self.view.fwWidth, 50)];
+    bottomLabel.textAlignment = NSTextAlignmentCenter;
+    bottomLabel.text = @"I am bottom";
+    [contentView addSubview:bottomLabel];
+    [scrollView addSubview:contentView];
+    
+    scrollView.bounces = NO;
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] init];
+    FWWeakifySelf();
+    [panGesture fwDrawerView:scrollView topPosition:-ViewHeight / 4 * 3 bottomPosition:0 kickbackHeight:25 callback:^(CGFloat position) {
+        FWStrongifySelf();
+        [self.view bringSubviewToFront:scrollView];
+        if (position == 0) {
+            [self.navigationController.navigationBar fwSetBackgroundColor:[UIColor brownColor]];
+        } else {
+            [self.navigationController.navigationBar fwSetBackgroundColor:[UIColor fwColorWithHex:0xFFDA00]];
+        }
+    }];
+    [scrollView addGestureRecognizer:panGesture];
 }
 
 @end
