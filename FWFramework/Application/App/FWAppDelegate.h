a4335acc8ebff92063615fc9678ab36d7788d0af
wuyong <lingshizhuangzi@gmail.com>
2019-05-14 15:57:05 +0800

    fix

---
+++ FWFramework/Application/App/FWAppDelegate.h
@@ -0,0 +1,19 @@
+/*!
+ @header     FWAppDelegate.h
+ @indexgroup FWFramework
+ @brief      FWAppDelegate
+ @author     wuyong
+ @copyright  Copyright © 2019 wuyong.site. All rights reserved.
+ @updated    2019/5/14
+ */
+
+#import <UIKit/UIKit.h>
+
+/*!
+ @brief AppDelegate基类
+ */
+@interface FWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
