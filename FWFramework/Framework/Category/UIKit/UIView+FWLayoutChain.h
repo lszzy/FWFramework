ecac2b3d4a8b1cf3798efea69a9a41f15181764f
wuyong <lingshizhuangzi@gmail.com>
2019-06-22 01:24:57 +0800

    fix

---
+++ FWFramework/Framework/Category/UIKit/UIView+FWLayoutChain.h
@@ -0,0 +1,17 @@
+/*!
+ @header     UIView+FWLayoutChain.h
+ @indexgroup FWFramework
+ @brief      UIView+FWLayoutChain
+ @author     wuyong
+ @copyright  Copyright © 2019 wuyong.site. All rights reserved.
+ @updated    2019/6/22
+ */
+
+#import <UIKit/UIKit.h>
+
+/*!
+ @brief UIView+FWLayoutChain
+ */
+@interface UIView (FWLayoutChain)
+
+@end
---
+++ FWFramework/Framework/Category/UIKit/UIView+FWLayoutChain.m
@@ -0,0 +1,14 @@
+/*!
+ @header     UIView+FWLayoutChain.m
+ @indexgroup FWFramework
+ @brief      UIView+FWLayoutChain
+ @author     wuyong
+ @copyright  Copyright © 2019 wuyong.site. All rights reserved.
+ @updated    2019/6/22
+ */
+
+#import "UIView+FWLayoutChain.h"
+
+@implementation UIView (FWLayoutChain)
+
+@end
