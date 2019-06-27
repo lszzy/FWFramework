89cd6f45f65265a3d6b9eb552f1da12ec56b64f0
wuyong <lingshizhuangzi@gmail.com>
2019-06-28 00:55:18 +0800

    fix

--- FWFramework/Framework/Category/Foundation/NSBundle+FWFramework.swift
+++ FWFramework/Framework/Category/Foundation/NSBundle+FWFramework.swift
@@ -8,8 +8,15 @@
 
 import Foundation
 
-// 解决Xcode中swift项目不能运行9.3及以下模拟器问题：sudo mkdir '/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 9.3.simruntime/Contents/Resources/RuntimeRoot/usr/lib/swift'
-public func FWLocalizedString(key: String, table: String? = nil) -> String
+/// 读取本地化字符串
+///
+/// 解决Xcode中swift项目不能运行9.3及以下模拟器问题：sudo mkdir '/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 9.3.simruntime/Contents/Resources/RuntimeRoot/usr/lib/swift'
+///
+/// - Parameters:
+///   - key: 本地化键名
+///   - table: 本地化表名，默认Localizable.strings
+/// - Returns: 本地化字符串
+public func FWLocalizedString(_ key: String, _ table: String? = nil) -> String
 {
     return Bundle.main.localizedString(forKey: key, value: nil, table: table)
 }
