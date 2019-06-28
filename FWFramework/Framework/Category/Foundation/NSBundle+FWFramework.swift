1a134a963f5d4c128c0c49a494fe0e3d4277cf5c
wuyong <lingshizhuangzi@gmail.com>
2019-06-28 10:58:53 +0800

    fix

--- FWFramework/Framework/Category/Foundation/NSBundle+FWFramework.swift
+++ FWFramework/Framework/Category/Foundation/NSBundle+FWFramework.swift
@@ -2,6 +2,9 @@
 //  NSBundle+FWFramework.swift
 //  FWFramework
 //
+//  解决Xcode中swift项目不能运行9.3及以下模拟器问题：
+//  sudo mkdir '/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 9.3.simruntime/Contents/Resources/RuntimeRoot/usr/lib/swift'
+//
 //  Created by wuyong on 2019/6/27.
 //  Copyright © 2019 wuyong.site. All rights reserved.
 //
@@ -9,8 +12,6 @@
 import Foundation
 
 /// 读取本地化字符串
-///
-/// 解决Xcode中swift项目不能运行9.3及以下模拟器问题：sudo mkdir '/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 9.3.simruntime/Contents/Resources/RuntimeRoot/usr/lib/swift'
 ///
 /// - Parameters:
 ///   - key: 本地化键名
