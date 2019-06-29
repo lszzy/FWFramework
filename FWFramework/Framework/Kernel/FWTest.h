744f4b528d75e1e5b01dee5e7d469103aa0953aa
wuyong <lingshizhuangzi@gmail.com>
2019-06-29 23:59:30 +0800

    fix

--- FWFramework/Framework/Kernel/FWTest.h
+++ FWFramework/Framework/Kernel/FWTest.h
@@ -20,17 +20,15 @@ NS_ASSUME_NONNULL_BEGIN
  @param ... 断言表达式
  */
 #define FWAssertTrue( ... ) \
-    [self assertTrue:__VA_ARGS__ expr:@(#__VA_ARGS__) file:@(__FILE__) line:__LINE__];
+    [self assertTrue:__VA_ARGS__ expression:@(#__VA_ARGS__) file:@(__FILE__) line:__LINE__];
 
 #pragma mark - FWTestCase
 
 /*!
  @brief 单元测试用例基类，所有单元测试用例必须继承
- @discussion 按模块单元测试命名格式：FWTestCase_module_name
+ @discussion 调试模式下自动执行，按模块单元测试命名格式：FWTestCase_module_name
  */
 @interface FWTestCase : NSObject
-
-+ (BOOL)autoLoad;
 
 /*!
  @brief 测试初始化，每次执行测试方法开始都会调用
@@ -43,42 +41,14 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)tearDown;
 
 /*!
- @brief 断言，详细版
+ @brief 断言方法，请勿直接调用，请调用FWAssertTrue
  
  @param value 布尔表达式
- @param expr 当前表达式，一般使用宏@(#__VA_ARGS__)或@(__FUNCTION__)
+ @param expression 当前表达式，一般使用宏@(#__VA_ARGS__)或@(__FUNCTION__)
  @param file 当前文件，一般使用宏@(__FILE__)
  @param line 当前行，一般使用宏__LINE__
  */
-- (void)assertTrue:(BOOL)value expr:(NSString *)expr file:(NSString *)file line:(NSInteger)line;
-
-@end
-
-#pragma mark - FWUnitTest
-
-/*!
- @brief 单元测试。单例为框架测试(自动在应用启动后后台运行)
- */
-@interface FWUnitTest : NSObject
-
-/*!
- @brief 单例模式
- 
- @return 单例对象
- */
-+ (instancetype)sharedInstance;
-
-/*!
- @brief 添加测试用例
- 
- @param testCase 测试用例
- */
-- (void)addTestCase:(Class)testCase;
-
-/*!
- @brief 运行单元测试
- */
-- (void)run;
+- (void)assertTrue:(BOOL)value expression:(NSString *)expression file:(NSString *)file line:(NSInteger)line;
 
 @end
 
