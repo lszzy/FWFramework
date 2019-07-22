867d9cd5f64b956b8630b2fb7ec93e01dd366e0e
wuyong <lingshizhuangzi@gmail.com>
2019-07-22 22:25:08 +0800

    fix

--- FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.h
+++ FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.h
@@ -33,7 +33,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (UISwipeGestureRecognizerDirection)fwSwipeDirection;
 
 /*!
- @brief 设置抽屉拖拽效果。如果view为滚动视图，自动设置delegate处理与滚动视图pan手势冲突的问题
+ @brief 设置抽屉拖拽效果。如果未自定义delegate，会自动设置delegate处理与滚动视图pan手势冲突的问题
  
  @param view 抽屉视图，默认为self.view
  @param direction 拖拽方向，如向上拖动视图时为Up
