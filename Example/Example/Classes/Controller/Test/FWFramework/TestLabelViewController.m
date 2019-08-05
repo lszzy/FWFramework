36c30970a257bdba5a10f79f3c4ac4e48a1b76fd
wuyong <lingshizhuangzi@gmail.com>
2019-08-05 23:44:23 +0800

    fix

--- Example/Example/Classes/Controller/Test/FWFramework/TestLabelViewController.m
+++ Example/Example/Classes/Controller/Test/FWFramework/TestLabelViewController.m
@@ -11,8 +11,11 @@
 @interface TestLabelViewController ()
 
 @property (nonatomic, weak) UILabel *label;
+@property (nonatomic, weak) UILabel *label2;
 @property (nonatomic, weak) FWAttributedLabel *attrLabel;
+@property (nonatomic, weak) FWAttributedLabel *attrLabel2;
 @property (nonatomic, weak) UITextView *textView;
+@property (nonatomic, weak) UITextView *textView2;
 
 @end
 
@@ -28,28 +31,55 @@
     [self.view addSubview:label];
     label.fwLayoutChain.leftWithInset(10).rightWithInset(10).topWithInset(10);
     
+    UILabel *label2 = [UILabel new];
+    _label2 = label2;
+    label2.backgroundColor = [UIColor lightGrayColor];
+    label2.numberOfLines = 0;
+    label2.font = [UIFont systemFontOfSize:16];
+    [self.view addSubview:label2];
+    label2.fwLayoutChain.leftToView(label).rightToView(label).topToBottomOfViewWithOffset(label, 10);
+    
     FWAttributedLabel *attrLabel = [FWAttributedLabel new];
     _attrLabel = attrLabel;
     attrLabel.backgroundColor = [UIColor lightGrayColor];
-    attrLabel.numberOfLines = 0;
     attrLabel.font = [UIFont systemFontOfSize:16];
-    attrLabel.lineSpacing = 8 - attrLabel.font.fwSpaceHeight * 2;
     [self.view addSubview:attrLabel];
-    attrLabel.fwLayoutChain.leftToView(label).rightToView(label).topToBottomOfViewWithOffset(label, 10);
+    attrLabel.fwLayoutChain.leftToView(label).rightToView(label).topToBottomOfViewWithOffset(label2, 10);
+    
+    FWAttributedLabel *attrLabel2 = [FWAttributedLabel new];
+    _attrLabel2 = attrLabel2;
+    attrLabel2.backgroundColor = [UIColor lightGrayColor];
+    attrLabel2.numberOfLines = 0;
+    attrLabel2.font = [UIFont systemFontOfSize:16];
+    attrLabel2.lineSpacing = 8 - attrLabel.font.fwSpaceHeight * 2;
+    [self.view addSubview:attrLabel2];
+    attrLabel2.fwLayoutChain.leftToView(label).rightToView(label).topToBottomOfViewWithOffset(attrLabel, 10);
     
     UITextView *textView = [UITextView new];
     _textView = textView;
+    textView.editable = NO;
     textView.backgroundColor = [UIColor lightGrayColor];
     textView.font = [UIFont systemFontOfSize:16];
     [self.view addSubview:textView];
-    textView.fwLayoutChain.leftToView(label).rightToView(label).topToBottomOfViewWithOffset(attrLabel, 10).height(150);
+    textView.fwLayoutChain.leftToView(label).rightToView(label).topToBottomOfViewWithOffset(attrLabel2, 10).height(120);
+    
+    UITextView *textView2 = [UITextView new];
+    _textView2 = textView2;
+    textView2.editable = NO;
+    textView2.backgroundColor = [UIColor lightGrayColor];
+    textView2.font = [UIFont systemFontOfSize:16];
+    [self.view addSubview:textView2];
+    textView2.fwLayoutChain.leftToView(label).rightToView(label).topToBottomOfViewWithOffset(textView, 10).height(120);
 }
 
 - (void)renderData
 {
     self.label.text = @"我是很长很长的文本我是很长很长的文本我是很长很长的文本\n我是很长很长的文本我是很长很长的文本我是很长很长的文本";
+    self.label2.text = @"我是很长很长的文本我是很长很长的文本我是很长很长的文本\n我是很长很长的文本我是很长很长的文本我是很长很长的文本";
     self.attrLabel.text = @"我是很长很长的文本我是很长很长的文本我是很长很长的文本\n我是很长很长的文本我是很长很长的文本我是很长很长的文本";
+    self.attrLabel2.text = @"我是很长很长的文本我是很长很长的文本我是很长很长的文本\n我是很长很长的文本我是很长很长的文本我是很长很长的文本";
     self.textView.text = @"我是很长很长的文本我是很长很长的文本我是很长很长的文本\n我是很长很长的文本我是很长很长的文本我是很长很长的文本";
+    self.textView2.text = @"我是很长很长的文本我是很长很长的文本我是很长很长的文本\n我是很长很长的文本我是很长很长的文本我是很长很长的文本";
 }
 
 @end
