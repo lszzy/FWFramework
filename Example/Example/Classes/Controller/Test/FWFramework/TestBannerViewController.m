d8c8822456e1f5442125968372493305a93dd193
wuyong <lingshizhuangzi@gmail.com>
2019-08-09 13:14:59 +0800

    fix

--- Example/Example/Classes/Controller/Test/FWFramework/TestBannerViewController.m
+++ Example/Example/Classes/Controller/Test/FWFramework/TestBannerViewController.m
@@ -71,9 +71,9 @@
     [cycleView fwSetDimension:NSLayoutAttributeWidth toSize:FWScreenWidth + 10];
     
     NSMutableArray *imageUrls = [NSMutableArray array];
-    [imageUrls addObject:@"http://e.hiphotos.baidu.com/image/h%3D300/sign=0e95c82fa90f4bfb93d09854334e788f/10dfa9ec8a136327ee4765839c8fa0ec09fac7dc.jpg"];
-    [imageUrls addObject:@"public_picture"];
-    [imageUrls addObject:@"not_found.jpg"];
+    [imageUrls addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
+    [imageUrls addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
+    [imageUrls addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
     [imageUrls addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
     cycleView.imageURLStringsGroup = [imageUrls copy];
     cycleView.titlesGroup = @[@"1", @"2", @"3", @"4"];
@@ -189,9 +189,9 @@
     [cycleView fwSetDimension:NSLayoutAttributeHeight toSize:100];
     
     NSMutableArray *imageUrls2 = [NSMutableArray array];
-    [imageUrls2 addObject:@"http://e.hiphotos.baidu.com/image/h%3D300/sign=0e95c82fa90f4bfb93d09854334e788f/10dfa9ec8a136327ee4765839c8fa0ec09fac7dc.jpg"];
-    [imageUrls2 addObject:@"public_picture"];
-    [imageUrls2 addObject:@"not_found.jpg"];
+    [imageUrls2 addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
+    [imageUrls2 addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
+    [imageUrls2 addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
     [imageUrls2 addObject:@"http://ww2.sinaimg.cn/bmiddle/642beb18gw1ep3629gfm0g206o050b2a.gif"];
     cycleView.imageURLStringsGroup = [imageUrls2 copy];
     cycleView.titlesGroup = @[@"1", @"2", @"3", @"4"];
