53a3ca753c826aa9821eaddfd7b12aafeb7338f8
wuyong <lingshizhuangzi@gmail.com>
2019-08-14 10:51:31 +0800

    fix

--- FWFramework/Framework/Category/Foundation/NSAttributedString+FWOption.m
+++ FWFramework/Framework/Category/Foundation/NSAttributedString+FWOption.m
@@ -17,6 +17,30 @@
 
 @implementation FWAttributedOption
 
++ (void)initialize
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self setDefaultAppearance];
+    });
+}
+
++ (void)setDefaultAppearance
+{
+    // 设置统一默认样式
+    FWAttributedOption *appearance = [FWAttributedOption appearance];
+}
+
++ (instancetype)appearance
+{
+    static FWAttributedOption *appearance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        appearance = [[self alloc] init];
+    });
+    return appearance;
+}
+
 - (NSDictionary<NSAttributedStringKey,id> *)toDictionary
 {
     NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
@@ -24,6 +48,8 @@
 }
 
 @end
+
+#pragma mark - NSAttributedString+FWOption
 
 @implementation NSAttributedString (FWOption)
 
