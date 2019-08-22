16e38f10bb923ed5518f78a727ed0dee4d219be7
wuyong <lingshizhuangzi@gmail.com>
2019-08-22 22:29:21 +0800

    fix

--- FWFramework/Framework/Category/Foundation/NSAttributedString+FWOption.m
+++ FWFramework/Framework/Category/Foundation/NSAttributedString+FWOption.m
@@ -11,29 +11,26 @@
 
 #pragma mark - FWAttributedOption
 
+static FWAttributedOption *appearance = nil;
+
 @interface FWAttributedOption ()
 
 @end
 
 @implementation FWAttributedOption
 
+#pragma mark - Lifecycle
+
 + (void)initialize
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self setDefaultAppearance];
+        [self appearance];
     });
-}
-
-+ (void)setDefaultAppearance
-{
-    // 设置统一默认样式
-    // WAttributedOption *appearance = [FWAttributedOption appearance];
 }
 
 + (instancetype)appearance
 {
-    static FWAttributedOption *appearance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         appearance = [[self alloc] init];
@@ -41,35 +38,96 @@
     return appearance;
 }
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        if (appearance) {
+            self.font = appearance.font;
+            self.paragraphStyle = appearance.paragraphStyle;
+            self.foregroundColor = appearance.foregroundColor;
+            self.backgroundColor = appearance.backgroundColor;
+            self.ligature = appearance.ligature;
+            self.kern = appearance.kern;
+            self.strikethroughStyle = appearance.strikethroughStyle;
+            self.strikethroughColor = appearance.strikethroughColor;
+            self.underlineStyle = appearance.underlineStyle;
+            self.underlineColor = appearance.underlineColor;
+            self.strokeWidth = appearance.strokeWidth;
+            self.strokeColor = appearance.strokeColor;
+            self.shadow = appearance.shadow;
+            self.textEffect = appearance.textEffect;
+            self.baselineOffset = appearance.baselineOffset;
+            self.obliqueness = appearance.obliqueness;
+            self.expansion = appearance.expansion;
+            self.writingDirection = appearance.writingDirection;
+            self.verticalGlyphForm = appearance.verticalGlyphForm;
+            self.link = appearance.link;
+            self.attachment = appearance.attachment;
+            self.lineSpacingMultiplier = appearance.lineSpacingMultiplier;
+            self.lineHeightMultiplier = appearance.lineHeightMultiplier;
+        }
+    }
+    return self;
+}
+
+#pragma mark - NSCopying
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    FWAttributedOption *option = [[[self class] allocWithZone:zone] init];
+    option.font = self.font;
+    option.paragraphStyle = self.paragraphStyle;
+    option.foregroundColor = self.foregroundColor;
+    option.backgroundColor = self.backgroundColor;
+    option.ligature = self.ligature;
+    option.kern = self.kern;
+    option.strikethroughStyle = self.strikethroughStyle;
+    option.strikethroughColor = self.strikethroughColor;
+    option.underlineStyle = self.underlineStyle;
+    option.underlineColor = self.underlineColor;
+    option.strokeWidth = self.strokeWidth;
+    option.strokeColor = self.strokeColor;
+    option.shadow = self.shadow;
+    option.textEffect = self.textEffect;
+    option.baselineOffset = self.baselineOffset;
+    option.obliqueness = self.obliqueness;
+    option.expansion = self.expansion;
+    option.writingDirection = self.writingDirection;
+    option.verticalGlyphForm = self.verticalGlyphForm;
+    option.link = self.link;
+    option.attachment = self.attachment;
+    option.lineSpacingMultiplier = self.lineSpacingMultiplier;
+    option.lineHeightMultiplier = self.lineHeightMultiplier;
+    return option;
+}
+
+#pragma mark - Dictionary
+
 - (NSDictionary<NSAttributedStringKey,id> *)toDictionary
 {
     NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-    
-    /*
-     * API: Character Attributes , NSAttributedString 共有21个属性*
-     * 1. NSFontAttributeName ->设置字体属性，默认值：字体：Helvetica(Neue) 字号：12
-     * 2. NSParagraphStyleAttributeName ->设置文本段落排版格式，取值为 NSParagraphStyle 对象(详情见下面的API说明)
-     * 3. NSForegroundColorAttributeName ->设置字体颜色，取值为 UIColor对象，默认值为黑色
-     * 4. NSBackgroundColorAttributeName ->设置字体所在区域背景颜色，取值为 UIColor对象，默认值为nil, 透明色
-     * 5. NSLigatureAttributeName ->设置连体属性，取值为NSNumber 对象(整数)，0 表示没有连体字符，1 表示使用默认的连体字符
-     * 6. NSKernAttributeName ->设置字符间距，取值为 NSNumber 对象（整数），正值间距加宽，负值间距变窄
-     * 7. NSStrikethroughStyleAttributeName ->设置删除线，取值为 NSNumber 对象（整数）
-     * 8. NSStrikethroughColorAttributeName ->设置删除线颜色，取值为 UIColor 对象，默认值为黑色
-     * 9. NSUnderlineStyleAttributeName ->设置下划线，取值为 NSNumber 对象（整数），枚举常量 NSUnderlineStyle中的值，与删除线类似
-     * 10. NSUnderlineColorAttributeName ->设置下划线颜色，取值为 UIColor 对象，默认值为黑色
-     * 11. NSStrokeWidthAttributeName ->设置笔画宽度(粗细)，取值为 NSNumber 对象（整数），负值填充效果，正值中空效果
-     * 12. NSStrokeColorAttributeName ->填充部分颜色，不是字体颜色，取值为 UIColor 对象
-     * 13. NSShadowAttributeName ->设置阴影属性，取值为 NSShadow 对象
-     * 14. NSTextEffectAttributeName ->设置文本特殊效果，取值为 NSString 对象，目前只有图版印刷效果可用
-     * 15. NSBaselineOffsetAttributeName ->设置基线偏移值，取值为 NSNumber （float）,正值上偏，负值下偏
-     * 16. NSObliquenessAttributeName ->设置字形倾斜度，取值为 NSNumber （float）,正值右倾，负值左倾
-     * 17. NSExpansionAttributeName ->设置文本横向拉伸属性，取值为 NSNumber （float）,正值横向拉伸文本，负值横向压缩文本
-     * 18. NSWritingDirectionAttributeName ->设置文字书写方向，从左向右书写或者从右向左书写
-     * 19. NSVerticalGlyphFormAttributeName ->设置文字排版方向，取值为 NSNumber 对象(整数)，0 表示横排文本，1 表示竖排文本
-     * 20. NSLinkAttributeName ->设置链接属性，点击后调用浏览器打开指定URL地址
-     * 21. NSAttachmentAttributeName ->设置文本附件,取值为NSTextAttachment对象,常用于文字图片混排
-     */
-    
+    if (self.font) dictionary[NSFontAttributeName] = self.font;
+    if (self.paragraphStyle) dictionary[NSParagraphStyleAttributeName] = self.paragraphStyle;
+    if (self.foregroundColor) dictionary[NSForegroundColorAttributeName] = self.foregroundColor;
+    if (self.backgroundColor) dictionary[NSBackgroundColorAttributeName] = self.backgroundColor;
+    if (self.ligature) dictionary[NSLigatureAttributeName] = @(self.ligature);
+    if (self.kern) dictionary[NSKernAttributeName] = @(self.kern);
+    if (self.strikethroughStyle) dictionary[NSStrikethroughStyleAttributeName] = @(self.strikethroughStyle);
+    if (self.strikethroughColor) dictionary[NSStrikethroughColorAttributeName] = self.strikethroughColor;
+    if (self.underlineStyle) dictionary[NSUnderlineStyleAttributeName] = @(self.underlineStyle);
+    if (self.underlineColor) dictionary[NSUnderlineColorAttributeName] = self.underlineColor;
+    if (self.strokeWidth) dictionary[NSStrokeWidthAttributeName] = @(self.strokeWidth);
+    if (self.strokeColor) dictionary[NSStrokeColorAttributeName] = self.strokeColor;
+    if (self.shadow) dictionary[NSShadowAttributeName] = self.shadow;
+    if (self.textEffect) dictionary[NSTextEffectAttributeName] = self.textEffect;
+    if (self.baselineOffset) dictionary[NSBaselineOffsetAttributeName] = @(self.baselineOffset);
+    if (self.obliqueness) dictionary[NSObliquenessAttributeName] = @(self.obliqueness);
+    if (self.expansion) dictionary[NSExpansionAttributeName] = @(self.expansion);
+    if (self.writingDirection) dictionary[NSWritingDirectionAttributeName] = @(self.writingDirection);
+    if (self.verticalGlyphForm) dictionary[NSVerticalGlyphFormAttributeName] = @(self.verticalGlyphForm);
+    if (self.link) dictionary[NSLinkAttributeName] = self.link;
+    if (self.attachment) dictionary[NSAttachmentAttributeName] = self.attachment;
     return dictionary;
 }
 
