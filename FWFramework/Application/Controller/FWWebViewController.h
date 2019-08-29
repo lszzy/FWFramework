6dcbea7c718ab48364e0d7c5f1ff9008cf0b8e67
wuyong <lingshizhuangzi@gmail.com>
2019-08-29 11:25:22 +0800

    fix

---
+++ FWFramework/Application/Controller/FWWebViewController.h
@@ -0,0 +1,17 @@
+/*!
+ @header     FWWebViewController.h
+ @indexgroup FWFramework
+ @brief      FWWebViewController
+ @author     wuyong
+ @copyright  Copyright © 2019 wuyong.site. All rights reserved.
+ @updated    2019/8/28
+ */
+
+#import <UIKit/UIKit.h>
+
+/*!
+ @brief FWWebViewController
+ */
+@interface FWWebViewController : UIViewController
+
+@end
---
+++ FWFramework/Application/Controller/FWWebViewController.m
@@ -0,0 +1,14 @@
+/*!
+ @header     FWWebViewController.m
+ @indexgroup FWFramework
+ @brief      FWWebViewController
+ @author     wuyong
+ @copyright  Copyright © 2019 wuyong.site. All rights reserved.
+ @updated    2019/8/28
+ */
+
+#import "FWWebViewController.h"
+
+@implementation FWWebViewController
+
+@end
