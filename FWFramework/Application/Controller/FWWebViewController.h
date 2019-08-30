2c043ea6edda82515b38b71932c7781bf37d3d21
wuyong <lingshizhuangzi@gmail.com>
2019-08-30 17:17:34 +0800

    fix

--- FWFramework/Application/Controller/FWWebViewController.h
+++ FWFramework/Application/Controller/FWWebViewController.h
@@ -7,11 +7,34 @@
  @updated    2019/8/28
  */
 
-#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+#import "FWViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 /*!
- @brief FWWebViewController
+ @brief 网页视图控制器协议，可覆写
  */
-@interface FWWebViewController : UIViewController
+@protocol FWWebViewController <FWViewController, WKNavigationDelegate>
+
+@optional
+
+// 网页视图，默认不显示滚动条
+@property (nonatomic, readonly) WKWebView *webView;
+
+// 渲染网页视图配置，默认系统配置
+- (WKWebViewConfiguration *)renderWebConfiguration;
+
+// 渲染网页视图和布局等，默认铺满
+- (void)renderWebView;
 
 @end
+
+/*!
+ @brief 管理器网页视图控制器分类
+ */
+@interface FWViewControllerManager (FWWebViewController)
+
+@end
+
+NS_ASSUME_NONNULL_END
