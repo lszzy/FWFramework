3d4c2957c59cc8111fe9339f8488656064d9d1ad
wuyong <lingshizhuangzi@gmail.com>
2019-09-04 16:51:05 +0800

    fix

--- FWFramework/Framework/Kernel/FWRouter.h
+++ FWFramework/Framework/Kernel/FWRouter.h
@@ -43,27 +43,27 @@ typedef BOOL (^FWRouterFilterHandler)(NSDictionary *parameters);
 /**
  *  注册 pattern 对应的 Handler，在 handler 中可以初始化 VC，然后对 VC 做各种操作
  *
- *  @param pattern    带上 scheme，如 app://beauty/:id
+ *  @param pattern    字符串或字符串数组(批量)，带上 scheme，如 app://beauty/:id
  *  @param handler    该 block 会传一个字典，包含了注册的 URL 中对应的变量。
  *                    假如注册的 URL 为 app://beauty/:id 那么，就会传一个 @{@"id": 4} 这样的字典过来
  */
-+ (void)registerURL:(NSString *)pattern withHandler:(FWRouterHandler)handler;
++ (void)registerURL:(id)pattern withHandler:(FWRouterHandler)handler;
 
 /**
  *  注册 pattern 对应的 ObjectHandler，需要返回一个 object 给调用方
  *
- *  @param pattern    带上 scheme，如 app://beauty/:id
+ *  @param pattern    字符串或字符串数组(批量)，带上 scheme，如 app://beauty/:id
  *  @param handler    该 block 会传一个字典，包含了注册的 URL 中对应的变量。
  *                    假如注册的 URL 为 app://beauty/:id 那么，就会传一个 @{@"id": 4} 这样的字典过来
  */
-+ (void)registerURL:(NSString *)pattern withObjectHandler:(FWRouterObjectHandler)handler;
++ (void)registerURL:(id)pattern withObjectHandler:(FWRouterObjectHandler)handler;
 
 /**
  *  取消注册某个 pattern
  *
- *  @param pattern pattern
+ *  @param pattern 字符串或字符串数组(批量)
  */
-+ (void)unregisterURL:(NSString *)pattern;
++ (void)unregisterURL:(id)pattern;
 
 /**
  *  取消注册所有 pattern
