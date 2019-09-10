38a29ea87121934e5b69752fd78353fe2bbbc4c8
wuyong <lingshizhuangzi@gmail.com>
2019-09-10 15:30:45 +0800

    fix

---
+++ FWFramework/Application/App/Plugin/FWAppToastPlugin.m
@@ -0,0 +1,14 @@
+/*!
+ @header     FWAppToastPlugin.m
+ @indexgroup FWFramework
+ @brief      FWAppToastPlugin
+ @author     wuyong
+ @copyright  Copyright © 2019 wuyong.site. All rights reserved.
+ @updated    2019/9/10
+ */
+
+#import "FWAppToastPlugin.h"
+
+@implementation FWAppToastPlugin
+
+@end
