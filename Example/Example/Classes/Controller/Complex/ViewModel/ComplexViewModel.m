1e18dd8c883f57c443963497e525367c88c2d262
wuyong <lingshizhuangzi@gmail.com>
2019-10-11 12:50:40 +0800

    fix

---
+++ Example/Example/Classes/Controller/Complex/ViewModel/ComplexViewModel.m
@@ -0,0 +1,13 @@
+//
+//  ComplexViewModel.m
+//  Example
+//
+//  Created by wuyong on 2019/10/11.
+//  Copyright © 2019 wuyong.site. All rights reserved.
+//
+
+#import "ComplexViewModel.h"
+
+@implementation ComplexViewModel
+
+@end
