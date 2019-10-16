183c7a82d11afcd7cf79c18f0c835f55b455d6c3
wuyong <lingshizhuangzi@gmail.com>
2019-10-16 23:12:31 +0800

    fix

--- Example/Example/Classes/App/AppDelegate.m
+++ Example/Example/Classes/App/AppDelegate.m
@@ -7,16 +7,59 @@
 //
 
 #import "AppDelegate.h"
+#import "ObjcController.h"
+#import "TestViewController.h"
 
 #if APP_TARGET == 2
 
 #pragma mark - AppDelegate+Flutter
+
+@import FlutterPluginRegistrant;
 
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.flutterEngine = [[FlutterEngine alloc] initWithName:@"io.flutter" project:nil];
+    [self.flutterEngine runWithEntrypoint:nil];
+    [GeneratedPluginRegistrant registerWithRegistry:self.flutterEngine];
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [self rootViewController];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (UIViewController *)rootViewController
+{
+    UIViewController *homeController = [ObjcController new];
+    homeController.hidesBottomBarWhenPushed = NO;
+    UINavigationController *homeNav = [[UINavigationController alloc] initWithRootViewController:homeController];
+    homeNav.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
+    homeNav.tabBarItem.title = @"首页";
+    
+    FlutterViewController *flutterController = [[FlutterViewController alloc] initWithEngine:self.flutterEngine nibName:nil bundle:nil];
+    // [flutterController setInitialRoute:@"route1"];
+    flutterController.hidesBottomBarWhenPushed = NO;
+    flutterController.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
+    flutterController.tabBarItem.title = @"Flutter";
+    
+    UIViewController *testController = [TestViewController new];
+    testController.hidesBottomBarWhenPushed = NO;
+    UINavigationController *testNav = [[UINavigationController alloc] initWithRootViewController:testController];
+    testNav.tabBarItem.image = [UIImage imageNamed:@"tabbar_settings"];
+    testNav.tabBarItem.title = @"测试";
+    
+    UITabBarController *tabBarController = [[UITabBarController alloc] init];
+    tabBarController.viewControllers = @[homeNav, flutterController, testNav];
+    return tabBarController;
+}
 
 @end
 
@@ -30,84 +73,22 @@
 
 @implementation AppDelegate
 
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [[FWNotificationManager sharedInstance] handleRemoteNotification:userInfo];
-    completionHandler(UIBackgroundFetchResultNewData);
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [self rootViewController];
+    [self.window makeKeyAndVisible];
+    return YES;
 }
 
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+- (UIViewController *)rootViewController
 {
-    [[FWNotificationManager sharedInstance] handleLocalNotification:notification];
-}
-
-@end
-
-#endif
-
-#pragma mark - AppDelegate+FWAppDelegate
-
-#import "ObjcController.h"
-#import "TestViewController.h"
-
-@interface AppDelegate (FWAppDelegate) <UITabBarControllerDelegate>
-
-@end
-
-@implementation AppDelegate (FWAppDelegate)
-
-- (void)setupApplication:(UIApplication *)application options:(NSDictionary *)options
-{
-    [[FWNotificationManager sharedInstance] clearNotificationBadges];
-    
-    NSDictionary *remoteNotification = (NSDictionary *)[options objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
-    if (remoteNotification) {
-        [[FWNotificationManager sharedInstance] handleRemoteNotification:remoteNotification];
-    }
-    NSDictionary *localNotification = (NSDictionary *)[options objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
-    if (localNotification) {
-        [[FWNotificationManager sharedInstance] handleLocalNotification:localNotification];
-    }
-}
-
-- (void)setupService
-{
-    [[FWNotificationManager sharedInstance] registerNotificationHandler];
-    [[FWNotificationManager sharedInstance] requestAuthorize:nil];
-    [FWNotificationManager sharedInstance].remoteNotificationHandler = ^(NSDictionary * userInfo, id notification) {
-        NSString *title = nil;
-        if ([notification isKindOfClass:[UNNotificationResponse class]]) {
-            title = ((UNNotificationResponse *)notification).notification.request.content.title;
-        }
-        [[UIWindow fwMainWindow] fwShowToastWithAttributedText:[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"收到远程通知：%@\n%@", FWSafeString(title), userInfo]]];
-        [[UIWindow fwMainWindow] fwHideToastAfterDelay:2.0 completion:nil];
-    };
-    [FWNotificationManager sharedInstance].localNotificationHandler = ^(NSDictionary * userInfo, id notification) {
-        NSString *title = nil;
-        if ([notification isKindOfClass:[UNNotificationResponse class]]) {
-            title = ((UNNotificationResponse *)notification).notification.request.content.title;
-        } else if ([notification isKindOfClass:[UILocalNotification class]]) {
-            title = ((UILocalNotification *)notification).alertTitle ?: ((UILocalNotification *)notification).alertBody;
-        }
-        [[UIWindow fwMainWindow] fwShowToastWithAttributedText:[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"收到本地通知：%@\n%@", FWSafeString(title), userInfo]]];
-        [[UIWindow fwMainWindow] fwHideToastAfterDelay:2.0 completion:nil];
-    };
-}
-
-- (void)setupController
-{
-    // 自动布局适配RTL
-    [UIView fwAutoLayoutRTL:YES];
-    // 统一设置导航栏样式
-    [[UINavigationBar appearance] fwSetTextColor:[UIColor fwColorWithHex:0x111111]];
-    
-    // 初始化tabBar控制器
     UIViewController *homeController = [ObjcController new];
     homeController.hidesBottomBarWhenPushed = NO;
     UINavigationController *homeNav = [[UINavigationController alloc] initWithRootViewController:homeController];
     homeNav.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
     homeNav.tabBarItem.title = @"首页";
-    [homeNav.tabBarItem fwShowBadgeView:[[FWBadgeView alloc] initWithBadgeStyle:FWBadgeStyleSmall] badgeValue:@"1"];
     
     UIViewController *testController = [TestViewController new];
     testController.hidesBottomBarWhenPushed = NO;
@@ -116,36 +97,10 @@
     testNav.tabBarItem.title = @"测试";
     
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    tabBarController.delegate = self;
-    // present时隐藏tabBar
-    tabBarController.definesPresentationContext = YES;
     tabBarController.viewControllers = @[homeNav, testNav];
-    
-    // 设置主控制器
-    self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = tabBarController;
-}
-
-- (void)setupDeviceToken:(NSData *)deviceToken error:(NSError *)error
-{
-    [UIDevice fwSetDeviceToken:deviceToken];
-}
-
-- (BOOL)handleOpenURL:(NSURL *)url options:(NSDictionary *)options
-{
-    [FWRouter openURL:url.absoluteString];
-    return YES;
-}
-
-#pragma mark - UITabBarControllerDelegate
-
-- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController
-{
-    UIImageView *imageView = viewController.tabBarItem.fwImageView;
-    CABasicAnimation *animation = [imageView fwAddAnimationWithKeyPath:@"transform.scale" fromValue:@(0.7) toValue:@(1.3) duration:0.08 completion:nil];
-    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-    animation.repeatCount = 1;
-    animation.autoreverses = YES;
+    return tabBarController;
 }
 
 @end
+
+#endif
