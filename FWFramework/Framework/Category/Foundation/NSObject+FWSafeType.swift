51a17f23bcd6608b4f5cad13d3c90b0d84656250
wuyong <lingshizhuangzi@gmail.com>
2019-10-30 17:54:41 +0800

    fix

--- FWFramework/Framework/Category/Foundation/NSObject+FWSafeType.swift
+++ FWFramework/Framework/Category/Foundation/NSObject+FWSafeType.swift
@@ -9,14 +9,14 @@
 import Foundation
 
 /// 安全解包协议
-public protocol FWSafelyUnwrapped {
+public protocol FWSafelyUnwrappable: Equatable {
     // 提供安全解包默认值
     static var fwUnwrappedValue: Self { get }
 }
 
 /// 可选值安全解包扩展
-extension Optional where Wrapped: FWSafelyUnwrapped {
-    /// 解包对象实现了安全解包协议，当值为nil时，会返回默认值
+extension Optional where Wrapped: FWSafelyUnwrappable {
+    /// 当值为nil时，会返回默认值。注意可选链调用时可能不会触发，推荐使用FWSafeValue
     public func fwSafeValue() -> Wrapped {
         if let value = self {
             return value
@@ -24,48 +24,69 @@ extension Optional where Wrapped: FWSafelyUnwrapped {
             return Wrapped.fwUnwrappedValue
         }
     }
+    
+    /// 判断对象是否非空。当值为nil时，会返回false。注意可选链调用时可能不会触发，推荐使用FWIsNotEmpty
+    public func fwIsNotEmpty() -> Bool {
+        if let value = self {
+            return value != Wrapped.fwUnwrappedValue;
+        } else {
+            return false
+        }
+    }
+}
+
+/// 当值为nil时，会返回默认值
+/// - Parameter value: 实现了安全解包协议的可选对象
+public func FWSafeValue<T: FWSafelyUnwrappable>(_ value: T?) -> T {
+    return value.fwSafeValue()
+}
+
+/// 当值为nil时，会返回false
+/// - Parameter value: 实现了安全判断协议的可选对象
+public func FWIsNotEmpty<T: FWSafelyUnwrappable>(_ value: T?) -> Bool {
+    return value.fwIsNotEmpty()
 }
 
 /// 常用类实现安全解包协议
-extension Int: FWSafelyUnwrapped {
+extension Int: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Int = .zero
 }
-extension Int8: FWSafelyUnwrapped {
+extension Int8: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Int8 = .zero
 }
-extension Int16: FWSafelyUnwrapped {
+extension Int16: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Int16 = .zero
 }
-extension Int32: FWSafelyUnwrapped {
+extension Int32: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Int32 = .zero
 }
-extension Int64: FWSafelyUnwrapped {
+extension Int64: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Int64 = .zero
 }
-extension UInt: FWSafelyUnwrapped {
+extension UInt: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: UInt = .zero
 }
-extension UInt8: FWSafelyUnwrapped {
+extension UInt8: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: UInt8 = .zero
 }
-extension UInt16: FWSafelyUnwrapped {
+extension UInt16: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: UInt16 = .zero
 }
-extension UInt32: FWSafelyUnwrapped {
+extension UInt32: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: UInt32 = .zero
 }
-extension UInt64: FWSafelyUnwrapped {
+extension UInt64: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: UInt64 = .zero
 }
-extension Float: FWSafelyUnwrapped {
+extension Float: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Float = .zero
 }
-extension Double: FWSafelyUnwrapped {
+extension Double: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Double = .zero
 }
-extension Bool: FWSafelyUnwrapped {
+extension Bool: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: Bool = false
 }
-extension String: FWSafelyUnwrapped {
+extension String: FWSafelyUnwrappable {
     public static var fwUnwrappedValue: String = ""
 }
