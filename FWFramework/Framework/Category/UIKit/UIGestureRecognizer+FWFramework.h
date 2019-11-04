42bf7ddd5130e5521ad96073873d735a521a95bd
wuyong <lingshizhuangzi@gmail.com>
2019-11-04 13:13:49 +0800

    fix

--- FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.h
+++ FWFramework/Framework/Category/UIKit/UIGestureRecognizer+FWFramework.h
@@ -8,6 +8,7 @@
  */
 
 #import <UIKit/UIKit.h>
+#import "UIGestureRecognizer+FWDrawerView.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -38,57 +39,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 // 当前滑动进度，滑动绝对值相对于手势视图的宽或高
 - (CGFloat)fwSwipePercent;
-
-#pragma mark - DrawerView
-
-/*!
- @brief 二级抽屉拖拽效果(简单版)。如果view为滚动视图，自动设置delegate处理与滚动视图pan手势冲突的问题
- 
- @param view 抽屉视图，默认为self.view
- @param direction 拖拽方向，如向上拖动视图时为Up，向下为Down，向右为Right，向左为Left
- @param fromPosition 相对于view父视图的起点originY位置
- @param toPosition 相对于view父视图的终点originY位置
- @param kickbackHeight 回弹高度，拖拽小于该高度执行回弹
- @param callback 抽屉视图位移回调，参数为相对view父视图的origin位置和是否拖拽完成的标记
- */
-- (void)fwDrawerView:(nullable UIView *)view
-           direction:(UISwipeGestureRecognizerDirection)direction
-        fromPosition:(CGFloat)fromPosition
-          toPosition:(CGFloat)toPosition
-      kickbackHeight:(CGFloat)kickbackHeight
-            callback:(nullable void (^)(CGFloat position, BOOL finished))callback;
-
-/*!
- @brief 多级抽屉拖拽效果(详细版)。如果view为滚动视图，自动设置delegate处理与滚动视图pan手势冲突的问题
- 
- @param view 抽屉视图，默认为self.view
- @param direction 拖拽方向，如向上拖动视图时为Up，向下为Down，向右为Right，向左为Left
- @param positions 抽屉位置，至少两级，相对于view父视图的originY位置
- @param kickbackHeight 回弹高度，拖拽小于该高度执行回弹
- @param callback 抽屉视图位移回调，参数为相对view父视图的origin位置和是否拖拽完成的标记
- */
-- (void)fwDrawerView:(nullable UIView *)view
-           direction:(UISwipeGestureRecognizerDirection)direction
-           positions:(NSArray<NSNumber *> *)positions
-      kickbackHeight:(CGFloat)kickbackHeight
-            callback:(nullable void (^)(CGFloat position, BOOL finished))callback;
-
-/*!
- @brief 判断二级或多级抽屉效果视图是否位于打开位置
- @discussion 打开位置定义：拖拽方向为Up时fromPosition，Down时toPosition，Right时toPosition，Left时fromPosition，关闭位置取反即可
- 
- @return 是否位于打开位置
- */
-- (BOOL)fwDrawerViewIsOpen;
-
-// 设置二级抽屉效果视图到打开位置或关闭位置，如果位置发生改变，会触发抽屉callback回调
-- (void)fwDrawerViewToggleOpen:(BOOL)open;
-
-// 判断多级抽屉效果视图是否位于指定位置
-- (BOOL)fwDrawerViewIsPosition:(CGFloat)position;
-
-// 设置多级抽屉效果视图到指定位置，如果位置发生改变，会触发抽屉callback回调
-- (void)fwDrawerViewTogglePosition:(CGFloat)position;
 
 @end
 
