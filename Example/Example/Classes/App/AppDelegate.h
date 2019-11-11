32f46e909c54b8de41d13d079065db43109f73ae
wuyong <lingshizhuangzi@gmail.com>
2019-11-11 20:47:10 +0800

    fix

--- Example/Example/Classes/App/AppDelegate.h
+++ Example/Example/Classes/App/AppDelegate.h
@@ -6,20 +6,8 @@
 //  Copyright © 2018年 wuyong.site. All rights reserved.
 //
 
-#if APP_TARGET == 2
-
-#import <Flutter/Flutter.h>
-
-@interface AppDelegate : FlutterAppDelegate
-
-@end
-
-#else
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : FWAppDelegate
 
 @end
-
-#endif
