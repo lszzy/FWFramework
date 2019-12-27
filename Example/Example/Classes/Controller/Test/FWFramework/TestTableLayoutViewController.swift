c0575f4ef63ab89e01adb75537c0c89e27e8dca0
wuyong <lingshizhuangzi@gmail.com>
2019-12-27 21:57:47 +0800

    swift

--- Example/Example/Classes/Controller/Test/FWFramework/TestTableLayoutViewController.swift
+++ Example/Example/Classes/Controller/Test/FWFramework/TestTableLayoutViewController.swift
@@ -13,12 +13,12 @@ extension TestTableLayoutViewController {
         self.tableView.fwTabAnimated = FWTabTableAnimated(cellClass: TestTableLayoutCell.self, cellHeight: 120)
         // self.tableView.fwTabAnimated?.animatedBackgroundColor = UIColor.appColorBg()
         self.tableView.fwTabAnimated?.adjustBlock = { (manager) in
-            manager.animation()?(3)?.remove()()
-            manager.animation()?(0)?.line()(1)
-            manager.animation()?(1)?.line()(1)
-            manager.animation()?(0)?.width()(100)?.toLongAnimation()()
-            manager.animation()?(1)?.up()(5)
-            manager.animation()?(2)?.width()(30)?.height()(30)?.placeholder()("AppIcon")
+            manager.animation(3)?.remove()
+            manager.animations(indexs: 0,1)?.line(1)
+            manager.animations(0, 1)?.line(1)
+            manager.animation(0)?.width(100).toLongAnimation()
+            manager.animation(1)?.up(5)
+            manager.animation(2)?.width(30).height(30).placeholder("AppIcon")
         }
     }
 }
--- FWFramework/Application/View/Component/FWTabAnimated.swift
+++ FWFramework/Application/View/Component/FWTabAnimated.swift
@@ -31,8 +31,108 @@ extension FWTabComponentManager {
 
 extension Array where Element: FWTabBaseComponent {
     @discardableResult
+    public func left(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_left()(value)!
+    }
+    
+    @discardableResult
+    public func right(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_right()(value)!
+    }
+    
+    @discardableResult
+    public func up(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_up()(value)!
+    }
+    
+    @discardableResult
+    public func down(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_down()(value)!
+    }
+    
+    @discardableResult
+    public func width(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_width()(value)!
+    }
+    
+    @discardableResult
+    public func height(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_height()(value)!
+    }
+    
+    @discardableResult
+    public func radius(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_radius()(value)!
+    }
+    
+    @discardableResult
+    public func reducedWidth(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_reducedWidth()(value)!
+    }
+    
+    @discardableResult
+    public func reducedHeight(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_reducedHeight()(value)!
+    }
+    
+    @discardableResult
+    public func reducedRadius(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_reducedRadius()(value)!
+    }
+    
+    @discardableResult
     public func line(_ value: Int) -> [FWTabBaseComponent] {
         return (self as NSArray)._oc_line()(value)!
+    }
+    
+    @discardableResult
+    public func space(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_space()(value)!
+    }
+    
+    @discardableResult
+    public func remove() -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_remove()()!
+    }
+    
+    @discardableResult
+    public func placeholder(_ value: String) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_placeholder()(value)!
+    }
+    
+    @discardableResult
+    public func x(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_x()(value)!
+    }
+    
+    @discardableResult
+    public func y(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_y()(value)!
+    }
+    
+    @discardableResult
+    public func color(_ value: UIColor) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_color()(value)!
+    }
+    
+    @discardableResult
+    public func dropIndex(_ value: Int) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_dropIndex()(value)!
+    }
+    
+    @discardableResult
+    public func dropFromIndex(_ value: Int) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_dropFromIndex()(value)!
+    }
+    
+    @discardableResult
+    public func removeOnDrop() -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_removeOnDrop()()!
+    }
+    
+    @discardableResult
+    public func dropStayTime(_ value: CGFloat) -> [FWTabBaseComponent] {
+        return (self as NSArray)._oc_dropStayTime()(value)!
     }
 }
 
@@ -40,7 +140,127 @@ extension Array where Element: FWTabBaseComponent {
 
 extension FWTabBaseComponent {
     @discardableResult
+    public func left(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_left()(value)!
+    }
+    
+    @discardableResult
+    public func right(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_right()(value)!
+    }
+    
+    @discardableResult
+    public func up(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_up()(value)!
+    }
+    
+    @discardableResult
+    public func down(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_down()(value)!
+    }
+    
+    @discardableResult
+    public func width(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_width()(value)!
+    }
+    
+    @discardableResult
+    public func height(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_height()(value)!
+    }
+    
+    @discardableResult
+    public func radius(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_radius()(value)!
+    }
+    
+    @discardableResult
+    public func reducedWidth(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_reducedWidth()(value)!
+    }
+    
+    @discardableResult
+    public func reducedHeight(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_reducedHeight()(value)!
+    }
+    
+    @discardableResult
+    public func reducedRadius(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_reducedRadius()(value)!
+    }
+    
+    @discardableResult
+    public func x(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_x()(value)!
+    }
+    
+    @discardableResult
+    public func y(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_y()(value)!
+    }
+    
+    @discardableResult
+    public func line(_ value: Int) -> FWTabBaseComponent {
+        return self._oc_line()(value)!
+    }
+    
+    @discardableResult
+    public func space(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_space()(value)!
+    }
+    
+    @discardableResult
+    public func lastLineScale(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_lastLineScale()(value)!
+    }
+    
+    @discardableResult
     public func remove() -> FWTabBaseComponent {
         return self._oc_remove()()!
+    }
+    
+    @discardableResult
+    public func placeholder(_ value: String) -> FWTabBaseComponent {
+        return self._oc_placeholder()(value)!
+    }
+    
+    @discardableResult
+    public func toLongAnimation() -> FWTabBaseComponent {
+        return self._oc_toLongAnimation()()!
+    }
+    
+    @discardableResult
+    public func toShortAnimation() -> FWTabBaseComponent {
+        return self._oc_toShortAnimation()()!
+    }
+    
+    @discardableResult
+    public func cancelAlignCenter() -> FWTabBaseComponent {
+        return self._oc_cancelAlignCenter()()!
+    }
+    
+    @discardableResult
+    public func color(_ value: UIColor) -> FWTabBaseComponent {
+        return self._oc_color()(value)!
+    }
+    
+    @discardableResult
+    public func dropIndex(_ value: Int) -> FWTabBaseComponent {
+        return self._oc_dropIndex()(value)!
+    }
+    
+    @discardableResult
+    public func dropFromIndex(_ value: Int) -> FWTabBaseComponent {
+        return self._oc_dropFromIndex()(value)!
+    }
+    
+    @discardableResult
+    public func removeOnDrop() -> FWTabBaseComponent {
+        return self._oc_removeOnDrop()()!
+    }
+    
+    @discardableResult
+    public func dropStayTime(_ value: CGFloat) -> FWTabBaseComponent {
+        return self._oc_dropStayTime()(value)!
     }
 }
