525b920e00efe7c3c4d323c625ccddc79c7fb4d9
wuyong <lingshizhuangzi@gmail.com>
2020-02-22 22:21:26 +0800

    fix

---
+++ FWFramework/Framework/Service/Manager/FWExceptionManager.h
@@ -0,0 +1,17 @@
+/*!
+ @header     FWExceptionManager.h
+ @indexgroup FWFramework
+ @brief      FWExceptionManager
+ @author     wuyong
+ @copyright  Copyright © 2020 wuyong.site. All rights reserved.
+ @updated    2020/2/22
+ */
+
+#import <Foundation/Foundation.h>
+
+/*!
+ @brief FWExceptionManager
+ */
+@interface FWExceptionManager : NSObject
+
+@end
