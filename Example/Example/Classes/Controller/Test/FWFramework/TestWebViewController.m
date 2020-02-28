df63f4104d1c55ae610c36c40d176eeba461f5d3
wuyong <lingshizhuangzi@gmail.com>
2020-02-28 14:12:53 +0800

    uiwebview

--- Example/Example/Classes/Controller/Test/FWFramework/TestWebViewController.m
+++ Example/Example/Classes/Controller/Test/FWFramework/TestWebViewController.m
@@ -8,16 +8,10 @@
 
 #import "TestWebViewController.h"
 
-@interface TestWebViewController () <UIWebViewDelegate, FWWebViewProgressDelegate, FWWebViewNavigationDelegate>
+@interface TestWebViewController () <FWWebViewNavigationDelegate>
 
-@property (nonatomic, assign) BOOL isWKWebView;
-
-@property (nonatomic, strong) UIWebView *webView;
-@property (nonatomic, strong) FWWebViewProgress *progressProxy;
-@property (nonatomic, strong) FWWebViewProgressView *progressView;
-
-@property (nonatomic, strong) WKWebView *webView2;
-@property (nonatomic, strong) UIProgressView *progressView2;
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UIProgressView *progressView;
 
 @end
 
@@ -25,72 +19,33 @@
 
 - (void)renderView
 {
-    if (!self.isWKWebView) {
-        self.webView = [[UIWebView alloc] init];
-        [self.view addSubview:self.webView];
-        [self.webView fwPinEdgesToSuperview];
-        
-        self.progressProxy = [[FWWebViewProgress alloc] init];
-        self.webView.delegate = self.progressProxy;
-        self.progressProxy.webViewProxyDelegate = self;
-        self.progressProxy.progressDelegate = self;
-        
-        _progressView = [[FWWebViewProgressView alloc] initWithFrame:CGRectMake(0, 0, FWScreenWidth, 2.f)];
-        [self.webView addSubview:_progressView];
-        
-        NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://www.wuyong.site"]];
-        [_webView loadRequest:req];
-    } else {
-        self.webView2 = [[WKWebView alloc] initWithFrame:self.view.bounds];
-        self.webView2.fwNavigationDelegate = self;
-        [self.view addSubview:self.webView2];
-        [self.webView2 fwPinEdgesToSuperview];
-        
-        _progressView2 = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 0, FWScreenWidth, 2.f)];
-        _progressView2.trackTintColor = [UIColor clearColor];
-        [_progressView2 fwSetProgress:0];
-        [self.webView2 addSubview:_progressView2];
-        
-        NSURLRequest *req2 = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://www.wuyong.site"]];
-        [_webView2 loadRequest:req2];
-    }
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    self.webView.fwNavigationDelegate = self;
+    [self.view addSubview:self.webView];
+    [self.webView fwPinEdgesToSuperview];
+    
+    _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 0, FWScreenWidth, 2.f)];
+    _progressView.trackTintColor = [UIColor clearColor];
+    [_progressView fwSetProgress:0];
+    [self.webView addSubview:_progressView];
+    
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://www.wuyong.site"]];
+    [_webView loadRequest:req];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    if (!self.isWKWebView) {
-        self.title = @"UIWebView";
-        
-        FWWeakifySelf();
-        [self fwSetRightBarItem:@"WKWebView" block:^(id sender) {
-            FWStrongifySelf();
-            
-            TestWebViewController *viewController = [TestWebViewController new];
-            viewController.isWKWebView = YES;
-            [self fwOpenViewController:viewController animated:YES];
-        }];
-    } else {
-        self.title = @"WKWebView";
-    }
-}
-
-#pragma mark - FWWebViewProgressDelegate
-
-- (void)webViewProgress:(FWWebViewProgress *)webViewProgress updateProgress:(float)progress
-{
-    [_progressView setProgress:progress animated:YES];
-    self.title = [_webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    self.title = @"WKWebView";
 }
 
 #pragma mark - FWWebViewNavigationDelegate
 
 - (void)webView:(WKWebView *)webView updateProgress:(CGFloat)progress
 {
-    [self.progressView2 fwSetProgress:progress];
+    [self.progressView fwSetProgress:progress];
     FWWeakifySelf();
-    [_webView2 evaluateJavaScript:@"document.title" completionHandler:^(id title, NSError * _Nullable error) {
+    [_webView evaluateJavaScript:@"document.title" completionHandler:^(id title, NSError * _Nullable error) {
         FWStrongifySelf();
         self.title = [title fwAsNSString];
     }];
