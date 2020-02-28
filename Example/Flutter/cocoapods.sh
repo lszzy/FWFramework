9c9e841ee8b84314106f37f943ec7834f4d1040a
wuyong <lingshizhuangzi@gmail.com>
2020-02-29 01:59:12 +0800

    flutter

--- Example/Flutter/cocoapods.sh
+++ Example/Flutter/cocoapods.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # 创建模块：flutter create --template module example
-# 编译模块：flutter版本1.13.6之后才有--cocoapods参数，检出分支git clone -b v1.13.6 https://github.com/flutter/flutter.git或者官网下载
+# 编译模块：flutter版本1.13.6才有--cocoapods参数，可官网下载或切换版本
+# 切换版本：git clone -b v1.13.6 https://github.com/flutter/flutter.git
 cd example
 flutter build ios-framework --cocoapods --output=../cocoapods/
