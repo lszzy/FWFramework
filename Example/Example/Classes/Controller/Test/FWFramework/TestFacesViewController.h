50c2babb5e39c95f495421413d39147a53ac80b3
wuyong <lingshizhuangzi@gmail.com>
2020-06-24 00:45:58 +0800

    fix

---
+++ Example/Example/Classes/Controller/Test/FWFramework/TestFacesViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestFacesViewController.h
+//  Example
+//
+//  Created by wuyong on 2020/6/24.
+//  Copyright © 2020 wuyong.site. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestFacesViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
