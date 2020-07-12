acb31193940f0cb339c6ebfed394012aff3b1d3d
wuyong <lingshizhuangzi@gmail.com>
2020-07-12 12:52:39 +0800

    fix

---
+++ FWFramework/Component/Foundation/Component/Foundation+FWComponent.h
@@ -0,0 +1,12 @@
+//
+//  Foundation+FWComponent.h
+//  FWFramework
+//
+//  Created by wuyong on 2020/7/12.
+//  Copyright © 2020 wuyong.site. All rights reserved.
+//
+
+#ifndef Foundation_FWComponent_h
+#define Foundation_FWComponent_h
+
+#endif /* Foundation_FWComponent_h */
