c738be364381b568f01945ff415f4a0b3ad3bb62
wuyong <lingshizhuangzi@gmail.com>
2020-07-24 11:16:50 +0800

    如果Example插件没法加载，去掉FWFramework.xcodeproj项目即可

--- FWFramework/Component/UIKit/Component/FWPopupMenu.h
+++ FWFramework/Component/UIKit/Component/FWPopupMenu.h
@@ -223,6 +223,11 @@ typedef NS_ENUM(NSInteger , FWPopupMenuPriorityDirection) {
 @property (nonatomic, assign) CGFloat fontSize;
 
 /**
+ 设置字体 设置时忽略fontSize Default is nil
+ */
+@property (nonatomic, strong) UIFont * font;
+
+/**
  设置字体颜色 自定义cell时忽略 Default is [UIColor blackColor]
  */
 @property (nonatomic, strong) UIColor * textColor;
