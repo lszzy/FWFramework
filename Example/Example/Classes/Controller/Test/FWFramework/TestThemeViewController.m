02f55a39f883936d288db1d6983ce241a2220237
wuyong <lingshizhuangzi@gmail.com>
2020-08-15 20:33:06 +0800

    fix

--- Example/Example/Classes/Controller/Test/FWFramework/TestThemeViewController.m
+++ Example/Example/Classes/Controller/Test/FWFramework/TestThemeViewController.m
@@ -8,12 +8,6 @@
 
 #import "TestThemeViewController.h"
 
-@interface TestThemeViewController ()
-
-@property (nonatomic, strong) NSMutableArray *themeViews;
-
-@end
-
 @implementation TestThemeViewController
 
 - (void)renderInit
@@ -21,29 +15,6 @@
     FWThemeManager.sharedInstance.overrideWindow = YES;
     [self fwObserveNotification:FWThemeChangedNotification block:^(NSNotification * _Nonnull notification) {
         NSLog(@"主题改变通知：%@", @(FWThemeManager.sharedInstance.style));
-    }];
-    
-    self.themeViews = [NSMutableArray new];
-}
-
-- (void)addView:(id)view
-{
-    [self.themeViews addObject:view];
-    if ([view isKindOfClass:[UIView class]]) {
-        [self.view addSubview:view];
-    } else {
-        [self.view.layer addSublayer:view];
-    }
-}
-
-- (void)removeViews
-{
-    [self.themeViews enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        if ([obj isKindOfClass:[UIView class]]) {
-            [obj removeFromSuperview];
-        } else {
-            [obj removeFromSuperlayer];
-        }
     }];
 }
 
@@ -53,34 +24,34 @@
     
     UIView *colorView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 50, 50)];
     colorView.backgroundColor = [UIColor fwThemeLight:[UIColor blackColor] dark:[UIColor whiteColor]];
-    [self addView:colorView];
+    [self.view addSubview:colorView];
     
     colorView = [[UIView alloc] initWithFrame:CGRectMake(90, 20, 50, 50)];
     colorView.backgroundColor = [UIColor fwThemeColor:^UIColor * _Nonnull(FWThemeStyle style) {
         return style == FWThemeStyleDark ? [UIColor whiteColor] : [UIColor blackColor];
     }];
-    [self addView:colorView];
+    [self.view addSubview:colorView];
     
     colorView = [[UIView alloc] initWithFrame:CGRectMake(160, 20, 50, 50)];
     colorView.backgroundColor = [UIColor fwThemeNamed:@"theme_color"];
-    [self addView:colorView];
+    [self.view addSubview:colorView];
     
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 90, 50, 50)];
     imageView.image = [UIImage fwThemeLight:[UIImage imageNamed:@"theme_image_light"] dark:[UIImage imageNamed:@"theme_image_dark"]];
     [imageView fwAddThemeListener:^(FWThemeStyle style) {
         imageView.image = [UIImage fwThemeLight:[UIImage imageNamed:@"theme_image_light"] dark:[UIImage imageNamed:@"theme_image_dark"]];
     }];
-    [self addView:imageView];
+    [self.view addSubview:imageView];
     
     imageView = [[UIImageView alloc] initWithFrame:CGRectMake(90, 90, 50, 50)];
     imageView.fwThemeImage = [UIImage fwThemeImage:^UIImage *(FWThemeStyle style) {
         return style == FWThemeStyleDark ? [UIImage imageNamed:@"theme_image_dark"] : [UIImage imageNamed:@"theme_image_light"];
     }];
-    [self addView:imageView];
+    [self.view addSubview:imageView];
     
     imageView = [[UIImageView alloc] initWithFrame:CGRectMake(160, 90, 50, 50)];
     imageView.image = [UIImage fwThemeNamed:@"theme_image"];
-    [self addView:imageView];
+    [self.view addSubview:imageView];
     
     CALayer *layer = [CALayer new];
     layer.frame = CGRectMake(20, 160, 50, 50);
@@ -88,19 +59,19 @@
     [layer fwAddThemeListener:^(FWThemeStyle style) {
         layer.backgroundColor = [UIColor fwThemeLight:[UIColor blackColor] dark:[UIColor whiteColor]].CGColor;
     }];
-    [self addView:layer];
+    [self.view.layer addSublayer:layer];
     
     layer = [CALayer new];
     layer.frame = CGRectMake(90, 160, 50, 50);
     layer.fwThemeBackgroundColor = [UIColor fwThemeColor:^UIColor * _Nonnull(FWThemeStyle style) {
         return style == FWThemeStyleDark ? [UIColor whiteColor] : [UIColor blackColor];
     }];
-    [self addView:layer];
+    [self.view.layer addSublayer:layer];
     
-    layer = [CALayer new];
-    layer.frame = CGRectMake(160, 160, 50, 50);
-    layer.fwThemeBackgroundColor = [UIColor fwThemeNamed:@"theme_color"];
-    [self addView:layer];
+    CAGradientLayer *gradientLayer = [CAGradientLayer new];
+    gradientLayer.frame = CGRectMake(160, 160, 50, 50);
+    gradientLayer.fwThemeColors = @[[UIColor fwThemeNamed:@"theme_color"]];
+    [self.view.layer addSublayer:gradientLayer];
     
     layer = [CALayer new];
     layer.frame = CGRectMake(20, 230, 50, 50);
@@ -108,19 +79,19 @@
     [layer fwAddThemeListener:^(FWThemeStyle style) {
         layer.contents = (id)[UIImage fwThemeLight:[UIImage imageNamed:@"theme_image_light"] dark:[UIImage imageNamed:@"theme_image_dark"]].CGImage;
     }];
-    [self addView:layer];
+    [self.view.layer addSublayer:layer];
     
     layer = [CALayer new];
     layer.frame = CGRectMake(90, 230, 50, 50);
     layer.fwThemeContents = (id)[UIImage fwThemeImage:^UIImage * _Nonnull(FWThemeStyle style) {
         return style == FWThemeStyleDark ? [UIImage imageNamed:@"theme_image_dark"] : [UIImage imageNamed:@"theme_image_light"];
     }];
-    [self addView:layer];
+    [self.view.layer addSublayer:layer];
     
     layer = [CALayer new];
     layer.frame = CGRectMake(160, 230, 50, 50);
     layer.fwThemeContents = (id)[UIImage fwThemeNamed:@"theme_image"];
-    [self addView:layer];
+    [self.view.layer addSublayer:layer];
 }
 
 - (void)renderModel
@@ -135,10 +106,6 @@
             FWStrongifySelf();
             
             FWThemeManager.sharedInstance.mode = index;
-            if (@available(iOS 13, *)) { } else {
-                [self removeViews];
-                [self renderView];
-            }
             [self renderModel];
         }];
     }];
