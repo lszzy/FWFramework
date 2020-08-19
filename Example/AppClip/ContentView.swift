d47270c9a391d88c23eea9451c78e83686808efb
wuyong <lingshizhuangzi@gmail.com>
2020-08-19 14:59:18 +0800

    AppClip

---
+++ Example/AppClip/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AppClip
+//
+//  Created by wuyong on 2020/8/19.
+//  Copyright © 2020 site.wuyong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
