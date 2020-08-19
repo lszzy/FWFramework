cdd9834a3873b159006ea8b1cae19e48de3ce5ae
wuyong <lingshizhuangzi@gmail.com>
2020-08-19 14:59:18 +0800

    AppClip

---
+++ Example/AppClip/AppClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppClipApp.swift
+//  AppClip
+//
+//  Created by wuyong on 2020/8/19.
+//  Copyright © 2020 site.wuyong. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
