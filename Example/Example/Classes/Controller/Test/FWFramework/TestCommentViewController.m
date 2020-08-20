0be8d32a0857ab94736ba7827044e0e461236d00
wuyong <lingshizhuangzi@gmail.com>
2020-08-20 11:47:34 +0800

    fix

---
+++ Example/Example/Classes/Controller/Test/FWFramework/TestCommentViewController.m
@@ -0,0 +1,32 @@
+//
+//  TestCommentViewController.m
+//  Example
+//
+//  Created by wuyong on 2020/8/20.
+//  Copyright © 2020 site.wuyong. All rights reserved.
+//
+
+#import "TestCommentViewController.h"
+
+@interface TestCommentViewController ()
+
+@end
+
+@implementation TestCommentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
