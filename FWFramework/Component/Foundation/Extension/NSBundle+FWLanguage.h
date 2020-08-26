d44167fe60d2c02adf673652d3ab00b4ab5e82fb
wuyong <lingshizhuangzi@gmail.com>
2020-08-26 22:39:09 +0800

    fix

--- FWFramework/Component/Foundation/Extension/NSBundle+FWLanguage.h
+++ FWFramework/Component/Foundation/Extension/NSBundle+FWLanguage.h
@@ -21,7 +21,7 @@ extern NSString *const FWLocalizedLanguageChangedNotification;
 @discussion 如果系统组件无法正确显示语言，需Info.plist设置CFBundleAllowMixedLocalizations为YES，从而允许应用程序获取框架库内语言。
 如果key为nil，value为nil，返回空串；key为nil，value非nil，返回value；如果key不存在，value为nil或空，返回key；如果key不存在，value非空，返回value
 当前使用修改bundle类方式实现，也可以使用动态替换localizedStringForKey方法来实现，但需注意此方式的性能
-FWFramework所需本地化翻译如下(默认中文)：完成|关闭|确定|取消
+FWFramework所需本地化翻译如下(默认zh|en等)：完成|关闭|确定|取消
 */
 @interface NSBundle (FWLanguage)
 
