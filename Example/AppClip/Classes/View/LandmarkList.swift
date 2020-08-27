1e2ad4bb640c7e9a5a21ce82ed057f90b2ace7d1
wuyong <lingshizhuangzi@gmail.com>
2020-08-27 11:01:03 +0800

    fix

--- Example/AppClip/Classes/View/LandmarkList.swift
+++ Example/AppClip/Classes/View/LandmarkList.swift
@@ -23,6 +23,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 8", "iPhone XS"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
--- Example/AppClip/Classes/View/MapView.swift
+++ Example/AppClip/Classes/View/MapView.swift
@@ -12,13 +12,14 @@ import MapKit
 struct MapView: UIViewRepresentable {
     typealias UIViewType = MKMapView
     
+    var coordinate: CLLocationCoordinate2D
+    
     func makeUIView(context: Context) -> MKMapView {
         return MKMapView(frame: .zero)
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         uiView.setRegion(region, animated: true)
     }
@@ -26,6 +27,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
