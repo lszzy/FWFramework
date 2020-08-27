04947d8ed546fc7d26e49cec12966d4d275c2703
wuyong <lingshizhuangzi@gmail.com>
2020-08-27 11:18:50 +0800

    fix

--- Example/AppClip/Classes/View/LandmarkRow.swift
+++ Example/AppClip/Classes/View/LandmarkRow.swift
@@ -18,6 +18,12 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .imageScale(.medium)
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
