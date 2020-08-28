652cb61cd434e5e0d170ec773a185fd475af4609
wuyong <lingshizhuangzi@gmail.com>
2020-08-28 14:58:31 +0800

    fix

--- Example/AppClip/Classes/Model/UserData.swift
+++ Example/AppClip/Classes/Model/UserData.swift
@@ -12,4 +12,5 @@ import Combine
 final class UserData: ObservableObject {
     @Published var showFavoritesOnly = false
     @Published var landmarks = landmarkData
+    @Published var profile = Profile.default
 }
--- Example/AppClip/Classes/View/ProfileHost.swift
+++ Example/AppClip/Classes/View/ProfileHost.swift
@@ -9,11 +9,35 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var mode
+    @EnvironmentObject var userData: UserData
     @State var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                if self.mode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        self.draftProfile = self.userData.profile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }
+                }
+                
+                Spacer()
+                EditButton()
+            }
+            
+            if self.mode?.wrappedValue == .inactive {
+                ProfileSummary(profile: userData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear(perform: {
+                        self.draftProfile = self.userData.profile
+                    })
+                    .onDisappear(perform: {
+                        self.userData.profile = self.draftProfile
+                    })
+            }
         }
         .padding()
     }
@@ -22,5 +46,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(UserData())
     }
 }
