31658d40f63ecc72651581f2dda522f299282915
wuyong <lingshizhuangzi@gmail.com>
2020-09-02 16:39:31 +0800

    fix

--- Example/Example/Classes/Library/Vendor/SDWebImage/Core/UIImage+ForceDecode.m
+++ Example/Example/Classes/Library/Vendor/SDWebImage/Core/UIImage+ForceDecode.m
@@ -21,22 +21,4 @@
     objc_setAssociatedObject(self, @selector(sd_isDecoded), @(sd_isDecoded), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-+ (nullable UIImage *)sd_decodedImageWithImage:(nullable UIImage *)image {
-    if (!image) {
-        return nil;
-    }
-    return [SDImageCoderHelper decodedImageWithImage:image];
-}
-
-+ (nullable UIImage *)sd_decodedAndScaledDownImageWithImage:(nullable UIImage *)image {
-    return [self sd_decodedAndScaledDownImageWithImage:image limitBytes:0];
-}
-
-+ (nullable UIImage *)sd_decodedAndScaledDownImageWithImage:(nullable UIImage *)image limitBytes:(NSUInteger)bytes {
-    if (!image) {
-        return nil;
-    }
-    return [SDImageCoderHelper decodedAndScaledDownImageWithImage:image limitBytes:bytes];
-}
-
 @end
