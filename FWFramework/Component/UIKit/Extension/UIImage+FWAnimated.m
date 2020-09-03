2dace5b5bccd4bf31ea631a4f5f35deec1255d80
wuyong <lingshizhuangzi@gmail.com>
2020-09-03 22:32:45 +0800

    fix

--- FWFramework/Component/UIKit/Extension/UIImage+FWAnimated.m
+++ FWFramework/Component/UIKit/Extension/UIImage+FWAnimated.m
@@ -9,6 +9,28 @@
 
 #import "UIImage+FWAnimated.h"
 
+UIImage * FWImageName(NSString *name) {
+    return [UIImage imageNamed:name];
+}
+
+UIImage * FWImageFile(NSString *path) {
+    return [UIImage imageWithContentsOfFile:(path.isAbsolutePath ? path : [NSBundle.mainBundle pathForResource:path ofType:nil])];
+}
+
 @implementation UIImage (FWAnimated)
+
++ (UIImage *)fwImageWithName:(NSString *)name
+{
+    return [UIImage imageNamed:name];
+}
+
++ (UIImage *)fwImageWithFile:(NSString *)path
+{
+    NSString *file = path;
+    if (![file isAbsolutePath]) {
+        file = [[NSBundle mainBundle] pathForResource:file ofType:nil];
+    }
+    return [UIImage imageWithContentsOfFile:file];
+}
 
 @end
