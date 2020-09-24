d8c941237b829933537d2c2bae39d7e81dcb4042
wuyong <lingshizhuangzi@gmail.com>
2020-09-24 13:26:09 +0800

    fix

--- FWFramework/Application/Library/Foundation/Extension/NSNumber+FWFramework.h
+++ FWFramework/Application/Library/Foundation/Extension/NSNumber+FWFramework.h
@@ -22,23 +22,35 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface NSNumber (FWFramework)
 
-// 转换为CGFloat
+/// 转换为CGFloat
 - (CGFloat)fwCGFloatValue;
 
-// 小数展示，去掉末尾0，最多digit位
+/// 小数展示，四舍五入，去掉末尾0，最多digit位，示例：12345.6789 => 12345.68
 - (NSString *)fwDigitString:(NSInteger)digit;
 
-// 百分比展示，最多digit位
+/// 逗号展示，四舍五入，去掉末尾0，最多digit位，示例：12345.6789 => 12,345.68
+- (NSString *)fwDecimalString:(NSInteger)digit;
+
+/// 百分比展示，逗号格式，四舍五入，去掉末尾0，最多digit位，示例：12345.6789 => 1,234,567.89%
 - (NSString *)fwPercentString:(NSInteger)digit;
 
-// 四舍五入，最多digit位
+/// 格式化为字符串，去掉末尾0，最多digit位，指定格式样式和取舍模式
+- (NSString *)fwFormatString:(NSInteger)digit
+                 numberStyle:(NSNumberFormatterStyle)numberStyle
+                roundingMode:(NSNumberFormatterRoundingMode)roundingMode;
+
+/// 四舍五入，去掉末尾0，最多digit位，示例：12345.6789 => 12345.68
 - (NSNumber *)fwRoundNumber:(NSUInteger)digit;
 
-// 取上整，最多digit位
+/// 取上整，去掉末尾0，最多digit位，示例：12345.6789 => 12345.68
 - (NSNumber *)fwCeilNumber:(NSUInteger)digit;
 
-// 取下整，最多digit位
+/// 取下整，去掉末尾0，最多digit位，示例：12345.6789 => 12345.67
 - (NSNumber *)fwFloorNumber:(NSUInteger)digit;
+
+/// 格式化数字，去掉末尾0，最多digit位，指定取舍模式
+- (NSNumber*)fwFormatNumber:(NSUInteger)digit
+               roundingMode:(NSNumberFormatterRoundingMode)roundingMode;
 
 @end
 
