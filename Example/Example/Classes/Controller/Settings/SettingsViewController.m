14cdb70915bd3f4d62de5992739809fb0288f349
wuyong <lingshizhuangzi@gmail.com>
2020-09-28 00:16:26 +0800

    fix

--- Example/Example/Classes/Controller/Settings/SettingsViewController.m
+++ Example/Example/Classes/Controller/Settings/SettingsViewController.m
@@ -10,6 +10,9 @@
 
 @interface SettingsViewController ()
 
+@property (nonatomic, strong) CADisplayLink *displayLink;
+@property (nonatomic, strong) UIImageView *screenView;
+
 @end
 
 @implementation SettingsViewController
@@ -31,6 +34,11 @@
     [button3 fwAddTouchTarget:self action:@selector(onLanguage)];
     [self.view addSubview:button3];
     button3.fwLayoutChain.centerX().topToBottomOfViewWithOffset(button2, 50);
+    
+    UIButton *button4 = [UIButton fwButtonWithFont:[UIFont appFontBoldNormal] titleColor:[UIColor appColorBlackOpacityHuge] title:@"开始截屏"];
+    [button4 fwAddTouchTarget:self action:@selector(onScreenButton:)];
+    [self.view addSubview:button4];
+    button4.fwLayoutChain.centerX().topToBottomOfViewWithOffset(button3, 50);
 }
 
 - (void)onLogout
@@ -63,6 +71,61 @@
         NSBundle.fwLocalizedLanguage = language;
         [AppRouter refreshController];
     }];
+}
+
+- (void)onScreenButton:(UIButton *)sender
+{
+    static dispatch_queue_t screenQueue;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        screenQueue = dispatch_queue_create("ScreenQueue", NULL);
+    });
+    
+    if ([@"开始截屏" isEqualToString:[sender titleForState:UIControlStateNormal]]) {
+        [sender setTitle:@"停止截屏" forState:UIControlStateNormal];
+        
+        self.screenView = [UIImageView new];
+        self.screenView.backgroundColor = [UIColor whiteColor];
+        [self.screenView fwSetBorderColor:[UIColor appColorBorder] width:0.5 cornerRadius:5];
+        [[UIWindow fwMainWindow] addSubview:self.screenView];
+        self.screenView.fwLayoutChain.rightWithInset(10).bottomWithInset(FWBottomBarHeight + 10).size(CGSizeMake(100, 100.0 / FWScreenWidth * FWScreenHeight));
+        
+        FWWeakifySelf();
+        self.displayLink = [CADisplayLink fwCommonDisplayLinkWithBlock:^(CADisplayLink * _Nonnull displayLink) {
+            FWStrongifySelf();
+            
+            dispatch_async(screenQueue, ^{
+                UIImage *image = [self onScreenShot];
+                if (image != nil) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        self.screenView.image = image;
+                    });
+                }
+            });
+        }];
+    } else {
+        [sender setTitle:@"开始截屏" forState:UIControlStateNormal];
+        
+        [self.displayLink invalidate];
+        self.displayLink = nil;
+        
+        [self.screenView removeFromSuperview];
+        self.screenView = nil;
+    }
+}
+
+- (UIImage *)onScreenShot
+{
+    UIWindow *window = UIWindow.fwMainWindow;
+    UIGraphicsBeginImageContextWithOptions(window.bounds.size, NO, 0);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [window.layer performSelectorOnMainThread:@selector(renderInContext:) withObject:(__bridge id)context waitUntilDone:YES];
+    window.layer.contents = nil;
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
 }
 
 @end
