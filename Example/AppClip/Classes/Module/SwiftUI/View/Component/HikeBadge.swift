a5ce3e129f3cd3b2f36955e33ab89c9d963689b8
wuyong <lingshizhuangzi@gmail.com>
2020-10-15 12:32:00 +0800

    clip

---
+++ Example/AppClip/Classes/Module/SwiftUI/View/Component/HikeBadge.swift
@@ -0,0 +1,30 @@
+//
+//  HikeBadge.swift
+//  AppClip
+//
+//  Created by wuyong on 2020/8/28.
+//  Copyright © 2020 site.wuyong. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    var body: some View {
+        VStack(alignment: .center) {
+            BadgeView()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibility(label: Text("Badge for \(name)."))
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
