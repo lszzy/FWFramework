0902e06f566ce1d6330f5bcd34a8b428ad1bcc75
wuyong <lingshizhuangzi@gmail.com>
2020-10-15 22:34:55 +0800

    fix

--- FWFramework/Component/FWFramework+Component.h
+++ FWFramework/Component/FWFramework+Component.h
@@ -9,5 +9,39 @@
 #ifndef FWFramework_Component_h
 #define FWFramework_Component_h
 
+#import "FWCoroutine.h"
+#import "FWMacro.h"
+#import "FWMessage.h"
+#import "FWPromise.h"
+#import "FWProxy.h"
+#import "FWSwizzle.h"
+
+#import "FWLog.h"
+#import "FWPlugin.h"
+#import "FWRouter.h"
+#import "FWState.h"
+#import "FWTask.h"
+#import "FWTest.h"
+
+#import "FWDatabaseQueue.h"
+#import "FWCacheManager.h"
+#import "FWCoder.h"
+#import "FWJson.h"
+#import "FWHTTPSessionManager.h"
+#import "FWNetworkPrivate.h"
+#import "FWAsyncSocket.h"
+#import "FWAsyncUdpSocket.h"
+
+#import "FWAuthorize.h"
+#import "FWKeychain.h"
+#import "FWNotification.h"
+#import "FWLocation.h"
+
+#import "FWAutoLayout.h"
+#import "FWWebImage.h"
+#import "FWDevice.h"
+#import "FWLanguage.h"
+#import "FWTheme.h"
+#import "FWVersion.h"
 
 #endif /* FWFramework_Component_h */
