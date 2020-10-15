8f459dd8942b033929d423c978855558e62de787
wuyong <lingshizhuangzi@gmail.com>
2020-10-15 12:32:00 +0800

    clip

---
+++ Example/AppClip/Classes/Module/SwiftUI/Model/Profile.swift
@@ -0,0 +1,32 @@
+//
+//  Profile.swift
+//  AppClip
+//
+//  Created by wuyong on 2020/8/28.
+//  Copyright © 2020 site.wuyong. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications: Bool
+    var seasonalPhoto: Season
+    var goalDate: Date
+    
+    static let `default` = Self(username: "g_kumar", prefersNotifications: true, seasonalPhoto: .winter)
+    
+    init(username: String, prefersNotifications: Bool = true, seasonalPhoto: Season = .winter) {
+        self.username = username
+        self.prefersNotifications = prefersNotifications
+        self.seasonalPhoto = seasonalPhoto
+        self.goalDate = Date()
+    }
+    
+    enum Season: String, CaseIterable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+    }
+}
---
+++ Example/AppClip/Classes/Module/SwiftUI/View/ProfileSummary.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileSummary.swift
+//  AppClip
+//
+//  Created by wuyong on 2020/8/28.
+//  Copyright © 2020 site.wuyong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var profile: Profile
+    
+    static let goalFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    var body: some View {
+        List {
+            Text(profile.username)
+                .bold()
+                .font(.title)
+            
+            Text("Notifications: \(self.profile.prefersNotifications ? "On" : "Off")")
+            Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
+            Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
+            
+            VStack(alignment: .leading) {
+                Text("Completed Badges")
+                    .font(.headline)
+                ScrollView {
+                    HStack {
+                        HikeBadge(name: "First Hike")
+                        HikeBadge(name: "Earth Day")
+                            .hueRotation(Angle(degrees: 90))
+                        HikeBadge(name: "Tenth Hike")
+                            .grayscale(0.5)
+                            .hueRotation(Angle(degrees: 45))
+                    }
+                }
+                .frame(height: 140)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Recent Hikes")
+                    .font(.headline)
+                
+                HikeView(hike: hikeData[0])
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+    }
+}
