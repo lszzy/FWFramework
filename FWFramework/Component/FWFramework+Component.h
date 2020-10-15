79d591c9feea7ca82ffb3f0a02aadd5e659250e0
wuyong <lingshizhuangzi@gmail.com>
2020-10-15 16:26:27 +0800

    fix

---
+++ FWFramework/Component/FWFramework+Component.h
@@ -0,0 +1,13 @@
+//
+//  FWFramework+Component.h
+//  FWFramework
+//
+//  Created by wuyong on 2020/10/15.
+//  Copyright © 2020 wuyong.site. All rights reserved.
+//
+
+#ifndef FWFramework_Component_h
+#define FWFramework_Component_h
+
+
+#endif /* FWFramework_Component_h */
