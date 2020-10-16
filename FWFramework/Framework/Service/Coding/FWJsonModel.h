ad6e0591027381375137fe5813a2030842c0e376
wuyong <lingshizhuangzi@gmail.com>
2020-10-16 23:03:50 +0800

    fix

--- FWFramework/Framework/Service/Coding/FWJsonModel.h
+++ FWFramework/Framework/Service/Coding/FWJsonModel.h
@@ -1,7 +1,7 @@
 /*!
- @header     FWJson.h
+ @header     FWJsonModel.h
  @indexgroup FWFramework
- @brief      FWJson
+ @brief      FWJsonModel
  @author     wuyong
  @copyright  Copyright © 2018 wuyong.site. All rights reserved.
  @updated    2018/9/26
