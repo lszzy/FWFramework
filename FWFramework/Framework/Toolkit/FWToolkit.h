e92e385b6f41931d5437fa701089ed90e36d1f84
wuyong <lingshizhuangzi@gmail.com>
2020-10-22 17:18:51 +0800

    fix

---
+++ FWFramework/Framework/Toolkit/FWToolkit.h
@@ -0,0 +1,138 @@
+/*!
+ @header     FWToolkit.h
+ @indexgroup FWFramework
+ @brief      FWToolkit
+ @author     wuyong
+ @copyright  Copyright © 2020 wuyong.site. All rights reserved.
+ @updated    2020/10/22
+ */
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark - UIApplication+FWToolkit
+
+/*!
+ @brief UIApplication+FWToolkit
+ @discussion 注意Info.plist文件URL SCHEME配置项只影响canOpenUrl方法，不影响openUrl。微信返回app就是获取sourceUrl，直接openUrl实现。因为跳转微信的时候，来源app肯定已打开过，可以跳转，只要不检查canOpenUrl，就可以跳转回app
+ */
+@interface UIApplication (FWToolkit)
+
+// 能否打开URL(NSString|NSURL)，需配置对应URL SCHEME到Info.plist才能返回YES
++ (BOOL)fwCanOpenURL:(id)url;
+
+// 打开URL，支持NSString|NSURL，即使未配置URL SCHEME，实际也能打开成功，只要调用时已打开过对应App
++ (void)fwOpenURL:(id)url;
+
+// 打开URL，支持NSString|NSURL，完成时回调，即使未配置URL SCHEME，实际也能打开成功，只要调用时已打开过对应App
++ (void)fwOpenURL:(id)url completionHandler:(nullable void (^)(BOOL success))completion;
+
+// 打开通用链接URL，支持NSString|NSURL，完成时回调。如果是iOS10+通用链接且安装了App，打开并回调YES，否则回调NO
++ (void)fwOpenUniversalLinks:(id)url completionHandler:(nullable void (^)(BOOL success))completion;
+
+// 打开AppStore下载页
++ (void)fwOpenAppStore:(NSString *)appId;
+
+// 判断URL是否是AppStore链接，支持NSString|NSURL
++ (BOOL)fwIsAppStoreURL:(id)url;
+
+// 判断URL是否是系统链接(如AppStore|电话|设置等)，支持NSString|NSURL
++ (BOOL)fwIsSystemURL:(id)url;
+
+// 判断URL是否HTTP链接，支持NSString|NSURL
++ (BOOL)fwIsHttpURL:(id)url;
+
+// 打开内部浏览器，支持NSString|NSURL
++ (void)fwOpenSafariController:(id)url;
+
+// 打开内部浏览器，支持NSString|NSURL，点击完成时回调
++ (void)fwOpenSafariController:(id)url completionHandler:(nullable void (^)(void))completion;
+
+@end
+
+#pragma mark - UIColor+FWToolkit
+
+/// 从16进制创建UIColor，格式0xFFFFFF，透明度可选，默认1.0
+#define FWColorHex( hex, ... ) \
+    [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16))/255.0 green:((float)((hex & 0xFF00) >> 8))/255.0 blue:((float)(hex & 0xFF))/255.0 alpha:fw_macro_default(1.0, ##__VA_ARGS__)]
+
+/// 从RGB创建UIColor，透明度可选，默认1.0
+#define FWColorRgb( r, g, b, ... ) \
+    [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:fw_macro_default(1.0f, ##__VA_ARGS__)]
+
+/*!
+ @brief UIColor+FWToolkit
+ */
+@interface UIColor (FWToolkit)
+
+/// 从十六进制值初始化，格式：0x20B2AA，透明度为1.0
++ (UIColor *)fwColorWithHex:(long)hex;
+
+/// 从十六进制值初始化，格式：0x20B2AA，自定义透明度
++ (UIColor *)fwColorWithHex:(long)hex alpha:(CGFloat)alpha;
+
+/// 设置十六进制颜色标准为ARGB|RGBA，启用为ARGB，默认为RGBA
++ (void)fwColorStandardARGB:(BOOL)enabled;
+
+/// 从十六进制字符串初始化，支持RGB、RGBA|ARGB，格式：@"20B2AA", @"#FFFFFF"，透明度为1.0，失败时返回clear
++ (UIColor *)fwColorWithHexString:(NSString *)hexString;
+
+/// 从十六进制字符串初始化，支持RGB、RGBA|ARGB，格式：@"20B2AA", @"#FFFFFF"，自定义透明度，失败时返回clear
++ (UIColor *)fwColorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha;
+
+/// 从颜色字符串初始化，支持十六进制和颜色值，透明度为1.0，失败时返回clear
++ (UIColor *)fwColorWithString:(NSString *)string;
+
+/// 从颜色字符串初始化，支持十六进制和颜色值，自定义透明度，失败时返回clear
++ (UIColor *)fwColorWithString:(NSString *)string alpha:(CGFloat)alpha;
+
+/// 读取颜色的十六进制值RGB，不含透明度
+- (long)fwHexValue;
+
+/// 读取颜色的透明度值，范围0~1
+- (CGFloat)fwAlphaValue;
+
+/// 读取颜色的十六进制字符串RGB，不含透明度
+- (NSString *)fwHexString;
+
+/// 读取颜色的十六进制字符串RGBA|ARGB(透明度为1时RGB)，包含透明度
+- (NSString *)fwHexStringWithAlpha;
+
+@end
+
+#pragma mark - UIFont+FWToolkit
+
+/// 快速创建系统字体，字重可选，默认Regular
+#define FWFontSize( size, ... ) [UIFont fwFontOfSize:size weight:fw_macro_default(UIFontWeightRegular, ##__VA_ARGS__)]
+
+/// 快速创建细字体
+FOUNDATION_EXPORT UIFont * FWFontLight(CGFloat size);
+/// 快速创建普通字体
+FOUNDATION_EXPORT UIFont * FWFontRegular(CGFloat size);
+/// 快速创建粗体字体
+FOUNDATION_EXPORT UIFont * FWFontBold(CGFloat size);
+/// 快速创建斜体字体
+FOUNDATION_EXPORT UIFont * FWFontItalic(CGFloat size);
+
+/*!
+ @brief UIFont+FWToolkit
+ */
+@interface UIFont (FWToolkit)
+
+/// 返回系统字体的细体
++ (UIFont *)fwLightFontOfSize:(CGFloat)size;
+/// 返回系统字体的普通体
++ (UIFont *)fwFontOfSize:(CGFloat)size;
+/// 返回系统字体的粗体
++ (UIFont *)fwBoldFontOfSize:(CGFloat)size;
+/// 返回系统字体的斜体
++ (UIFont *)fwItalicFontOfSize:(CGFloat)size;
+
+/// 创建指定尺寸和weight的系统字体
++ (UIFont *)fwFontOfSize:(CGFloat)size weight:(UIFontWeight)weight;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
---
+++ FWFramework/Framework/Toolkit/FWToolkit.m
@@ -0,0 +1,340 @@
+/*!
+ @header     FWToolkit.m
+ @indexgroup FWFramework
+ @brief      FWToolkit
+ @author     wuyong
+ @copyright  Copyright © 2020 wuyong.site. All rights reserved.
+ @updated    2020/10/22
+ */
+
+#import "FWToolkit.h"
+#import "FWEncode.h"
+#import "FWRouter.h"
+#import <SafariServices/SafariServices.h>
+#import <objc/runtime.h>
+
+#pragma mark - UIApplication+FWToolkit
+
+@interface FWSafariViewControllerDelegate : NSObject <SFSafariViewControllerDelegate>
+
+@end
+
+@implementation FWSafariViewControllerDelegate
+
++ (FWSafariViewControllerDelegate *)sharedInstance
+{
+    static FWSafariViewControllerDelegate *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[FWSafariViewControllerDelegate alloc] init];
+    });
+    return instance;
+}
+
+- (void)safariViewControllerDidFinish:(SFSafariViewController *)controller
+{
+    void (^completion)(void) = objc_getAssociatedObject(controller, @selector(safariViewControllerDidFinish:));
+    if (completion) {
+        completion();
+    }
+}
+
+@end
+
+@implementation UIApplication (FWToolkit)
+
++ (BOOL)fwCanOpenURL:(id)url
+{
+    NSURL *nsurl = [url isKindOfClass:[NSString class]] ? [NSURL fwURLWithString:url] : url;
+    return [[UIApplication sharedApplication] canOpenURL:nsurl];
+}
+
++ (void)fwOpenURL:(id)url
+{
+    [self fwOpenURL:url completionHandler:nil];
+}
+
++ (void)fwOpenURL:(id)url completionHandler:(void (^)(BOOL success))completion
+{
+    NSURL *nsurl = [url isKindOfClass:[NSString class]] ? [NSURL fwURLWithString:url] : url;
+    if (@available(iOS 10.0, *)) {
+        [[UIApplication sharedApplication] openURL:nsurl options:@{} completionHandler:completion];
+    } else {
+        BOOL success = [[UIApplication sharedApplication] openURL:nsurl];
+        if (completion) {
+            completion(success);
+        }
+    }
+}
+
++ (void)fwOpenUniversalLinks:(id)url completionHandler:(void (^)(BOOL))completion
+{
+    NSURL *nsurl = [url isKindOfClass:[NSString class]] ? [NSURL fwURLWithString:url] : url;
+    if (@available(iOS 10.0, *)) {
+        [[UIApplication sharedApplication] openURL:nsurl options:@{UIApplicationOpenURLOptionUniversalLinksOnly: @YES} completionHandler:completion];
+    } else {
+        if (completion) {
+            completion(NO);
+        }
+    }
+}
+
++ (void)fwOpenAppStore:(NSString *)appId
+{
+    // SKStoreProductViewController可以内部打开，但需要加载
+    [self fwOpenURL:[NSString stringWithFormat:@"https://itunes.apple.com/app/id%@", appId]];
+}
+
++ (BOOL)fwIsAppStoreURL:(id)url
+{
+    // itms-apps等
+    NSURL *nsurl = [url isKindOfClass:[NSString class]] ? [NSURL fwURLWithString:url] : url;
+    if ([nsurl.scheme.lowercaseString hasPrefix:@"itms"]) {
+        return YES;
+    // https://itunes.apple.com/等
+    } else if ([nsurl.host.lowercaseString isEqualToString:@"itunes.apple.com"] ||
+               [nsurl.host.lowercaseString isEqualToString:@"apps.apple.com"]) {
+        return YES;
+    }
+    return NO;
+}
+
++ (BOOL)fwIsSystemURL:(id)url
+{
+    NSURL *nsurl = [url isKindOfClass:[NSString class]] ? [NSURL fwURLWithString:url] : url;
+    if (nsurl.scheme.lowercaseString && [@[@"tel", @"telprompt", @"sms", @"mailto"] containsObject:nsurl.scheme.lowercaseString]) {
+        return YES;
+    }
+    if ([self fwIsAppStoreURL:nsurl]) {
+        return YES;
+    }
+    if (nsurl.absoluteString && [nsurl.absoluteString isEqualToString:UIApplicationOpenSettingsURLString]) {
+        return YES;
+    }
+    return NO;
+}
+
++ (BOOL)fwIsHttpURL:(id)url
+{
+    NSString *urlString = [url isKindOfClass:[NSURL class]] ? [(NSURL *)url absoluteString] : url;
+    return [urlString.lowercaseString hasPrefix:@"http://"] || [urlString.lowercaseString hasPrefix:@"https://"];
+}
+
++ (void)fwOpenSafariController:(id)url
+{
+    [self fwOpenSafariController:url completionHandler:nil];
+}
+
++ (void)fwOpenSafariController:(id)url completionHandler:(nullable void (^)(void))completion
+{
+    NSURL *nsurl = [url isKindOfClass:[NSString class]] ? [NSURL fwURLWithString:url] : url;
+    SFSafariViewController *safariController = [[SFSafariViewController alloc] initWithURL:nsurl];
+    if (completion) {
+        objc_setAssociatedObject(safariController, @selector(safariViewControllerDidFinish:), completion, OBJC_ASSOCIATION_COPY_NONATOMIC);
+        safariController.delegate = [FWSafariViewControllerDelegate sharedInstance];
+    }
+    [UIWindow.fwMainWindow fwPresentViewController:safariController animated:YES completion:nil];
+}
+
+@end
+
+#pragma mark - UIColor+FWToolkit
+
+static BOOL fwStaticColorARGB = NO;
+
+@implementation UIColor (FWToolkit)
+
++ (UIColor *)fwColorWithHex:(long)hex
+{
+    return [UIColor fwColorWithHex:hex alpha:1.0f];
+}
+
++ (UIColor *)fwColorWithHex:(long)hex alpha:(CGFloat)alpha
+{
+    float red = ((float)((hex & 0xFF0000) >> 16)) / 255.0;
+    float green = ((float)((hex & 0xFF00) >> 8)) / 255.0;
+    float blue = ((float)(hex & 0xFF)) / 255.0;
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
++ (void)fwColorStandardARGB:(BOOL)enabled
+{
+    fwStaticColorARGB = enabled;
+}
+
++ (UIColor *)fwColorWithHexString:(NSString *)hexString
+{
+    return [UIColor fwColorWithHexString:hexString alpha:1.0f];
+}
+
++ (UIColor *)fwColorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha
+{
+    // 处理参数
+    NSString *string = hexString ? hexString.uppercaseString : @"";
+    if ([string hasPrefix:@"0X"]) {
+        string = [string substringFromIndex:2];
+    }
+    if ([string hasPrefix:@"#"]) {
+        string = [string substringFromIndex:1];
+    }
+    
+    // 检查长度
+    NSUInteger length = string.length;
+    if (length != 3 && length != 4 && length != 6 && length != 8) {
+        return [UIColor clearColor];
+    }
+    
+    // 解析颜色
+    NSString *strR = nil, *strG = nil, *strB = nil, *strA = nil;
+    if (length < 5) {
+        // ARGB
+        if (fwStaticColorARGB && length == 4) {
+            string = [NSString stringWithFormat:@"%@%@", [string substringWithRange:NSMakeRange(1, 3)], [string substringWithRange:NSMakeRange(0, 1)]];
+        }
+        // RGB|RGBA
+        NSString *tmpR = [string substringWithRange:NSMakeRange(0, 1)];
+        NSString *tmpG = [string substringWithRange:NSMakeRange(1, 1)];
+        NSString *tmpB = [string substringWithRange:NSMakeRange(2, 1)];
+        strR = [NSString stringWithFormat:@"%@%@", tmpR, tmpR];
+        strG = [NSString stringWithFormat:@"%@%@", tmpG, tmpG];
+        strB = [NSString stringWithFormat:@"%@%@", tmpB, tmpB];
+        if (length == 4) {
+            NSString *tmpA = [string substringWithRange:NSMakeRange(3, 1)];
+            strA = [NSString stringWithFormat:@"%@%@", tmpA, tmpA];
+        }
+    } else {
+        // AARRGGBB
+        if (fwStaticColorARGB && length == 8) {
+            string = [NSString stringWithFormat:@"%@%@", [string substringWithRange:NSMakeRange(2, 6)], [string substringWithRange:NSMakeRange(0, 2)]];
+        }
+        // RRGGBB|RRGGBBAA
+        strR = [string substringWithRange:NSMakeRange(0, 2)];
+        strG = [string substringWithRange:NSMakeRange(2, 2)];
+        strB = [string substringWithRange:NSMakeRange(4, 2)];
+        if (length == 8) {
+            strA = [string substringWithRange:NSMakeRange(6, 2)];
+        }
+    }
+    
+    // 解析颜色
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:strR] scanHexInt:&r];
+    [[NSScanner scannerWithString:strG] scanHexInt:&g];
+    [[NSScanner scannerWithString:strB] scanHexInt:&b];
+    float fr = (r * 1.0f) / 255.0f;
+    float fg = (g * 1.0f) / 255.0f;
+    float fb = (b * 1.0f) / 255.0f;
+    
+    // 解析透明度，字符串的透明度优先级高于alpha参数
+    if (strA) {
+        unsigned int a;
+        [[NSScanner scannerWithString:strA] scanHexInt:&a];
+        // 计算十六进制对应透明度
+        alpha = (a * 1.0f) / 255.0f;
+    }
+    
+    return [UIColor colorWithRed:fr green:fg blue:fb alpha:alpha];
+}
+
++ (UIColor *)fwColorWithString:(NSString *)string
+{
+    return [UIColor fwColorWithString:string alpha:1.0f];
+}
+
++ (UIColor *)fwColorWithString:(NSString *)string alpha:(CGFloat)alpha
+{
+    // 颜色值
+    SEL colorSelector = NSSelectorFromString([NSString stringWithFormat:@"%@Color", string]);
+    if ([[UIColor class] respondsToSelector:colorSelector]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        UIColor *color = [[UIColor class] performSelector:colorSelector];
+#pragma clang diagnostic pop
+        return alpha < 1.0f ? [color colorWithAlphaComponent:alpha] : color;
+    }
+    
+    // 十六进制
+    return [UIColor fwColorWithHexString:string alpha:alpha];
+}
+
+- (long)fwHexValue
+{
+    CGFloat r = 0, g = 0, b = 0, a = 0;
+    if (![self getRed:&r green:&g blue:&b alpha:&a]) {
+        if ([self getWhite:&r alpha:&a]) { g = r; b = r; }
+    }
+    
+    int8_t red = r * 255;
+    uint8_t green = g * 255;
+    uint8_t blue = b * 255;
+    return (red << 16) + (green << 8) + blue;
+}
+
+- (CGFloat)fwAlphaValue
+{
+    return CGColorGetAlpha(self.CGColor);
+}
+
+- (NSString *)fwHexString
+{
+    CGFloat r = 0, g = 0, b = 0, a = 0;
+    if (![self getRed:&r green:&g blue:&b alpha:&a]) {
+        if ([self getWhite:&r alpha:&a]) { g = r; b = r; }
+    }
+    
+    return [NSString stringWithFormat:@"#%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255)];
+}
+
+- (NSString *)fwHexStringWithAlpha
+{
+    CGFloat r = 0, g = 0, b = 0, a = 0;
+    if (![self getRed:&r green:&g blue:&b alpha:&a]) {
+        if ([self getWhite:&r alpha:&a]) { g = r; b = r; }
+    }
+    
+    if (a >= 1.0) {
+        return [NSString stringWithFormat:@"#%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255)];
+    } else if (fwStaticColorARGB) {
+        return [NSString stringWithFormat:@"#%02lX%02lX%02lX%02lX", lround(a * 255), lroundf(r * 255), lroundf(g * 255), lroundf(b * 255)];
+    } else {
+        return [NSString stringWithFormat:@"#%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lround(a * 255)];
+    }
+}
+
+@end
+
+#pragma mark - UIFont+FWToolkit
+
+UIFont * FWFontLight(CGFloat size) { return [UIFont fwLightFontOfSize:size]; }
+UIFont * FWFontRegular(CGFloat size) { return [UIFont fwFontOfSize:size]; }
+UIFont * FWFontBold(CGFloat size) { return [UIFont fwBoldFontOfSize:size]; }
+UIFont * FWFontItalic(CGFloat size) { return [UIFont fwItalicFontOfSize:size]; }
+
+@implementation UIFont (FWToolkit)
+
++ (UIFont *)fwLightFontOfSize:(CGFloat)size
+{
+    return [UIFont systemFontOfSize:size weight:UIFontWeightLight];
+}
+
++ (UIFont *)fwFontOfSize:(CGFloat)size
+{
+    return [UIFont systemFontOfSize:size];
+}
+
++ (UIFont *)fwBoldFontOfSize:(CGFloat)size
+{
+    return [UIFont boldSystemFontOfSize:size];
+}
+
++ (UIFont *)fwItalicFontOfSize:(CGFloat)size
+{
+    return [UIFont italicSystemFontOfSize:size];
+}
+
++ (UIFont *)fwFontOfSize:(CGFloat)size weight:(UIFontWeight)weight
+{
+    return [UIFont systemFontOfSize:size weight:weight];
+}
+
+@end
