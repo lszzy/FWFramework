5ce482ef224059e54bf103ffd76892853af733c2
wuyong <lingshizhuangzi@gmail.com>
2020-11-12 00:34:43 +0800

    fix

--- Example/AppClip/Classes/Module/SwiftUI/View/CategoryRow.swift
+++ Example/AppClip/Classes/Module/SwiftUI/View/CategoryRow.swift
@@ -41,7 +41,7 @@ struct CategoryItem: View {
     var landmark: Landmark
     var body: some View {
         VStack(alignment: .leading) {
-            FWWebImage()
+            FWImageWrapper()
                 .url("https://picsum.photos/50/50?i=\(arc4random_uniform(30) + 1)")
                 .placeholder(FWImageFile("test.webp"))
             //landmark.image
