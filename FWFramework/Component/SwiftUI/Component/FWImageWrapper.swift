a1bd5e2f2ee0354173ba3d58e5933c6732ffd9ca
wuyong <lingshizhuangzi@gmail.com>
2020-11-12 13:18:28 +0800

    fix

--- FWFramework/Component/SwiftUI/Component/FWImageWrapper.swift
+++ FWFramework/Component/SwiftUI/Component/FWImageWrapper.swift
@@ -13,37 +13,36 @@ import SwiftUI
 /// SwiftUI动图、网络图片包装器
 @available(iOS 13.0, *)
 public struct FWImageWrapper: UIViewRepresentable {
-    var image: UIImage?
+    
     var url: Any?
     var placeholder: UIImage?
     var contentMode: UIView.ContentMode = .scaleAspectFill
     
-    public init(_ image: UIImage? = nil) {
-        self.image = image
+    /// 指定本地占位图片初始化
+    public init(_ placeholder: UIImage? = nil) {
+        self.placeholder = placeholder
     }
     
+    /// 指定网络图片URL初始化
     public init(url: Any?) {
         self.url = url
     }
     
-    public func image(_ image: UIImage?) -> FWImageWrapper {
-        var result = self
-        result.image = image
-        return result
-    }
-    
+    /// 设置网络图片URL
     public func url(_ url: Any?) -> FWImageWrapper {
         var result = self
         result.url = url
         return result
     }
     
+    /// 设置本地占位图片
     public func placeholder(_ placeholder: UIImage?) -> FWImageWrapper {
         var result = self
         result.placeholder = placeholder
         return result
     }
     
+    /// 设置图片显示内容模式，默认scaleAspectFill
     public func contentMode(_ contentMode: UIView.ContentMode) -> FWImageWrapper {
         var result = self
         result.contentMode = contentMode
@@ -58,11 +57,7 @@ public struct FWImageWrapper: UIViewRepresentable {
         let imageClass = UIImageView.fwImageViewAnimatedClass as! UIImageView.Type
         let imageView = imageClass.init()
         imageView.contentMode = contentMode
-        if image != nil {
-            imageView.image = image
-        } else {
-            imageView.fwSetImage(withURL: url, placeholderImage: placeholder)
-        }
+        imageView.fwSetImage(withURL: url, placeholderImage: placeholder)
         return imageView
     }
     
