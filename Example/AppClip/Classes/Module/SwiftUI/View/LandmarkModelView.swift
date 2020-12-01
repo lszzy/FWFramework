a4145645939d8ee7dd379a8c447e7806388b758c
wuyong <lingshizhuangzi@gmail.com>
2020-12-01 23:41:48 +0800

    fix

--- Example/AppClip/Classes/Module/SwiftUI/View/LandmarkModelView.swift
+++ Example/AppClip/Classes/Module/SwiftUI/View/LandmarkModelView.swift
@@ -11,70 +11,18 @@ import SwiftUI
 struct LandmarkModelView: View {
     @ObservedObject var viewModel: LandmarkViewModel = LandmarkViewModel()
     
-    @State private var headerRefreshing: Bool = false
-    @State private var footerRefreshing: Bool = false
-    @State private var noMore: Bool = false
-    
     var body: some View {
-        ScrollView {
-            if viewModel.items.count > 0 {
-                RefreshHeader(refreshing: $headerRefreshing, action: {
-                    self.reload()
-                }) { progress in
-                    if self.headerRefreshing {
-                        ProgressView()
-                    } else {
-                        Text("Pull to refresh")
-                    }
+        NavigationView {
+            VStack {
+                switch viewModel.state {
+                default:
+                    ProgressView()
+                        .padding()
                 }
-            }
-            
-            ForEach(viewModel.items, id: \.self) { item in
-                Text(item)
-                    .frame(height: 500)
-            }
-             
-            if viewModel.items.count > 0 {
-                RefreshFooter(refreshing: $footerRefreshing, action: {
-                    self.loadMore()
-                }) {
-                    if self.noMore {
-                        Text("No more data !")
-                            .foregroundColor(.secondary)
-                            .padding()
-                    } else {
-                        ProgressView()
-                            .padding()
-                    }
-                }
-                .noMore(noMore)
-                .preload(offset: 50)
             }
         }
-        .enableRefresh()
-        .overlay(Group {
-            if viewModel.items.count == 0 {
-                ProgressView()
-            } else {
-                EmptyView()
-            }
-        })
         .onAppear {
-            self.reload()
-        }
-    }
-    
-    func reload() {
-        viewModel.refreshData {
-            self.headerRefreshing = false
-            self.noMore = false
-        }
-    }
-    
-    func loadMore() {
-        viewModel.loadData {
-            self.footerRefreshing = false
-            self.noMore = self.viewModel.items.count > 10
+            self.viewModel.onRefreshing()
         }
     }
 }
---
+++ FWFramework/Framework/Toolkit/FWHelper.swift
@@ -0,0 +1,33 @@
+//
+//  FWHelper.swift
+//  FWFramework
+//
+//  Created by wuyong on 2020/12/1.
+//  Copyright © 2020 wuyong.site. All rights reserved.
+//
+
+import Foundation
+
+/// UserDefault属性包装器
+/// 使用示例：@FWUserDefault("userName", defaultValue: "test")
+/// public static var userName: String
+@propertyWrapper
+public struct FWUserDefault<T: Any> {
+    let key: String
+    let defaultValue: T
+    
+    public init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    public var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+            UserDefaults.standard.synchronize()
+        }
+    }
+}
