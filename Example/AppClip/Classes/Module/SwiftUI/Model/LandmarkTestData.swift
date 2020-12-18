96519eae3c0459c8e37ca8be780a1bde2f944a85
wuyong <lingshizhuangzi@gmail.com>
2020-12-19 01:59:43 +0800

    fix

--- Example/AppClip/Classes/Module/SwiftUI/Model/LandmarkTestData.swift
+++ Example/AppClip/Classes/Module/SwiftUI/Model/LandmarkTestData.swift
@@ -13,9 +13,25 @@ struct LandmarkTestData: Codable {
     var name: String = ""
     var info: LandmarkTestInfo = LandmarkTestInfo()
     var infos: [LandmarkTestInfo] = []
+    
+    init() {}
+    
+    init(from decoder: Decoder) throws {
+        id = try decoder.fwDecodeJson("id").intValue
+        name = try decoder.fwDecodeJson("name").stringValue
+        info = try decoder.fwDecode("info")
+        infos = try decoder.fwDecode("infos")
+    }
 }
 
 struct LandmarkTestInfo: Codable {
     var id: Int = 0
     var title: String = ""
+    
+    init() {}
+    
+    init(from decoder: Decoder) throws {
+        id = try decoder.fwDecodeJson("id").intValue
+        title = try decoder.fwDecodeJson("name").stringValue
+    }
 }
