30bbd2f516d0a83a8c656039d9695be3e24cc0ec
wuyong <lingshizhuangzi@gmail.com>
2020-12-21 23:03:52 +0800

    fix

--- FWFramework/Component/UIKit/Component/FWAsyncLayer.h
+++ FWFramework/Component/UIKit/Component/FWAsyncLayer.h
@@ -24,7 +24,6 @@
 #import "FWProgressView.h"
 #import "FWQrcodeScanView.h"
 #import "FWSegmentedControl.h"
-#import "FWTabBarController.h"
 #import "FWTagCollectionView.h"
 
 NS_ASSUME_NONNULL_BEGIN
