1092ed5a1811b62fedc6c075ab17b0b3f0d66bb9
wuyong <lingshizhuangzi@gmail.com>
2021-01-01 15:22:39 +0800

    fix

---
+++ Example/Mediator/Test/Example/Test/TESTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TESTViewController.h
+//  Test
+//
+//  Created by lingshizhuangzi@gmail.com on 01/01/2021.
+//  Copyright (c) 2021 lingshizhuangzi@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TESTViewController : UIViewController
+
+@end
