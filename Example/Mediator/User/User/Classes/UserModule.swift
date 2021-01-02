f82631ce66075a4363f9a76687550e02bbd5a0fa
wuyong <lingshizhuangzi@gmail.com>
2021-01-02 14:04:01 +0800

    fix

---
+++ Example/Mediator/User/User/Classes/UserModule.swift
@@ -0,0 +1,29 @@
+//
+//  UserModule.swift
+//  User
+//
+//  Created by wuyong on 2021/1/1.
+//
+
+import FWFramework
+import Mediator
+
+@objcMembers class UserModule: NSObject, FWModuleProtocol {
+    private static let shared: UserModule = UserModule()
+    
+    static func sharedInstance() -> Self {
+        return shared as! Self
+    }
+    
+    func setup() {
+        print("UserModule.setup")
+    }
+}
+
+extension UserModule: UserModuleService {
+    func login(_ completion: (() -> Void)?) {
+        let viewController = UserLoginViewController()
+        viewController.completion = completion
+        FWRouter.present(viewController, animated: true, completion: nil)
+    }
+}
