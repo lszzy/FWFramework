43544e13f0e7bf6b3d8b3e1a036e878818b253d9
wuyong <lingshizhuangzi@gmail.com>
2021-01-03 19:22:41 +0800

    fix

--- FWFramework/Application/App/FWAppDelegate.h
+++ FWFramework/Application/App/FWAppDelegate.h
@@ -29,26 +29,8 @@ NS_ASSUME_NONNULL_BEGIN
 /// 初始化应用配置，子类重写
 - (void)setupApplication:(UIApplication *)application options:(NSDictionary *)options;
 
-/// 初始化优先级高的服务，子类重写
-- (void)setupService;
-
-/// 初始化全局样式，子类重写
-- (void)setupAppearance;
-
 /// 初始化根控制器，子类重写
 - (void)setupController;
-
-/// 初始化优先级低的组件，子类重写
-- (void)setupComponent;
-
-/// 初始化设备token，失败时为error
-- (void)setupDeviceToken:(nullable NSData *)tokenData error:(nullable NSError *)error;
-
-/// 统一处理打开URL，默认包含通用链接
-- (BOOL)handleOpenURL:(NSURL *)url options:(nullable NSDictionary *)options;
-
-/// 统一处理通用链接，默认转发handleOpenURL
-- (BOOL)handleUserActivity:(NSUserActivity *)userActivity;
 
 @end
 
