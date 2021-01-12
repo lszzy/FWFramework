ec68951230329da413543dee17a3db7338a93934
wuyong <lingshizhuangzi@gmail.com>
2021-01-12 23:23:24 +0800

    优化NavigationBar、TabBar样式设置代码

--- Example/Example/Classes/Library/Base/BaseViewController.h
+++ Example/Example/Classes/Library/Base/BaseViewController.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-static const FWNavigationBarStyle FWNavigationBarStyleWhite = 2;
+static const FWNavigationBarStyle FWNavigationBarStyleRandom = 2;
 
 @interface BaseViewController : UIViewController <FWViewController>
 
