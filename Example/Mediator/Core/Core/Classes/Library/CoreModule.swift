a7bfa3905bbf481b3dbc625110458bdb6d52058c
wuyong <lingshizhuangzi@gmail.com>
2021-01-16 12:57:02 +0800

    Example项目调用Core模块

--- Example/Mediator/Core/Core/Classes/Library/CoreModule.swift
+++ Example/Mediator/Core/Core/Classes/Library/CoreModule.swift
@@ -1,59 +1,18 @@
 //
-//  UserModule.swift
-//  User
+//  CoreModule.swift
+//  Core
 //
 //  Created by wuyong on 2021/1/1.
 //
 
 import FWFramework
-import Mediator
 
-@objc extension FWAutoloader {
-    func loadUserModule() {
-        FWMediator.registerService(UserModuleService.self, withModule: UserModule.self)
-    }
-}
-
-@objcMembers public class UserBundle: FWModuleBundle {
+@objcMembers public class CoreBundle: FWModuleBundle {
     private static let sharedBundle: Bundle = {
-        return Bundle.fwBundle(with: UserBundle.classForCoder(), name: "User")?.fwLocalized() ?? .main
+        return Bundle.fwBundle(with: CoreBundle.classForCoder(), name: "Core")?.fwLocalized() ?? .main
     }()
     
     public override class func bundle() -> Bundle {
         return sharedBundle
-    }
-}
-
-@objcMembers public class UserModule: NSObject, UserModuleService {
-    private static let sharedModule = UserModule()
-    
-    @FWUserDefaultAnnotation("userId", defaultValue: "")
-    private var userId: String
-    
-    public static func sharedInstance() -> Self {
-        return sharedModule as! Self
-    }
-    
-    public func setup() {
-        FWLogDebug(#function)
-    }
-    
-    public func isLogin() -> Bool {
-        return userId.count > 0
-    }
-    
-    public func login(_ completion: (() -> Void)?) {
-        let viewController = UserLoginController()
-        viewController.completion = { [weak self] in
-            self?.userId = "1"
-            completion?()
-        }
-        let navigationController = UINavigationController(rootViewController: viewController)
-        FWRouter.present(navigationController, animated: true, completion: nil)
-    }
-    
-    public func logout(_ completion: (() -> Void)?) {
-        userId = ""
-        completion?()
     }
 }
