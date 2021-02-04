a8670d52710b5631fcd9c8bac8cf125436a73d9b
wuyong <lingshizhuangzi@gmail.com>
2021-02-05 01:34:03 +0800

    增加RSA加密解密，加签验签算法和测试Demo

--- FWFramework/Component/Foundation/Extension/NSData+FWFramework.h
+++ FWFramework/Component/Foundation/Extension/NSData+FWFramework.h
@@ -16,57 +16,57 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface NSData (FWFramework)
 
-// 使用NSKeyedArchiver压缩对象
+/// 使用NSKeyedArchiver压缩对象
 + (nullable NSData *)fwArchiveObject:(id)object;
 
-// 使用NSKeyedUnarchiver解压数据
+/// 使用NSKeyedUnarchiver解压数据
 - (nullable id)fwUnarchiveObject;
 
-// 保存对象归档
+/// 保存对象归档
 + (void)fwArchiveObject:(id)object toFile:(NSString *)path;
 
-// 读取对象归档
+/// 读取对象归档
 + (nullable id)fwUnarchiveObjectWithFile:(NSString *)path;
 
 #pragma mark - Encrypt
 
-/**
- *  利用AES加密数据
- *
- *  @param key key
- *  @param iv  iv description
- *
- *  @return data
- */
+/// 利用AES加密数据
 - (nullable NSData *)fwAESEncryptWithKey:(NSString *)key andIV:(NSData *)iv;
-/**
- *  @brief  利用AES解密据
- *
- *  @param key key
- *  @param iv  iv
- *
- *  @return 解密后数据
- */
+
+/// 利用AES解密数据
 - (nullable NSData *)fwAESDecryptWithKey:(NSString *)key andIV:(NSData *)iv;
 
-/**
- *  利用3DES加密数据
- *
- *  @param key key
- *  @param iv  iv description
- *
- *  @return data
- */
+/// 利用3DES加密数据
 - (nullable NSData *)fw3DESEncryptWithKey:(NSString *)key andIV:(NSData *)iv;
-/**
- *  @brief   利用3DES解密数据
- *
- *  @param key key
- *  @param iv  iv
- *
- *  @return 解密后数据
- */
+
+/// 利用3DES解密数据
 - (nullable NSData *)fw3DESDecryptWithKey:(NSString *)key andIV:(NSData *)iv;
+
+#pragma mark - RSA
+
+/// RSA公钥加密，使用默认标签，执行base64编码
+- (nullable NSData *)fwRSAEncryptWithPublicKey:(NSString *)publicKey;
+
+/// RSA公钥加密，可自定义标签，指定base64编码
+- (nullable NSData *)fwRSAEncryptWithPublicKey:(NSString *)publicKey andTag:(NSString *)tagName base64Encode:(BOOL)base64Encode;
+
+/// RSA私钥解密，使用默认标签，执行base64解密
+- (nullable NSData *)fwRSADecryptWithPrivateKey:(NSString *)privateKey;
+
+/// RSA私钥解密，可自定义标签，指定base64解码
+- (nullable NSData *)fwRSADecryptWithPrivateKey:(NSString *)privateKey andTag:(NSString *)tagName base64Decode:(BOOL)base64Decode;
+
+/// RSA私钥加签，使用默认标签，执行base64编码
+- (nullable NSData *)fwRSASignWithPrivateKey:(NSString *)privateKey;
+
+/// RSA私钥加签，可自定义标签，指定base64编码
+- (nullable NSData *)fwRSASignWithPrivateKey:(NSString *)privateKey andTag:(NSString *)tagName base64Encode:(BOOL)base64Encode;
+
+/// RSA公钥验签，使用默认标签，执行base64解密
+- (nullable NSData *)fwRSAVerifyWithPublicKey:(NSString *)publicKey;
+
+/// RSA公钥验签，可自定义标签，指定base64解码
+- (nullable NSData *)fwRSAVerifyWithPublicKey:(NSString *)publicKey andTag:(NSString *)tagName base64Decode:(BOOL)base64Decode;
 
 @end
 
