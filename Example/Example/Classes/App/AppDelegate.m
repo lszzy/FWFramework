07482ab85e3c754ba2b63017185beb661f4e744a
wuyong <lingshizhuangzi@gmail.com>
2021-02-09 16:13:23 +0800

    测试项目示例NotificationModule通知模块分离

--- Example/Example/Classes/App/AppDelegate.m
+++ Example/Example/Classes/App/AppDelegate.m
@@ -25,25 +25,6 @@
     
 }
 
-#pragma mark - Notification
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    [UIDevice fwSetDeviceTokenData:deviceToken];
-}
-
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    [UIDevice fwSetDeviceTokenData:nil];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-    [[FWNotificationManager sharedInstance] handleRemoteNotification:userInfo];
-    completionHandler(UIBackgroundFetchResultNewData);
-}
-
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-    [[FWNotificationManager sharedInstance] handleLocalNotification:notification];
-}
-
 #pragma mark - openURL
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
@@ -61,49 +42,6 @@
 }
 
 #pragma mark - Protected
-
-- (void)setupApplication:(UIApplication *)application options:(NSDictionary *)options
-{
-    [self setupNotification:options];
-}
-
-- (void)setupNotification:(NSDictionary *)options
-{
-    [[FWNotificationManager sharedInstance] clearNotificationBadges];
-    NSDictionary *remoteNotification = (NSDictionary *)[options objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
-    if (remoteNotification) {
-        [[FWNotificationManager sharedInstance] handleRemoteNotification:remoteNotification];
-    }
-    NSDictionary *localNotification = (NSDictionary *)[options objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
-    if (localNotification) {
-        [[FWNotificationManager sharedInstance] handleLocalNotification:localNotification];
-    }
-    
-    [[FWNotificationManager sharedInstance] registerNotificationHandler];
-    [[FWNotificationManager sharedInstance] requestAuthorize:nil];
-    [FWNotificationManager sharedInstance].remoteNotificationHandler = ^(NSDictionary * userInfo, id notification) {
-        NSString *title = nil;
-        if (@available(iOS 10.0, *)) {
-            if ([notification isKindOfClass:[UNNotificationResponse class]]) {
-                title = ((UNNotificationResponse *)notification).notification.request.content.title;
-            }
-        }
-        [UIWindow.fwMainWindow fwShowMessageWithText:[NSString stringWithFormat:@"收到远程通知：%@\n%@", FWSafeString(title), userInfo]];
-    };
-    [FWNotificationManager sharedInstance].localNotificationHandler = ^(NSDictionary * userInfo, id notification) {
-        NSString *title = nil;
-        if (@available(iOS 10.0, *)) {
-            if ([notification isKindOfClass:[UNNotificationResponse class]]) {
-                title = ((UNNotificationResponse *)notification).notification.request.content.title;
-            }
-        } else {
-            if ([notification isKindOfClass:[UILocalNotification class]]) {
-                title = ((UILocalNotification *)notification).alertTitle ?: ((UILocalNotification *)notification).alertBody;
-            }
-        }
-        [UIWindow.fwMainWindow fwShowMessageWithText:[NSString stringWithFormat:@"收到本地通知：%@\n%@", FWSafeString(title), userInfo]];
-    };
-}
 
 - (void)setupController
 {
