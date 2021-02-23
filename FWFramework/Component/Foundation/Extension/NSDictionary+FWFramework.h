88e58a6157ae13c86da2de76132e599e3fb5f11f
wuyong <lingshizhuangzi@gmail.com>
2021-02-23 16:12:07 +0800

    优化Foundation分类属性声明

--- FWFramework/Component/Foundation/Extension/NSDictionary+FWFramework.h
+++ FWFramework/Component/Foundation/Extension/NSDictionary+FWFramework.h
@@ -46,35 +46,35 @@ NS_ASSUME_NONNULL_BEGIN
  
  @return 随机Key
  */
-- (nullable KeyType)fwRandomKey;
+@property (nonatomic, readonly, nullable) KeyType fwRandomKey;
 
 /*!
  @brief 从字典中随机取出对象，如@{@"a"=>@2, @"b"=>@8, @"c"=>@0}随机取出@8
  
  @return 随机对象
  */
-- (nullable ObjectType)fwRandomObject;
+@property (nonatomic, readonly, nullable) ObjectType fwRandomObject;
 
 /*!
  @brief 从字典中按权重Object随机取出Key，如@{@"a"=>@2, @"b"=>@8, @"c"=>@0}大概率取出@"b"，不会取出@"c"
  
  @return 随机Key
  */
-- (nullable KeyType)fwRandomWeightKey;
+@property (nonatomic, readonly, nullable) KeyType fwRandomWeightKey;
 
 /*!
  @brief 字典中是否含有NSNull值
  
  @return 是否含有NSNull
  */
-- (BOOL)fwIncludeNull;
+@property (nonatomic, assign, readonly) BOOL fwIncludeNull;
 
 /*!
  @brief 递归移除字典中NSNull值
  
  @return 不含NSNull的字典
  */
-- (NSDictionary *)fwRemoveNull;
+@property (nonatomic, copy, readonly) NSDictionary *fwRemoveNull;
 
 /*!
  @brief 移除字典中NSNull值
