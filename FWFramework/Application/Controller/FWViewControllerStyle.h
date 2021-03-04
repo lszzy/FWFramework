8b7353021169966c21d280bed38f7fd85b6c3a0b
wuyong <lingshizhuangzi@gmail.com>
2021-03-04 13:39:44 +0800

    FWViewControllerStyle代码优化

--- FWFramework/Application/Controller/FWViewControllerStyle.h
+++ FWFramework/Application/Controller/FWViewControllerStyle.h
@@ -98,41 +98,11 @@ static const FWNavigationBarStyle FWNavigationBarStyleTransparent = 1;
 /// 设置导航栏返回按钮，支持UIBarButtonItem|NSString|UIImage等，nil时显示系统箭头，下个页面生效
 @property (nonatomic, strong, nullable) id fwBackBarItem;
 
-/// 当自定义left按钮或隐藏导航栏之后，系统返回手势失效，可开启此属性强制加回手势。设置即生效，VC优先级大于Nav，当interactivePop手势禁用时不生效
-@property (nonatomic, assign) BOOL fwForcePopGesture;
-
 /// 导航栏返回按钮点击事件(pop不会触发)，当前页面生效。返回YES关闭页面，NO不关闭，子类可重写。默认调用已设置的block事件
 - (BOOL)fwPopBackBarItem;
 
 /// 设置导航栏返回按钮点击block事件，默认fwPopBackBarItem自动调用。逻辑同上
 @property (nonatomic, copy, nullable) BOOL (^fwBackBarBlock)(void);
-
-#pragma mark - Fullscreen
-
-/// 视图控制器是否禁用全屏返回手势，默认NO
-@property (nonatomic, assign) BOOL fwFullscreenPopGestureDisabled;
-
-/// 视图控制器全屏手势距离左侧最大距离，默认0，无限制
-@property (nonatomic, assign) CGFloat fwFullscreenPopGestureDistance;
-
-@end
-
-#pragma mark - UINavigationController+FWStyle
-
-/*!
- @brief 导航栏全屏返回手势分类，兼容fwPopBackBarItem返回拦截方法
- @see https://github.com/forkingdog/FDFullscreenPopGesture
- */
-@interface UINavigationController (FWStyle)
-
-/// 是否启用导航栏全屏返回手势，默认NO。启用时系统返回手势失效，禁用时还原系统手势。如果只禁用系统手势，设置interactivePopGestureRecognizer.enabled即可
-@property (nonatomic, assign) BOOL fwFullscreenPopGestureEnabled;
-
-/// 导航栏全屏返回手势对象
-@property (nonatomic, strong, readonly) UIPanGestureRecognizer *fwFullscreenPopGestureRecognizer;
-
-/// 判断手势是否是全局返回手势对象
-+ (BOOL)fwIsFullscreenPopGestureRecognizer:(UIGestureRecognizer *)gestureRecognizer;
 
 @end
 
