65ce0a83281d0431fc42810e36d183f244ead2b8
wuyong <lingshizhuangzi@gmail.com>
2021-03-08 23:56:07 +0800

    移除编码规范和模板文件等

--- Document/Templates.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-PWD_PATH=`pwd`
-
-rm -rf ~/Library/Developer/Xcode/Templates.backup
-mv ~/Library/Developer/Xcode/Templates ~/Library/Developer/Xcode/Templates.backup
-cp -rf ${PWD_PATH}/Templates ~/Library/Developer/Xcode/Templates
-
-open ~/Library/Developer/Xcode/
