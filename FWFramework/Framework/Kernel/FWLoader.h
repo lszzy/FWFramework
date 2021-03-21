4791dd1fff7beba713bb3a8a6df143f737b43322
wuyong <lingshizhuangzi@gmail.com>
2021-03-21 21:45:14 +0800

    增加Loader加载器方法

--- FWFramework/Framework/Kernel/FWLoader.h
+++ FWFramework/Framework/Kernel/FWLoader.h
@@ -16,7 +16,22 @@ NS_ASSUME_NONNULL_BEGIN
  @discussion 本方案采用objc扩展方法实现，相对于全局扫描类方案性能高(1/200)，使用简单
     使用方法：新增FWLoader扩展objc类方法，以load开头即会自动调用，注意方法名不要重复，建议load+类名+扩展名
  */
-@interface FWLoader : NSObject
+@interface FWLoader<__covariant InputType, __covariant OutputType> : NSObject
+
+/// 指定加载block初始化
+- (instancetype)initWithBlock:(OutputType _Nullable (^)(InputType input))block;
+
+/// 添加加载block，返回标志id
+- (NSString *)addBlock:(OutputType _Nullable (^)(InputType input))block;
+
+/// 指定标志id移除block
+- (void)removeBlock:(NSString *)identifier;
+
+/// 移除所有的block
+- (void)removeAllBlocks;
+
+/// 依次执行加载block，直到加载成功
+- (nullable OutputType)load:(InputType)input;
 
 @end
 
