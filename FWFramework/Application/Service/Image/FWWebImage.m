86ef76d0bb7b91007adbf43fe4f668e92cfe0c3c
wuyong <lingshizhuangzi@gmail.com>
2021-03-25 18:57:06 +0800

    优化FWWebImage Fade动画处理

--- FWFramework/Application/Service/Image/FWWebImage.m
+++ FWFramework/Application/Service/Image/FWWebImage.m
@@ -781,7 +781,20 @@
     } completion:^(UIImage *image, BOOL isCache, NSError *error) {
         __typeof__(self) self = self_weak_;
         BOOL autoSetImage = image && (!(options & FWImageOptionAvoidSetImage) || !completion);
-        if (autoSetImage) {
+        if (autoSetImage && self.fadeAnimated && !isCache) {
+            NSUUID *originalReceiptID = [[FWImageDownloader sharedDownloader] activeImageDownloadReceipt:imageView].receiptID;
+            [UIView transitionWithView:imageView duration:0 options:0 animations:^{
+                NSUUID *receiptID = [[FWImageDownloader sharedDownloader] activeImageDownloadReceipt:imageView].receiptID;
+                if (!receiptID || ![originalReceiptID isEqual:receiptID]) return;
+            } completion:^(BOOL finished) {
+                [UIView transitionWithView:imageView duration:0.5 options:UIViewAnimationOptionTransitionCrossDissolve | UIViewAnimationOptionAllowUserInteraction animations:^{
+                    NSUUID *receiptID = [[FWImageDownloader sharedDownloader] activeImageDownloadReceipt:imageView].receiptID;
+                    if (!receiptID || ![originalReceiptID isEqual:receiptID]) return;
+                    
+                    imageView.image = image;
+                } completion:nil];
+            }];
+        } else if (autoSetImage) {
             imageView.image = image;
         }
         
