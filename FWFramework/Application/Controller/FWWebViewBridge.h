a526c7013194eb2090b6e85681faa01471de572e
wuyong <lingshizhuangzi@gmail.com>
2021-04-10 00:00:06 +0800

    WebView Cookie管理

--- FWFramework/Application/Controller/FWWebViewBridge.h
+++ FWFramework/Application/Controller/FWWebViewBridge.h
@@ -87,6 +87,38 @@ NSString * FWWebViewJsBridge_js(void);
 
 @end
 
+#pragma mark - FWWebViewCookieManager
+
+/*!
+@brief WKWebView管理Cookie
+
+@see https://github.com/karosLi/KKJSBridge
+*/
+@interface FWWebViewCookieManager : NSObject
+
+/// 同步首个请求的Cookie
++ (void)syncRequestCookie:(NSMutableURLRequest *)request;
+
+/// 同步请求的httpOnly Cookie
++ (void)syncRequestHttpOnlyCookie:(NSMutableURLRequest *)request;
+
+/// 同步ajax请求的Cookie
++ (NSString *)ajaxCookieScripts;
+
+/// 同步重定向请求的Cookie
++ (NSMutableURLRequest *)fixRequest:(NSURLRequest *)request;
+
+/// 拷贝共享Cookie到webView，iOS11+有效
++ (void)copySharedCookie:(WKWebView *)toWebView completion:(nullable void (^)(void))completion;
+
+/// 拷贝webView到共享Cookie，iOS11+有效
++ (void)copyWebViewCookie:(WKWebView *)fromWebView completion:(nullable void (^)(void))completion;
+
+/// Cookie日期格式化对象
++ (NSDateFormatter *)cookieDateFormatter;
+
+@end
+
 #pragma mark - FWWebView
 
 @interface UIProgressView (FWWebView)
