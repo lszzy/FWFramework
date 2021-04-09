2f52e9cdea6cab592302fe901fd5ce30a8d02d73
wuyong <lingshizhuangzi@gmail.com>
2021-04-10 00:00:06 +0800

    WebView Cookie管理

--- FWFramework/Application/Controller/FWWebViewBridge.m
+++ FWFramework/Application/Controller/FWWebViewBridge.m
@@ -575,6 +575,148 @@ NSString * FWWebViewJsBridge_js() {
 
 @end
 
+#pragma mark - FWWebViewCookieManager
+
+@implementation FWWebViewCookieManager
+
++ (void)syncRequestCookie:(NSMutableURLRequest *)request
+{
+    if (!request.URL) {
+        return;
+    }
+    
+    NSArray<NSHTTPCookie *> *availableCookie = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:request.URL];
+    if (availableCookie.count > 0) {
+        NSDictionary *reqHeader = [NSHTTPCookie requestHeaderFieldsWithCookies:availableCookie];
+        NSString *cookieStr = [reqHeader objectForKey:@"Cookie"];
+        [request setValue:cookieStr forHTTPHeaderField:@"Cookie"];
+    }
+}
+
++ (void)syncRequestHttpOnlyCookie:(NSMutableURLRequest *)request
+{
+    if (!request.URL) {
+        return;
+    }
+    
+    NSArray<NSHTTPCookie *> *availableCookie = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:request.URL];
+    if (availableCookie.count > 0) {
+        NSMutableString *cookieStr = [[request valueForHTTPHeaderField:@"Cookie"] mutableCopy];
+        if (!cookieStr) {
+            cookieStr = [[NSMutableString alloc] init];
+        }
+        for (NSHTTPCookie *cookie in availableCookie) {
+            if (!cookie.isHTTPOnly) {
+                continue;
+            }
+            [cookieStr appendFormat:@"%@=%@;", cookie.name, cookie.value];
+        }
+        [request setValue:cookieStr forHTTPHeaderField:@"Cookie"];
+    }
+}
+
++ (NSString *)ajaxCookieScripts
+{
+    NSMutableString *cookieScript = [[NSMutableString alloc] init];
+    for (NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]) {
+        if ([cookie.value rangeOfString:@"'"].location != NSNotFound) {
+            continue;
+        }
+        [cookieScript appendFormat:@"document.cookie='%@=%@;", cookie.name, cookie.value];
+        if (cookie.domain || cookie.domain.length > 0) {
+            [cookieScript appendFormat:@"domain=%@;", cookie.domain];
+        }
+        if (cookie.path || cookie.path.length > 0) {
+            [cookieScript appendFormat:@"path=%@;", cookie.path];
+        }
+        if (cookie.expiresDate) {
+            [cookieScript appendFormat:@"expires=%@;", [[self cookieDateFormatter] stringFromDate:cookie.expiresDate]];
+        }
+        if (cookie.secure) {
+            [cookieScript appendString:@"Secure;"];
+        }
+        if (cookie.HTTPOnly) {
+            [cookieScript appendString:@"HTTPOnly;"];
+        }
+        [cookieScript appendFormat:@"'\n"];
+    }
+    return cookieScript;
+}
+
++ (NSMutableURLRequest *)fixRequest:(NSURLRequest *)request
+{
+    NSMutableURLRequest *fixedRequest;
+    if ([request isKindOfClass:[NSMutableURLRequest class]]) {
+        fixedRequest = (NSMutableURLRequest *)request;
+    } else {
+        fixedRequest = request.mutableCopy;
+    }
+    
+    NSMutableArray *array = [NSMutableArray array];
+    for (NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:request.URL]) {
+        NSString *value = [NSString stringWithFormat:@"%@=%@", cookie.name, cookie.value];
+        [array addObject:value];
+    }
+
+    NSString *cookie = [array componentsJoinedByString:@";"];
+    [fixedRequest setValue:cookie forHTTPHeaderField:@"Cookie"];
+    return fixedRequest;
+}
+
++ (void)copySharedCookie:(WKWebView *)webView completion:(void (^)(void))completion
+{
+    if (@available(iOS 11.0, *)) {
+        NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies];
+        WKHTTPCookieStore *cookieStroe = webView.configuration.websiteDataStore.httpCookieStore;
+        if (cookies.count == 0) {
+            completion ? completion() : nil;
+            return;
+        }
+        for (NSHTTPCookie *cookie in cookies) {
+            [cookieStroe setCookie:cookie completionHandler:^{
+                if ([[cookies lastObject] isEqual:cookie]) {
+                    completion ? completion() : nil;
+                    return;
+                }
+            }];
+        }
+    }
+}
+
++ (void)copyWebViewCookie:(WKWebView *)webView completion:(void (^)(void))completion
+{
+    if (@available(iOS 11.0, *)) {
+        WKHTTPCookieStore *cookieStroe = webView.configuration.websiteDataStore.httpCookieStore;
+        [cookieStroe getAllCookies:^(NSArray<NSHTTPCookie *> * _Nonnull cookies) {
+            if (cookies.count == 0) {
+                completion ? completion() : nil;
+                return;
+            }
+            for (NSHTTPCookie *cookie in cookies) {
+                [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+                if ([[cookies lastObject] isEqual:cookie]) {
+                    completion ? completion() : nil;
+                    return;
+                }
+            }
+        }];
+    }
+}
+
++ (NSDateFormatter *)cookieDateFormatter
+{
+    static NSDateFormatter *formatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];
+        formatter.dateFormat = @"EEE, d MMM yyyy HH:mm:ss zzz";
+    });
+    return formatter;
+}
+
+@end
+
 #pragma mark - FWWebView
 
 @implementation UIProgressView (FWWebView)
