509335759b0740f00d976f0cb133e37d16e7ec24
wuyong <lingshizhuangzi@gmail.com>
2021-06-06 13:54:41 +0800

    feat: 新增FWPromise.retry方法

--- FWFramework/Classes/Framework/Kernel/FWPromise.swift
+++ FWFramework/Classes/Framework/Kernel/FWPromise.swift
@@ -20,46 +20,50 @@ public func fw_await(_ promise: FWPromise) throws -> Any? {
     return try FWPromise.await(promise)
 }
 
-/// 约定内置错误码枚举
-@objc public enum FWPromiseErrorCode: Int {
-    case `default`
-    case validation
-    case timeout
-}
-
 /// 框架约定类
 @objcMembers public class FWPromise: NSObject {
-    /// 约定内置错误域常量
-    public static let errorDomain: String = "site.wuyong.error.promise"
+    // MARK: - Config
     /// 约定回调队列，默认main队列
     public static var completionQueue: DispatchQueue = DispatchQueue.main
+    /// 约定默认错误，约定失败时可选使用，可用于错误判断，支持自定义
+    public static var defaultError: Error = NSError(domain: "site.wuyong.error.promise", code: 0, userInfo: [NSLocalizedDescriptionKey: "Promise failed"])
+    /// 约定验证错误，验证失败时默认使用，可用于错误判断，支持自定义
+    public static var validationError: Error = NSError(domain: "site.wuyong.error.promise", code: 1, userInfo: [NSLocalizedDescriptionKey: "Promise validation failed"])
+    /// 约定超时错误，约定超时时默认使用，可用于错误判断，支持自定义
+    public static var timeoutError: Error = NSError(domain: "site.wuyong.error.promise", code: 2, userInfo: [NSLocalizedDescriptionKey: "Promise timeout"])
     
+    // MARK: - Private
     /// 约定内部属性
     private let operation: (@escaping (_ result: Any?) -> Void) -> Void
     private var finished: Bool = false
-    private var retry: (times: Int, delay: TimeInterval) = (times: 0, delay: 0)
     
     /// 约定内部方法
     private func execute(completion: @escaping (_ result: Any?) -> Void) {
         self.operation() { result in
             FWPromise.completionQueue.async {
-                if self.finished { return }
-                if self.retry.times < 1 || !(result is Error) {
+                if !self.finished {
                     self.finished = true
-                    self.retry.times = 0
                     completion(result)
-                } else {
-                    FWPromise.delay(self.retry.delay) {
-                        self.retry.times -= 1
-                        self.execute(completion: completion)
-                    }
                 }
             }
         }
     }
     
-    // MARK: - Public
+    /// 约定内部延时方法
+    private static func delay(_ time: TimeInterval, block: @escaping () -> Void) {
+        FWPromise.completionQueue.asyncAfter(deadline: .now() + time, execute: block)
+    }
     
+    /// 约定内部重试方法
+    private static func retry(_ initialPromise: FWPromise?, times: Int, delay: TimeInterval, block: @escaping () -> FWPromise) -> FWPromise {
+        let promise = initialPromise ?? FWPromise.delay(delay).then({ _ in block() })
+        if times < 1 { return promise }
+        return promise.recover { _ in
+            FWPromise.retry(nil, times: times - 1, delay: delay, block: block)
+        }
+    }
+    
+    // MARK: - Public
     /// 指定操作完成句柄初始化
     public init(operation: @escaping (_ completion: @escaping (_ result: Any?) -> Void) -> Void) {
         self.operation = operation
@@ -142,6 +146,66 @@ public func fw_await(_ promise: FWPromise) throws -> Any? {
         }
     }
     
+    /// 验证约定，当前约定成功时验证结果，可返回Bool或Error?；验证通过时返回结果，验证失败时返回验证错误
+    public func validate(_ block: @escaping (_ value: Any?) -> Any?) -> FWPromise {
+        return FWPromise { completion in
+            self.done { value in
+                let result = block(value)
+                if let error = result as? Error {
+                    completion(error)
+                } else if let valid = result as? Bool, !valid {
+                    completion(FWPromise.validationError)
+                } else {
+                    completion(value)
+                }
+            } catch: { error in
+                completion(error)
+            }
+        }
+    }
+    
+    /// 减少约定，当前约定结果作为初始值value，顺序使用value和数组值item调用reducer，产生新的value继续循环直至结束，类似数组reduce方法
+    public func reduce(_ items: [Any], reducer: @escaping (_ value: Any?, _ item: Any) -> Any?) -> FWPromise {
+        var promise = self
+        for item in items {
+            promise = promise.then({ value in
+                return reducer(value, item)
+            })
+        }
+        return promise
+    }
+    
+    /// 约定延时，当前约定成功时延时返回结果；默认失败时不延时，可设置force强制失败时也延时
+    public func delay(_ time: TimeInterval, force: Bool = false) -> FWPromise {
+        return FWPromise { completion in
+            self.done { result in
+                if force || !(result is Error) {
+                    FWPromise.delay(time) {
+                        completion(result)
+                    }
+                } else {
+                    completion(result)
+                }
+            }
+        }
+    }
+    
+    /// 约定超时，当前约定未超时时返回结果；否则返回超时错误信息
+    public func timeout(_ time: TimeInterval, error: Error? = nil) -> FWPromise {
+        let promise = FWPromise { completion in
+            FWPromise.delay(time) {
+                completion(error ?? FWPromise.timeoutError)
+            }
+        }
+        return FWPromise.race([self, promise])
+    }
+    
+    /// 约定重试，block需返回新创建的约定，当前约定失败时延迟指定时间后调用block创建约定并执行，直至成功或达到最大重试次数
+    public func retry(_ times: Int = 1, delay: TimeInterval = 0, block: @escaping () -> FWPromise) -> FWPromise {
+        return FWPromise.retry(self, times: times, delay: delay, block: block)
+    }
+    
+    // MARK: - Static
     /// 仿协程异步执行方法
     @discardableResult
     public static func async(_ block: @escaping () throws -> Any?) -> FWPromise {
@@ -176,67 +240,6 @@ public func fw_await(_ promise: FWPromise) throws -> Any? {
             throw e
         }
         return result
-    }
-    
-    // MARK: - Extension
-    
-    /// 验证约定，当前约定成功时验证结果，可返回Bool或Error?；验证通过时返回结果，验证失败时返回验证错误
-    public func validate(_ block: @escaping (_ value: Any?) -> Any?) -> FWPromise {
-        return FWPromise { completion in
-            self.done { value in
-                let result = block(value)
-                if let error = result as? Error {
-                    completion(error)
-                } else if let valid = result as? Bool, !valid {
-                    completion(NSError(domain: FWPromise.errorDomain, code: FWPromiseErrorCode.validation.rawValue, userInfo: [NSLocalizedDescriptionKey: "Promise validation failed"]))
-                } else {
-                    completion(value)
-                }
-            } catch: { error in
-                completion(error)
-            }
-        }
-    }
-    
-    /// 减少约定，当前约定结果作为初始值value，顺序使用value和数组值item调用reducer，产生新的value继续循环直至结束，类似数组reduce方法
-    public func reduce(_ items: [Any], reducer: @escaping (_ value: Any?, _ item: Any) -> Any?) -> FWPromise {
-        var promise = self
-        for item in items {
-            promise = promise.then({ value in
-                return reducer(value, item)
-            })
-        }
-        return promise
-    }
-    
-    /// 约定重试，当前约定失败时延迟指定时间后重试，直至成功或达到最大重试次数时停止
-    public func retry(_ times: Int = 1, delay: TimeInterval = 0) -> FWPromise {
-        self.retry.times = times > 0 ? times : 0
-        self.retry.delay = delay > 0 ? delay : 0
-        return self
-    }
-    
-    /// 约定超时，当前约定未超时时返回结果；否则返回超时错误信息
-    public func timeout(_ time: TimeInterval, error: Error? = nil) -> FWPromise {
-        let promise = FWPromise { completion in
-            FWPromise.delay(time) {
-                completion(error ?? NSError(domain: FWPromise.errorDomain, code: FWPromiseErrorCode.timeout.rawValue, userInfo: [NSLocalizedDescriptionKey: "Promise timeout"]))
-            }
-        }
-        return FWPromise.race([self, promise])
-    }
-    
-    /// 约定延时，当前约定成功时延时返回相同的结果；失败时不执行延时
-    public func delay(_ time: TimeInterval) -> FWPromise {
-        return FWPromise { completion in
-            self.done { value in
-                FWPromise.delay(time) {
-                    completion(value)
-                }
-            } catch: { error in
-                completion(error)
-            }
-        }
     }
     
     /// 全部约定，所有约定成功才返回约定结果合集；如果某一个失败了，则返回该错误信息
@@ -293,8 +296,8 @@ public func fw_await(_ promise: FWPromise) throws -> Any? {
         }
     }
     
-    /// 约定内部延时方法
-    private static func delay(_ time: TimeInterval, block: @escaping () -> Void) {
-        FWPromise.completionQueue.asyncAfter(deadline: .now() + time, execute: block)
+    /// 约定重试，block需返回新创建的约定，该约定失败时延迟指定时间后重新创建并执行，直至成功或达到最大重试次数(总次数retry+1)
+    public static func retry(_ times: Int = 1, delay: TimeInterval = 0, block: @escaping () -> FWPromise) -> FWPromise {
+        return retry(block(), times: times, delay: delay, block: block)
     }
 }
