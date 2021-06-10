31c447a9f53c0ca8e1fb4d3453d9f1877535d5bb
wuyong <lingshizhuangzi@gmail.com>
2021-06-10 15:29:43 +0800

    perf: 优化FWFramework.h文件

--- FWFramework/Classes/Component/ObjC/FWFramework.h
+++ FWFramework/Classes/Component/ObjC/FWFramework.h
@@ -6,71 +6,6 @@
 //  Copyright © 2019 wuyong.site. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-#if __has_include(<FWFramework/FWFramework.h>)
-
-FOUNDATION_EXPORT double FWFrameworkVersionNumber;
-FOUNDATION_EXPORT const unsigned char FWFrameworkVersionString[];
-
-#import <FWFramework/FWDefine.h>
-#import <FWFramework/FWNotification.h>
-#import <FWFramework/FWToolkit.h>
-
-#if __has_include(<FWFramework/FWAppDelegate.h>)
-#import <FWFramework/FWAppDelegate.h>
-#endif
-
-#if __has_include(<FWFramework/FWWebViewController.h>)
-#import <FWFramework/FWWebViewController.h>
-#endif
-
-#if __has_include(<FWFramework/FWModel.h>)
-#import <FWFramework/FWModel.h>
-#endif
-
-#if __has_include(<FWFramework/FWView.h>)
-#import <FWFramework/FWView.h>
-#endif
-
-#if __has_include(<FWFramework/FWCacheManager.h>)
-#import <FWFramework/FWCacheManager.h>
-#endif
-
-#if __has_include(<FWFramework/FWDatabase.h>)
-#import <FWFramework/FWDatabase.h>
-#endif
-
-#if __has_include(<FWFramework/FWWebImage.h>)
-#import <FWFramework/FWWebImage.h>
-#endif
-
-#if __has_include(<FWFramework/FWHTTPSessionManager.h>)
-#import <FWFramework/FWHTTPSessionManager.h>
-#endif
-
-#if __has_include(<FWFramework/FWNetworkPrivate.h>)
-#import <FWFramework/FWNetworkPrivate.h>
-#endif
-
-#if __has_include(<FWFramework/FWAsyncSocket.h>)
-#import <FWFramework/FWAsyncSocket.h>
-#endif
-
-#if __has_include(<FWFramework/Foundation+FWFramework.h>)
-#import <FWFramework/Foundation+FWFramework.h>
-#endif
-
-#if __has_include(<FWFramework/UIKit+FWFramework.h>)
-#import <FWFramework/UIKit+FWFramework.h>
-#endif
-
-#if __has_include(<FWFramework/FWAttributedLabel.h>)
-#import <FWFramework/FWAttributedLabel.h>
-#endif
-
-#else
-
 #import "FWDefine.h"
 #import "FWNotification.h"
 #import "FWToolkit.h"
@@ -125,6 +60,4 @@ FOUNDATION_EXPORT const unsigned char FWFrameworkVersionString[];
 
 #if __has_include("FWAttributedLabel.h")
 #import "FWAttributedLabel.h"
-#endif
-
 #endif
