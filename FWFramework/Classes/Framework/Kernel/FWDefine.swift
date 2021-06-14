0b8e9cbda0f8bdee259404bfda607c7838d42456
wuyong <lingshizhuangzi@gmail.com>
2021-06-14 18:17:35 +0800

    refactor: 重构FWBenchmark为Swift实现

--- FWFramework/Classes/Framework/Kernel/FWDefine.swift
+++ FWFramework/Classes/Framework/Kernel/FWDefine.swift
@@ -12,7 +12,7 @@ import Foundation
 ///
 /// - Parameter name: 调试标签，默认空字符串
 public func FWBenchmarkBegin(_ name: String = "") {
-    FWBenchmark.benchmarkBegin(name)
+    FWBenchmark.begin(name)
 }
 
 /// 标记时间调试结束并打印消耗时间
@@ -21,5 +21,23 @@ public func FWBenchmarkBegin(_ name: String = "") {
 /// - Returns: 消耗时间
 @discardableResult
 public func FWBenchmarkEnd(_ name: String = "") -> TimeInterval {
-    return FWBenchmark.benchmarkEnd(name)
+    return FWBenchmark.end(name)
+}
+
+/// 时间调试器
+@objcMembers public class FWBenchmark: NSObject {
+    private static var times: [String : TimeInterval] = [:]
+    
+    /// 标记时间调试开始
+    public static func begin(_ name: String) {
+        times[name] = Date().timeIntervalSince1970
+    }
+    
+    /// 标记时间调试结束并打印消耗时间
+    public static func end(_ name: String) -> TimeInterval {
+        let beginTime = times[name] ?? Date().timeIntervalSince1970
+        let timeInterval = Date().timeIntervalSince1970 - beginTime
+        NSLog("FWBenchmark-%@: %.3fms", name, timeInterval * 1000)
+        return timeInterval
+    }
 }
