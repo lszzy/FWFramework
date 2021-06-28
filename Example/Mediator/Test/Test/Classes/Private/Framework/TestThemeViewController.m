82057fd16d2a529be9a8110236de55d218a65de1
wuyong <lingshizhuangzi@gmail.com>
2021-06-28 23:48:43 +0800

    fix: 新增fwThemeImage快速创建主题图片方法

--- Example/Mediator/Test/Test/Classes/Private/Framework/TestThemeViewController.m
+++ Example/Mediator/Test/Test/Classes/Private/Framework/TestThemeViewController.m
@@ -49,11 +49,11 @@
     [self.view addSubview:colorView];
     
     colorView = [[UIView alloc] initWithFrame:CGRectMake(160, 20, 50, 50)];
-    [UIColor fwSetThemeColor:[UIColor fwThemeLight:[UIColor whiteColor] dark:[UIColor blackColor]] forName:@"dynamic_color"];
+    [UIColor fwSetThemeColor:[UIColor fwThemeLight:[UIColor blackColor] dark:[UIColor whiteColor]] forName:@"dynamic_color"];
     UIColor *dynamicColor = [UIColor fwThemeNamed:@"dynamic_color"];
-    colorView.backgroundColor = [dynamicColor fwColor:FWThemeManager.sharedInstance.style == FWThemeStyleLight ? FWThemeStyleDark : FWThemeStyleLight];
+    colorView.backgroundColor = [dynamicColor fwColor:FWThemeManager.sharedInstance.style];
     [colorView fwAddThemeListener:^(FWThemeStyle style) {
-        colorView.backgroundColor = [dynamicColor fwColor:style == FWThemeStyleDark ? FWThemeStyleLight : FWThemeStyleDark];
+        colorView.backgroundColor = [dynamicColor fwColor:style];
     }];
     [self.view addSubview:colorView];
     
