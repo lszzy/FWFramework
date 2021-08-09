d6300100841de6f313edfe29ad4b50b5342bd6cb
wuyong <lingshizhuangzi@gmail.com>
2021-08-09 22:25:14 +0800

    test: 新增FWIcon测试列表

---
+++ Example/Mediator/Test/Test/Classes/Private/Framework/TestIconViewController.swift
@@ -0,0 +1,94 @@
+//
+//  TestIconViewController.swift
+//  Example
+//
+//  Created by wuyong on 2020/12/2.
+//  Copyright © 2020 site.wuyong. All rights reserved.
+//
+
+import UIKit
+
+class TestIconCell: UICollectionViewCell {
+    lazy var imageView: UIImageView = {
+        let result = UIImageView()
+        return result
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let result = UILabel()
+        result.textColor = Theme.textColor
+        result.font = FWFontSize(10)
+        result.textAlignment = .center
+        result.numberOfLines = 0
+        return result
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = Theme.cellColor
+        contentView.addSubview(imageView)
+        contentView.addSubview(nameLabel)
+        imageView.fwLayoutChain.centerX().top().size(CGSize(width: 60, height: 60))
+        nameLabel.fwLayoutChain.edges(.zero, excludingEdge: .top)
+            .topToBottomOfView(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+@objcMembers class TestIconViewController: TestViewController, FWCollectionViewController {
+    private var iconClass: FWIcon.Type = Octicons.self
+    
+    override func renderView() {
+        collectionView.backgroundColor = Theme.backgroundColor
+    }
+    
+    func renderCollectionViewLayout() -> UICollectionViewLayout {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 60, height: 100)
+        return flowLayout
+    }
+    
+    override func renderData() {
+        fwSetRightBarItem(NSStringFromClass(iconClass)) { [weak self] sender in
+            self?.fwShowSheet(withTitle: nil, message: nil, cancel: "取消", actions: ["Octicons", "MaterialIcons", "FontAwesome", "FoundationIcons", "IonIcons"], actionBlock: { index in
+                if index == 0 {
+                    self?.iconClass = Octicons.self
+                } else if index == 1 {
+                    self?.iconClass = MaterialIcons.self
+                } else if index == 2 {
+                    self?.iconClass = FontAwesome.self
+                } else if index == 3 {
+                    self?.iconClass = FoundationIcons.self
+                } else {
+                    self?.iconClass = IonIcons.self
+                }
+                self?.renderData()
+            })
+        }
+        
+        collectionData.setArray(Array(iconClass.iconMapper().keys))
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = TestIconCell.fwCell(with: collectionView, indexPath: indexPath)
+        let name = collectionData.fwObject(at: indexPath.item) as? String
+        cell.imageView.image = FWIconImage(name.fwSafeValue, 20)
+        cell.nameLabel.text = name
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let name = collectionData.fwObject(at: indexPath.item) as? String
+        UIPasteboard.general.string = FWSafeString(name)
+        fwShowMessage(withText: name)
+    }
+}
