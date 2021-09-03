c47d6345f0788c389cb4ba19e1d44e6dd4f7ba11
wuyong <lingshizhuangzi@gmail.com>
2021-09-03 17:29:55 +0800

    feat: 新增FWImagePreviewView

--- FWFramework/Classes/Application/Service/Media/Preview/FWImagePreview.m
+++ FWFramework/Classes/Application/Service/Media/Preview/FWImagePreview.m
@@ -9,6 +9,7 @@
 
 #import "FWImagePreview.h"
 #import "FWToolkit.h"
+#import "FWViewPlugin.h"
 
 #pragma mark - FWCollectionViewPagingLayout
 
@@ -200,6 +201,277 @@
 
 - (void)setMinimumScale:(CGFloat)minimumScale {
     _minimumScale = minimumScale;
+}
+
+@end
+
+#pragma mark - FWImagePreviewView
+
+@interface FWImagePreviewCell : UICollectionViewCell
+
+@property(nonatomic, strong) FWZoomImageView *zoomImageView;
+@end
+
+@implementation FWImagePreviewCell
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = UIColor.clearColor;
+        
+        self.zoomImageView = [[FWZoomImageView alloc] init];
+        [self.contentView addSubview:self.zoomImageView];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    self.zoomImageView.fwFrameApplyTransform = self.contentView.bounds;
+}
+
+@end
+
+static NSString * const kLivePhotoCellIdentifier = @"livephoto";
+static NSString * const kVideoCellIdentifier = @"video";
+static NSString * const kImageOrUnknownCellIdentifier = @"imageorunknown";
+
+@interface FWImagePreviewView ()
+
+@property(nonatomic, assign) BOOL isChangingCollectionViewBounds;
+@property(nonatomic, assign) CGFloat previousIndexWhenScrolling;
+@end
+
+@implementation FWImagePreviewView
+
+@synthesize currentImageIndex = _currentImageIndex;
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self didInitializedWithFrame:frame];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        [self didInitializedWithFrame:self.frame];
+    }
+    return self;
+}
+
+- (void)didInitializedWithFrame:(CGRect)frame {
+    _collectionViewLayout = [[FWCollectionViewPagingLayout alloc] initWithStyle:FWCollectionViewPagingLayoutStyleDefault];
+    self.collectionViewLayout.allowsMultipleItemScroll = NO;
+    
+    _collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height) collectionViewLayout:self.collectionViewLayout];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    self.collectionView.backgroundColor = UIColor.clearColor;
+    self.collectionView.showsHorizontalScrollIndicator = NO;
+    self.collectionView.showsVerticalScrollIndicator = NO;
+    self.collectionView.scrollsToTop = NO;
+    self.collectionView.delaysContentTouches = NO;
+    self.collectionView.decelerationRate = UIScrollViewDecelerationRateFast;
+    if (@available(iOS 11, *)) {
+        self.collectionView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
+    [self.collectionView registerClass:[FWImagePreviewCell class] forCellWithReuseIdentifier:kImageOrUnknownCellIdentifier];
+    [self.collectionView registerClass:[FWImagePreviewCell class] forCellWithReuseIdentifier:kVideoCellIdentifier];
+    [self.collectionView registerClass:[FWImagePreviewCell class] forCellWithReuseIdentifier:kLivePhotoCellIdentifier];
+    [self addSubview:self.collectionView];
+    
+    self.loadingColor = UIColor.whiteColor;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    BOOL isCollectionViewSizeChanged = !CGSizeEqualToSize(self.collectionView.bounds.size, self.bounds.size);
+    if (isCollectionViewSizeChanged) {
+        self.isChangingCollectionViewBounds = YES;
+        
+        // 必须先 invalidateLayout，再更新 collectionView.frame，否则横竖屏旋转前后的图片不一致（因为 scrollViewDidScroll: 时 contentSize、contentOffset 那些是错的）
+        [self.collectionViewLayout invalidateLayout];
+        self.collectionView.frame = self.bounds;
+        [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:self.currentImageIndex inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:NO];
+        
+        self.isChangingCollectionViewBounds = NO;
+    }
+}
+
+- (void)setCurrentImageIndex:(NSUInteger)currentImageIndex {
+    [self setCurrentImageIndex:currentImageIndex animated:NO];
+}
+
+- (void)setCurrentImageIndex:(NSUInteger)currentImageIndex animated:(BOOL)animated {
+    _currentImageIndex = currentImageIndex;
+    [self.collectionView reloadData];
+    if (currentImageIndex < [self.collectionView numberOfItemsInSection:0]) {
+        [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:currentImageIndex inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:animated];
+        [self.collectionView layoutIfNeeded];// scroll immediately
+    }
+}
+
+- (void)setLoadingColor:(UIColor *)loadingColor {
+    BOOL isLoadingColorChanged = _loadingColor && ![_loadingColor isEqual:loadingColor];
+    _loadingColor = loadingColor;
+    if (isLoadingColorChanged) {
+        [self.collectionView reloadItemsAtIndexPaths:self.collectionView.indexPathsForVisibleItems];
+    }
+}
+
+#pragma mark - <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    if ([self.delegate respondsToSelector:@selector(numberOfImagesInImagePreviewView:)]) {
+        return [self.delegate numberOfImagesInImagePreviewView:self];
+    }
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *identifier = kImageOrUnknownCellIdentifier;
+    if ([self.delegate respondsToSelector:@selector(imagePreviewView:assetTypeAtIndex:)]) {
+        FWImagePreviewMediaType type = [self.delegate imagePreviewView:self assetTypeAtIndex:indexPath.item];
+        if (type == FWImagePreviewMediaTypeLivePhoto) {
+            identifier = kLivePhotoCellIdentifier;
+        } else if (type == FWImagePreviewMediaTypeVideo) {
+            identifier = kVideoCellIdentifier;
+        }
+    }
+    FWImagePreviewCell *cell = (FWImagePreviewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    FWZoomImageView *zoomView = cell.zoomImageView;
+    zoomView.cloudProgressView.color = self.loadingColor;
+    zoomView.cloudDownloadRetryButton.tintColor = self.loadingColor;
+    zoomView.delegate = self;
+    
+    // 因为 cell 复用的问题，很可能此时会显示一张错误的图片，因此这里要清空所有图片的显示
+    zoomView.image = nil;
+    zoomView.videoPlayerItem = nil;
+    zoomView.livePhoto = nil;
+    if ([self.delegate respondsToSelector:@selector(imagePreviewView:renderZoomImageView:atIndex:)]) {
+        [self.delegate imagePreviewView:self renderZoomImageView:zoomView atIndex:indexPath.item];
+    }
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
+    FWImagePreviewCell *previewCell = (FWImagePreviewCell *)cell;
+    [previewCell.zoomImageView revertZooming];
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didEndDisplayingCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
+    FWImagePreviewCell *previewCell = (FWImagePreviewCell *)cell;
+    [previewCell.zoomImageView endPlayingVideo];
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return collectionView.bounds.size;
+}
+
+#pragma mark - <UIScrollViewDelegate>
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    if (scrollView != self.collectionView) {
+        return;
+    }
+    
+    // 当前滚动到的页数
+    if ([self.delegate respondsToSelector:@selector(imagePreviewView:didScrollToIndex:)]) {
+        [self.delegate imagePreviewView:self didScrollToIndex:self.currentImageIndex];
+    }
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView != self.collectionView) {
+        return;
+    }
+    
+    if (self.isChangingCollectionViewBounds) {
+        return;
+    }
+    
+    CGFloat pageWidth = [self collectionView:self.collectionView layout:self.collectionViewLayout sizeForItemAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]].width;
+    CGFloat pageHorizontalMargin = self.collectionViewLayout.minimumLineSpacing;
+    CGFloat contentOffsetX = self.collectionView.contentOffset.x;
+    CGFloat index = contentOffsetX / (pageWidth + pageHorizontalMargin);
+    
+    // 在滑动过临界点的那一次才去调用 delegate，避免过于频繁的调用
+    BOOL isFirstDidScroll = self.previousIndexWhenScrolling == 0;
+
+    // fastToRight example : self.previousIndexWhenScrolling 1.49, index = 2.0
+    BOOL fastToRight = (floor(index) - floor(self.previousIndexWhenScrolling) >= 1.0) && (floor(index) - self.previousIndexWhenScrolling > 0.5);
+    BOOL turnPageToRight = fastToRight || (self.previousIndexWhenScrolling <= floor(index) + 0.5 && floor(index) + 0.5 <= index);
+
+    // fastToLeft example : self.previousIndexWhenScrolling 2.51, index = 1.99
+    BOOL fastToLeft = (floor(self.previousIndexWhenScrolling) - floor(index) >= 1.0) && (self.previousIndexWhenScrolling - ceil(index) > 0.5);
+    BOOL turnPageToLeft = fastToLeft || (index <= floor(index) + 0.5 && floor(index) + 0.5 <= self.previousIndexWhenScrolling);
+    
+    if (!isFirstDidScroll && (turnPageToRight || turnPageToLeft)) {
+        index = round(index);
+        if (0 <= index && index < [self.collectionView numberOfItemsInSection:0]) {
+            
+            // 不调用 setter，避免又走一次 scrollToItem
+            _currentImageIndex = index;
+            
+            if ([self.delegate respondsToSelector:@selector(imagePreviewView:willScrollHalfToIndex:)]) {
+                [self.delegate imagePreviewView:self willScrollHalfToIndex:index];
+            }
+        }
+    }
+    self.previousIndexWhenScrolling = index;
+}
+
+- (NSInteger)indexForZoomImageView:(FWZoomImageView *)zoomImageView {
+    if ([zoomImageView.superview.superview isKindOfClass:[FWImagePreviewCell class]]) {
+        return [self.collectionView indexPathForCell:(FWImagePreviewCell *)zoomImageView.superview.superview].item;
+    }
+    return NSNotFound;
+}
+
+- (FWZoomImageView *)zoomImageViewAtIndex:(NSUInteger)index {
+    FWImagePreviewCell *cell = (FWImagePreviewCell *)[self.collectionView cellForItemAtIndexPath:[NSIndexPath indexPathForItem:index inSection:0]];
+    return cell.zoomImageView;
+}
+
+#pragma mark - <FWZoomImageViewDelegate>
+
+- (void)singleTouchInZoomingImageView:(FWZoomImageView *)imageView location:(CGPoint)location {
+    if ([self.delegate respondsToSelector:_cmd]) {
+        [self.delegate singleTouchInZoomingImageView:imageView location:location];
+    }
+}
+
+- (void)doubleTouchInZoomingImageView:(FWZoomImageView *)imageView location:(CGPoint)location {
+    if ([self.delegate respondsToSelector:_cmd]) {
+        [self.delegate doubleTouchInZoomingImageView:imageView location:location];
+    }
+}
+
+- (void)longPressInZoomingImageView:(FWZoomImageView *)imageView {
+    if ([self.delegate respondsToSelector:_cmd]) {
+        [self.delegate longPressInZoomingImageView:imageView];
+    }
+}
+
+- (void)didTouchICloudRetryButtonInZoomImageView:(FWZoomImageView *)imageView {
+    if ([self.delegate respondsToSelector:_cmd]) {
+        [self.delegate didTouchICloudRetryButtonInZoomImageView:imageView];
+    }
+}
+
+- (void)zoomImageView:(FWZoomImageView *)imageView didHideVideoToolbar:(BOOL)didHide {
+    if ([self.delegate respondsToSelector:_cmd]) {
+        [self.delegate zoomImageView:imageView didHideVideoToolbar:didHide];
+    }
+}
+
+- (BOOL)enabledZoomViewInZoomImageView:(FWZoomImageView *)imageView {
+    if ([self.delegate respondsToSelector:_cmd]) {
+        return [self.delegate enabledZoomViewInZoomImageView:imageView];
+    }
+    return YES;
 }
 
 @end
