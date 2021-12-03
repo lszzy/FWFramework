cc93d7e9f79842aaee14e858c5680d2875883a48
wuyong <lingshizhuangzi@gmail.com>
2021-12-03 11:22:49 +0800

    docs: 添加文档自动生成功能

---
+++ docs.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+rm -rf docs
+mkdir docs
+
+sourcekitten doc -- -workspace Example/Example.xcworkspace -scheme FWFramework > docs/FWSwift.json
+sourcekitten doc --objc FWFramework/Classes/Kernel/FWFramework.h -- -x objective-c -isysroot $(xcrun --show-sdk-path --sdk iphonesimulator) -I $(pwd) -fmodules > docs/FWKernel.json
+sourcekitten doc --objc FWFramework/Classes/Service/FWNotification.h -- -x objective-c -isysroot $(xcrun --show-sdk-path --sdk iphonesimulator) -I $(pwd) -fmodules > docs/FWService.json
+sourcekitten doc --objc FWFramework/Classes/Toolkit/FWToolkit.h -- -x objective-c -isysroot $(xcrun --show-sdk-path --sdk iphonesimulator) -I $(pwd) -fmodules > docs/FWToolkit.json
+jazzy --sourcekitten-sourcefile docs/FWKernel.json,docs/FWService.json,docs/FWToolkit.json,docs/FWSwift.json
+
+rm -f docs/FWSwift.json
+rm -f docs/FWKernel.json
+rm -f docs/FWService.json
+rm -f docs/FWToolkit.json
