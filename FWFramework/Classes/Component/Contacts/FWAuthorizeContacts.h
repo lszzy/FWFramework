4d4ea194d6aaf20eec8c965c61d375f8f4e68413
wuyong <lingshizhuangzi@gmail.com>
2021-12-25 01:49:32 +0800

    feat: 重构FWFramework模块

--- FWFramework/Classes/Component/Contacts/FWAuthorizeContacts.h
+++ FWFramework/Classes/Component/Contacts/FWAuthorizeContacts.h
@@ -1,12 +1,16 @@
 //
-//  FWAuthorize.h
+//  FWAuthorizeContacts.h
 //  FWFramework
 //
 //  Created by wuyong on 17/3/15.
 //  Copyright © 2018年 wuyong.site. All rights reserved.
 //
 
+#if FWFrameworkContacts
+@import FWFramework;
+#else
 #import "FWAuthorize.h"
+#endif
 
 /// 联系人，需启用Contacts子模块，Info.plst需配置NSContactsUsageDescription
 static const FWAuthorizeType FWAuthorizeTypeContacts = 9;
--- FWFramework/Classes/Component/Tracking/FWAuthorizeTracking.h
+++ FWFramework/Classes/Component/Tracking/FWAuthorizeTracking.h
@@ -1,12 +1,16 @@
 //
-//  FWAuthorize.h
+//  FWAuthorizeTracking.h
 //  FWFramework
 //
 //  Created by wuyong on 17/3/15.
 //  Copyright © 2018年 wuyong.site. All rights reserved.
 //
 
+#if FWFrameworkTracking
+@import FWFramework;
+#else
 #import "FWAuthorize.h"
+#endif
 
 /// 广告跟踪，需启用Tracking子模块，Info.plst需配置NSUserTrackingUsageDescription
 static const FWAuthorizeType FWAuthorizeTypeTracking = 11;
