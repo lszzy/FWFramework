82f3eafc8d5ca76d71025ce5c345da09fa7ce13e
wuyong <lingshizhuangzi@gmail.com>
2022-02-09 23:21:14 +0800

    feat: 新增等比例缩放字体

--- FWFramework/Classes/FWFramework/Toolkit/FWRelative.h
+++ FWFramework/Classes/FWFramework/Toolkit/FWRelative.h
@@ -39,4 +39,50 @@ CG_INLINE CGFloat FWRelativeValue(CGFloat value) {
     return [UIScreen fwRelativeValue:value];
 }
 
+#pragma mark - UIFont+FWRelative
+
+/// 快速创建等比例缩放系统字体，字重可选，默认Regular
+#define FWFontRelative( size, ... ) \
+    [UIFont fwFontRelative:size weight:fw_macro_default(UIFontWeightRegular, ##__VA_ARGS__)]
+
+/// 快速创建等比例缩放Thin字体
+FOUNDATION_EXPORT UIFont * FWFontThinRelative(CGFloat size);
+/// 快速创建等比例缩放Light字体
+FOUNDATION_EXPORT UIFont * FWFontLightRelative(CGFloat size);
+/// 快速创建等比例缩放Regular字体
+FOUNDATION_EXPORT UIFont * FWFontRegularRelative(CGFloat size);
+/// 快速创建等比例缩放Medium字体
+FOUNDATION_EXPORT UIFont * FWFontMediumRelative(CGFloat size);
+/// 快速创建等比例缩放Semibold字体
+FOUNDATION_EXPORT UIFont * FWFontSemiboldRelative(CGFloat size);
+/// 快速创建等比例缩放Bold字体
+FOUNDATION_EXPORT UIFont * FWFontBoldRelative(CGFloat size);
+/// 快速创建等比例缩放斜体字体
+FOUNDATION_EXPORT UIFont * FWFontItalicRelative(CGFloat size);
+
+/**
+ UIFont+FWRelative
+ */
+@interface UIFont (FWRelative)
+
+/// 返回等比例缩放系统Thin字体
++ (UIFont *)fwThinFontRelative:(CGFloat)size;
+/// 返回等比例缩放系统Light字体
++ (UIFont *)fwLightFontRelative:(CGFloat)size;
+/// 返回等比例缩放系统Regular字体
++ (UIFont *)fwFontRelative:(CGFloat)size;
+/// 返回等比例缩放系统Medium字体
++ (UIFont *)fwMediumFontRelative:(CGFloat)size;
+/// 返回等比例缩放系统Semibold字体
++ (UIFont *)fwSemiboldFontRelative:(CGFloat)size;
+/// 返回等比例缩放系统Bold字体
++ (UIFont *)fwBoldFontRelative:(CGFloat)size;
+/// 返回等比例缩放系统斜体字体
++ (UIFont *)fwItalicFontRelative:(CGFloat)size;
+
+/// 创建指定尺寸和weight的等比例缩放系统字体
++ (UIFont *)fwFontRelative:(CGFloat)size weight:(UIFontWeight)weight;
+
+@end
+
 NS_ASSUME_NONNULL_END
