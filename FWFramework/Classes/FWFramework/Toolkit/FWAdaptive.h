1f8da1577ee08a1b647f600039ab18dd19864bf4
wuyong <lingshizhuangzi@gmail.com>
2022-02-10 10:30:33 +0800

    feat: 新增快捷等比例缩放适配方法

--- FWFramework/Classes/FWFramework/Toolkit/FWAdaptive.h
+++ FWFramework/Classes/FWFramework/Toolkit/FWAdaptive.h
@@ -154,8 +154,16 @@ static const FWScreenInch FWScreenInch67 = 67;
 /// 当前屏幕高度缩放比例
 #define FWRelativeHeightScale [UIScreen fwRelativeHeightScale]
 
-/// 获取相对设计图宽度等比例缩放值
-FOUNDATION_EXPORT CGFloat FWRelativeValue(CGFloat value);
+/// 获取相对设计图等比例缩放值
+FOUNDATION_EXPORT CGFloat FWRelativeValue(CGFloat value) NS_SWIFT_UNAVAILABLE("");
+/// 获取相对设计图等比例缩放size
+FOUNDATION_EXPORT CGSize FWRelativeSize(CGSize size) NS_SWIFT_UNAVAILABLE("");
+/// 获取相对设计图等比例缩放point
+FOUNDATION_EXPORT CGPoint FWRelativePoint(CGPoint point) NS_SWIFT_UNAVAILABLE("");
+/// 获取相对设计图等比例缩放rect
+FOUNDATION_EXPORT CGRect FWRelativeRect(CGRect rect) NS_SWIFT_UNAVAILABLE("");
+/// 获取相对设计图等比例缩放insets
+FOUNDATION_EXPORT UIEdgeInsets FWRelativeInsets(UIEdgeInsets insets) NS_SWIFT_UNAVAILABLE("");
 
 /// 基于指定的倍数(0取当前设备)，对传进来的floatValue进行像素取整
 CG_INLINE CGFloat FWFlatScale(CGFloat floatValue, CGFloat scale) {
