630a2f9a1c7f1d9a76ae3050ec25e88209892f65
wuyong <lingshizhuangzi@gmail.com>
2022-03-22 23:42:17 +0800

    refactor: 重构FWAutoloader为Swift实现

--- FWFramework/Classes/FWFramework/Kernel/FWAutoloader.m
+++ FWFramework/Classes/FWFramework/Kernel/FWAutoloader.m
@@ -7,7 +7,6 @@
  */
 
 #import "FWAutoloader.h"
-#import <objc/runtime.h>
 
 @protocol FWInnerAutoloadProtocol <NSObject>
 @optional
@@ -20,61 +19,21 @@
 
 @end
 
-BOOL FWAutoload(id clazz) {
-    if ([FWAutoloader respondsToSelector:@selector(autoload:)]) {
-        return [FWAutoloader autoload:clazz];
-    }
-    return NO;
-}
-
-static NSArray<NSString *> *fwStaticAutoloadMethods = nil;
-
 @implementation FWAutoloader
 
 + (void)load
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [FWAutoloader autoload];
+        FWAutoload(FWAutoloader.class);
     });
 }
 
-+ (void)autoload
-{
-    NSMutableArray<NSString *> *methodNames = [NSMutableArray array];
-    unsigned int methodCount = 0;
-    Method *methods = class_copyMethodList([FWAutoloader class], &methodCount);
-    for (unsigned int i = 0; i < methodCount; ++i) {
-        const char *methodChar = sel_getName(method_getName(methods[i]));
-        if (!methodChar) continue;
-        NSString *methodName = [NSString stringWithUTF8String:methodChar];
-        if (![methodName hasPrefix:@"load"]) continue;
-        if ([methodName containsString:@":"]) continue;
-        [methodNames addObject:methodName];
-    }
-    free(methods);
-    [methodNames sortUsingComparator:^NSComparisonResult(NSString *obj1, NSString *obj2) {
-        return [obj1 compare:obj2];
-    }];
-    fwStaticAutoloadMethods = [methodNames copy];
-    
-    FWAutoloader *autoloader = [[FWAutoloader alloc] init];
-    for (NSString *methodName in methodNames) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        [autoloader performSelector:NSSelectorFromString(methodName)];
-#pragma clang diagnostic pop
-    }
-}
-
-+ (NSString *)debugDescription
-{
-    NSMutableString *debugDescription = [[NSMutableString alloc] init];
-    NSInteger debugCount = 0;
-    for (NSString *methodName in fwStaticAutoloadMethods) {
-        [debugDescription appendFormat:@"%@. %@\n", @(++debugCount), methodName];
-    }
-    return [NSString stringWithFormat:@"\n========== AUTOLOADER ==========\n%@========== AUTOLOADER ==========", debugDescription];
-}
-
 @end
+
+BOOL FWAutoload(id clazz) {
+    if ([FWAutoloader respondsToSelector:@selector(autoload:)]) {
+        return [FWAutoloader autoload:clazz];
+    }
+    return NO;
+}
