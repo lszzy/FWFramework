ea73216a586b6084a22f5898ddf81dab9ce0d9d8
wuyong <lingshizhuangzi@gmail.com>
2022-04-01 13:31:02 +0800

    feat: 重构FWEncode对应Swift实现

--- FWFramework/Classes/Module/Compatible/Toolkit/FWFoundation.swift
+++ FWFramework/Classes/Module/Compatible/Toolkit/FWFoundation.swift
@@ -13,7 +13,7 @@ extension Data {
     /// 使用NSKeyedArchiver压缩对象
     public static func fwArchiveObject(_ object: Any) -> Data? { return NSData.fwArchiveObject(object) }
     /// 使用NSKeyedUnarchiver解压数据
-    public func fwUnarchiveObject() -> Any? { return fw.nsdata.fwUnarchiveObject() }
+    public func fwUnarchiveObject() -> Any? { return (self as NSData).fwUnarchiveObject() }
     /// 保存对象归档
     public static func fwArchiveObject(_ object: Any, file: String) { NSData.fwArchiveObject(object, toFile: file) }
     /// 读取对象归档
@@ -26,18 +26,18 @@ extension Date {
     /// 从字符串初始化日期，自定义格式(默认yyyy-MM-dd HH:mm:ss)和时区(默认当前时区)
     public static func fwDate(string: String, format: String? = "yyyy-MM-dd HH:mm:ss", timeZone: TimeZone? = nil) -> Date? { return NSDate.fwDate(with: string) }
     /// 转化为字符串，默认当前时区，格式：yyyy-MM-dd HH:mm:ss
-    public var fwStringValue: String { return fwNSDate.fwStringValue }
+    public var fwStringValue: String { return (self as NSDate).fwStringValue }
     /// 转化为字符串，自定义格式和时区
-    public func fwString(format: String?, timeZone: TimeZone? = nil) -> String { return fwNSDate.fwString(withFormat: format, timeZone: timeZone) }
+    public func fwString(format: String?, timeZone: TimeZone? = nil) -> String { return (self as NSDate).fwString(withFormat: format, timeZone: timeZone) }
     /// 格式化时长，格式"00:00"或"00:00:00"
     public static func fwFormatDuration(_ duration: TimeInterval, hasHour: Bool) -> String { return NSDate.fwFormatDuration(duration, hasHour: hasHour) }
 }
 
 extension String {
     /// 计算多行字符串指定字体、指定属性在指定绘制区域内所占尺寸
-    public func fwSize(font: UIFont, drawSize: CGSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude), attributes: [NSAttributedString.Key: Any]? = nil) -> CGSize { return fw.nsstring.fwSize(with: font, draw: drawSize, attributes: attributes) }
+    public func fwSize(font: UIFont, drawSize: CGSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude), attributes: [NSAttributedString.Key: Any]? = nil) -> CGSize { return (self as NSString).fwSize(with: font, draw: drawSize, attributes: attributes) }
     /// 格式化文件大小为".0K/.1M/.1G"
     public static func fwSizeString(_ fileSize: UInt) -> String { return NSString.fwSizeString(fileSize) }
     /// 是否匹配正则表达式，示例：^[a-zA-Z0-9_\u4e00-\u9fa5]{4,14}$
-    public func fwMatchesRegex(_ regex: String) -> Bool { return fw.nsstring.fwMatchesRegex(regex) }
+    public func fwMatchesRegex(_ regex: String) -> Bool { return (self as NSString).fwMatchesRegex(regex) }
 }
---
+++ FWFramework/Classes/Module/Exception/FWException.m
@@ -0,0 +1,11 @@
+/**
+ @header     FWException.m
+ @indexgroup FWFramework
+ @author     wuyong
+ @copyright  Copyright © 2020 wuyong.site. All rights reserved.
+ @updated    2022/04/01
+ */
+
+#import "FWException.h"
+
+
