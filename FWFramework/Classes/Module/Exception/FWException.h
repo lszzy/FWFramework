84f399015e93271a49c0921cc055ac8e344b001e
wuyong <lingshizhuangzi@gmail.com>
2022-04-01 15:50:36 +0800

    feat: 新增FWException处理

--- FWFramework/Classes/Module/Exception/FWException.h
+++ FWFramework/Classes/Module/Exception/FWException.h
@@ -10,4 +10,15 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// 异常捕获通知，object为NSException对象，userInfo为附加信息
+UIKIT_EXTERN NSNotificationName const FWExceptionCapturedNotification;
+
+/// 框架异常捕获类
+@interface FWException : NSObject
+
+/// 捕获自定义异常并发送通知，可设置备注
++ (void)captureException:(NSException *)exception remark:(nullable NSString *)remark;
+
+@end
+
 NS_ASSUME_NONNULL_END
