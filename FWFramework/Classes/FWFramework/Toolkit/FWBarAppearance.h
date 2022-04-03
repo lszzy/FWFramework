0f7d7c3d5e9351d23db83c89ab3b525cc3959edd
wuyong <lingshizhuangzi@gmail.com>
2022-04-03 13:45:29 +0800

    feat: 去掉fw不支持UI_APPEARANCE_SELECTOR

--- FWFramework/Classes/FWFramework/Toolkit/FWBarAppearance.h
+++ FWFramework/Classes/FWFramework/Toolkit/FWBarAppearance.h
@@ -25,31 +25,31 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)fwUpdateAppearance API_AVAILABLE(ios(13.0));
 
 /// 导航栏是否半透明，会重置背景，需优先设置，默认NO；背景色需带有alpha时半透明才会生效
-@property (nonatomic, assign) BOOL fwIsTranslucent UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwIsTranslucent;
 
 /// 设置前景颜色，包含文字和按钮等
-@property (nonatomic, strong, nullable) UIColor *fwForegroundColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIColor *fwForegroundColor;
 
 /// 单独设置标题颜色，nil时显示前景颜色
-@property (nonatomic, strong, nullable) UIColor *fwTitleColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIColor *fwTitleColor;
 
 /// 设置背景颜色(nil时透明)，兼容主题颜色，后设置生效
-@property (nonatomic, strong, nullable) UIColor *fwBackgroundColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIColor *fwBackgroundColor;
 
 /// 设置背景图片(nil时透明)，兼容主题图片，后设置生效
-@property (nonatomic, strong, nullable) UIImage *fwBackgroundImage UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIImage *fwBackgroundImage;
 
 /// 设置背景是否全透明，默认NO，后设置生效
-@property (nonatomic, assign) BOOL fwBackgroundTransparent UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwBackgroundTransparent;
 
 /// 设置阴影颜色(nil时透明)，兼容主题颜色，后设置生效
-@property (nonatomic, strong, nullable) UIColor *fwShadowColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIColor *fwShadowColor;
 
 /// 设置阴影图片(nil时透明)，兼容主题图片，后设置生效
-@property (nonatomic, strong, nullable) UIImage *fwShadowImage UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIImage *fwShadowImage;
 
 /// 设置返回按钮图片，包含图片和转场Mask图片，自动偏移和系统左侧按钮位置保持一致
-@property (nonatomic, strong, nullable) UIImage *fwBackImage UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIImage *fwBackImage;
 
 @end
 
@@ -104,25 +104,25 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)fwUpdateAppearance API_AVAILABLE(ios(13.0));
 
 /// 工具栏是否半透明，会重置背景，需优先设置，默认NO；背景色需带有alpha时半透明才会生效
-@property (nonatomic, assign) BOOL fwIsTranslucent UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwIsTranslucent;
 
 /// 设置前景颜色，包含文字和按钮等
-@property (nonatomic, strong, nullable) UIColor *fwForegroundColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIColor *fwForegroundColor;
 
 /// 设置背景颜色，兼容主题颜色，后设置生效
-@property (nonatomic, strong, nullable) UIColor *fwBackgroundColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIColor *fwBackgroundColor;
 
 /// 设置背景图片，兼容主题图片，后设置生效
-@property (nonatomic, strong, nullable) UIImage *fwBackgroundImage UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIImage *fwBackgroundImage;
 
 /// 设置背景是否全透明，默认NO，后设置生效
-@property (nonatomic, assign) BOOL fwBackgroundTransparent UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwBackgroundTransparent;
 
 /// 设置阴影颜色(nil时透明)，兼容主题颜色，后设置生效
-@property (nonatomic, strong, nullable) UIColor *fwShadowColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIColor *fwShadowColor;
 
 /// 设置阴影图片(nil时透明)，兼容主题图片，后设置生效
-@property (nonatomic, strong, nullable) UIImage *fwShadowImage UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong, nullable) UIImage *fwShadowImage;
 
 /// 自定义工具栏位置，调用后才生效，会自动设置delegate。Bottom时背景自动向下延伸，TopAttached时背景自动向上延伸
 @property (nonatomic, assign) UIBarPosition fwBarPosition;
--- FWFramework/Classes/FWFramework/Toolkit/FWKeyboard.h
+++ FWFramework/Classes/FWFramework/Toolkit/FWKeyboard.h
@@ -19,16 +19,16 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark - Keyboard
 
 /// 是否启用键盘管理(自动滚动)，默认NO
-@property (nonatomic, assign) BOOL fwKeyboardManager UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwKeyboardManager;
 
 /// 设置输入框和键盘的空白高度，默认10.0
-@property (nonatomic, assign) CGFloat fwKeyboardSpacing UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) CGFloat fwKeyboardSpacing;
 
 /// 是否启用键盘后台关闭处理，退后台时收起键盘，回到前台时恢复键盘，解决系统退后台输入框跳动问题，默认NO
-@property (nonatomic, assign) BOOL fwKeyboardResign UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwKeyboardResign;
 
 /// 是否启用点击背景关闭键盘(会继续触发其它点击事件)，默认NO
-@property (nonatomic, assign) BOOL fwTouchResign UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwTouchResign;
 
 /// 指定用于键盘管理滚动的scrollView，默认为nil，通过修改VC.view.frame实现
 @property (nullable, nonatomic, weak) UIScrollView *fwKeyboardScrollView;
@@ -62,16 +62,16 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark - Keyboard
 
 /// 是否启用键盘管理(自动滚动)，默认NO
-@property (nonatomic, assign) BOOL fwKeyboardManager UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwKeyboardManager;
 
 /// 设置输入框和键盘的空白高度，默认10.0
-@property (nonatomic, assign) CGFloat fwKeyboardSpacing UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) CGFloat fwKeyboardSpacing;
 
 /// 是否启用键盘后台关闭处理，退后台时收起键盘，回到前台时恢复键盘，解决系统退后台输入框跳动问题，默认NO
-@property (nonatomic, assign) BOOL fwKeyboardResign UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwKeyboardResign;
 
 /// 是否启用点击背景关闭键盘(会继续触发其它点击事件)，默认NO
-@property (nonatomic, assign) BOOL fwTouchResign UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) BOOL fwTouchResign;
 
 /// 指定用于键盘管理滚动的scrollView，默认为nil，通过修改VC.view.frame实现
 @property (nullable, nonatomic, weak) UIScrollView *fwKeyboardScrollView;
