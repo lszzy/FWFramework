29768ec14e6dea16bba0edc389e1fc14fd70fe28
wuyong <lingshizhuangzi@gmail.com>
2022-04-19 14:06:00 +0800

    feat: 优化FWKeyboard键盘管理默认Toolbar

--- FWFramework/Classes/FWFramework/Toolkit/FWKeyboard.m
+++ FWFramework/Classes/FWFramework/Toolkit/FWKeyboard.m
@@ -26,7 +26,7 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
 
 @property (nonatomic, assign) BOOL keyboardManager;
 @property (nonatomic, assign) CGFloat keyboardDistance;
-@property (nonatomic, assign) CGFloat reboundHeight;
+@property (nonatomic, assign) CGFloat reboundDistance;
 @property (nonatomic, assign) BOOL keyboardResign;
 @property (nonatomic, assign) BOOL touchResign;
 
@@ -35,6 +35,9 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
 @property (nonatomic, copy) void (^returnBlock)(id textInput);
 
 @property (nonatomic, strong) UIToolbar *keyboardToolbar;
+@property (nonatomic, strong) id toolbarPreviousButton;
+@property (nonatomic, strong) id toolbarNextButton;
+@property (nonatomic, strong) id toolbarDoneButton;
 @property (nonatomic, weak) UIResponder *previousResponder;
 @property (nonatomic, weak) UIResponder *nextResponder;
 
@@ -208,6 +211,9 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
         CGRect convertRect = [self.textInput convertRect:self.textInput.bounds toView:convertView];
         CGPoint contentOffset = self.scrollView.contentOffset;
         CGFloat targetOffsetY = MAX(contentOffset.y + self.keyboardDistance + CGRectGetMaxY(convertRect) - CGRectGetMinY(keyboardRect), fwStaticKeyboardOffset);
+        if (self.reboundDistance > 0 && targetOffsetY < contentOffset.y) {
+            targetOffsetY = (targetOffsetY + self.reboundDistance >= contentOffset.y) ? contentOffset.y : targetOffsetY + self.reboundDistance;
+        }
         
         contentOffset.y = targetOffsetY;
         [UIView animateWithDuration:animationDuration delay:0 options:(animationCurve | UIViewAnimationOptionBeginFromCurrentState) animations:^{
@@ -229,6 +235,9 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     CGRect convertRect = [self.textInput convertRect:self.textInput.bounds toView:convertView];
     CGRect viewFrame = self.viewController.view.frame;
     CGFloat viewTargetY = MIN(viewFrame.origin.y - self.keyboardDistance + CGRectGetMinY(keyboardRect) - CGRectGetMaxY(convertRect), fwStaticKeyboardOrigin);
+    if (self.reboundDistance > 0 && viewTargetY > viewFrame.origin.y) {
+        viewTargetY = (viewTargetY - self.reboundDistance <= viewFrame.origin.y) ? viewFrame.origin.y : viewTargetY - self.reboundDistance;
+    }
     
     viewFrame.origin.y = viewTargetY;
     [UIView animateWithDuration:animationDuration delay:0 options:(animationCurve | UIViewAnimationOptionBeginFromCurrentState) animations:^{
@@ -293,36 +302,49 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     return _keyboardToolbar;
 }
 
+- (id)toolbarPreviousButton
+{
+    if (!_toolbarPreviousButton) {
+        _toolbarPreviousButton = [self.class toolbarPreviousImage];
+    }
+    return _toolbarPreviousButton;
+}
+
+- (id)toolbarNextButton
+{
+    if (!_toolbarNextButton) {
+        _toolbarNextButton = [self.class toolbarNextImage];
+    }
+    return _toolbarNextButton;
+}
+
+- (id)toolbarDoneButton
+{
+    if (!_toolbarDoneButton) {
+        _toolbarDoneButton = @(UIBarButtonSystemItemDone);
+    }
+    return _toolbarDoneButton;
+}
+
 - (void)addToolbarWithTitle:(id)title
-             previousButton:(id)previousButton
-                 nextButton:(id)nextButton
-                rightButton:(id)rightButton
-                      block:(void (^)(id sender))block
+                  doneBlock:(void (^)(id sender))doneBlock
 {
     UIBarButtonItem *titleItem = title ? [UIBarButtonItem.fw itemWithObject:title block:nil] : nil;
     titleItem.enabled = NO;
-    UIBarButtonItem *previousItem = previousButton ? [UIBarButtonItem.fw itemWithObject:previousButton target:self.previousResponder action:@selector(becomeFirstResponder)] : nil;
+    UIBarButtonItem *previousItem = [UIBarButtonItem.fw itemWithObject:self.toolbarPreviousButton target:self.previousResponder action:@selector(becomeFirstResponder)];
     previousItem.enabled = self.previousResponder != nil;
-    UIBarButtonItem *nextItem = nextButton ? [UIBarButtonItem.fw itemWithObject:nextButton target:self.nextResponder action:@selector(becomeFirstResponder)] : nil;
+    UIBarButtonItem *nextItem = [UIBarButtonItem.fw itemWithObject:self.toolbarNextButton target:self.nextResponder action:@selector(becomeFirstResponder)];
     nextItem.enabled = self.nextResponder != nil;
+    UIBarButtonItem *doneItem = doneBlock ? [UIBarButtonItem.fw itemWithObject:self.toolbarDoneButton block:doneBlock] : [UIBarButtonItem.fw itemWithObject:self.toolbarDoneButton target:self.textInput action:@selector(resignFirstResponder)];
+    doneItem.style = UIBarButtonItemStyleDone;
     
-    UIBarButtonItem *rightItem;
-    if (rightButton) {
-        if (block) {
-            rightItem = [UIBarButtonItem.fw itemWithObject:rightButton block:block];
-            rightItem.style = UIBarButtonItemStyleDone;
-        } else {
-            rightItem = [UIBarButtonItem.fw itemWithObject:rightButton target:self.textInput action:@selector(resignFirstResponder)];
-        }
-    }
-    
-    [self addToolbarWithTitleItem:titleItem previousItem:previousItem nextItem:nextItem rightItem:rightItem];
+    [self addToolbarWithTitleItem:titleItem previousItem:previousItem nextItem:nextItem doneItem:doneItem];
 }
 
-- (void)addToolbarWithTitleItem:(nullable UIBarButtonItem *)titleItem
-                   previousItem:(nullable UIBarButtonItem *)previousItem
-                       nextItem:(nullable UIBarButtonItem *)nextItem
-                      rightItem:(nullable UIBarButtonItem *)rightItem
+- (void)addToolbarWithTitleItem:(UIBarButtonItem *)titleItem
+                   previousItem:(UIBarButtonItem *)previousItem
+                       nextItem:(UIBarButtonItem *)nextItem
+                       doneItem:(UIBarButtonItem *)doneItem
 {
     NSMutableArray<UIBarButtonItem *> *items = [NSMutableArray array];
     if (previousItem) [items addObject:previousItem];
@@ -337,12 +359,38 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
         [items addObject:titleItem];
         [items addObject:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil]];
     }
-    if (rightItem) [items addObject:rightItem];
+    if (doneItem) [items addObject:doneItem];
     
     UIToolbar *toolbar = self.keyboardToolbar;
     toolbar.items = [items copy];
     [toolbar sizeToFit];
     ((UITextField *)self.textInput).inputAccessoryView = toolbar;
+}
+
+// MARK: - Image
+
++ (UIImage *)toolbarPreviousImage
+{
+    static UIImage *previousImage = nil;
+    if (previousImage == nil) {
+        NSString *base64Data = @"iVBORw0KGgoAAAANSUhEUgAAAD8AAAAkCAYAAAA+TuKHAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAABWWlUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iWE1QIENvcmUgNS40LjAiPgogICA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogICAgICA8cmRmOkRlc2NyaXB0aW9uIHJkZjphYm91dD0iIgogICAgICAgICAgICB4bWxuczp0aWZmPSJodHRwOi8vbnMuYWRvYmUuY29tL3RpZmYvMS4wLyI+CiAgICAgICAgIDx0aWZmOk9yaWVudGF0aW9uPjE8L3RpZmY6T3JpZW50YXRpb24+CiAgICAgIDwvcmRmOkRlc2NyaXB0aW9uPgogICA8L3JkZjpSREY+CjwveDp4bXBtZXRhPgpMwidZAAAGmklEQVRoBd1ZWWwbRRie2bVz27s2adPGxzqxqAQCIRA3CDVJGxpKaEtRoSAVISQQggdeQIIHeIAHkOCBFyQeKlARhaYHvUJa0ksVoIgKUKFqKWqdeG2nR1Lsdeo0h73D54iku7NO6ySOk3alyPN//+zM/81/7MyEkDl66j2eJXWK8vocTT82rTgXk/t8vqBNEI9QSp9zOeVkPJnomgs7ik5eUZQ6OxGOEEq9WcKUksdlWbqU0LRfi70ARSXv8Xi8dkE8CsJ+I1FK6BNYgCgW4A8jPtvtopFHqNeWCLbDIF6fkxQjK91O1z9IgRM59bMAFoV8YEFgka1EyBJfMhkH5L9ACFstS9IpRMDJyfoVEp918sGamoVCme0QyN3GG87wAKcTOBYA4hrJKf+VSCb+nsBnqYHVnr2ntra2mpWWH0BVu52fhRH2XSZDmsA/xensokC21Pv9T3J4wcWrq17gob1er7tEhMcJuYsfGoS3hdTweuBpxaM0iCJph8fLuX7DJMPWnI2GOzi8YOKseD4gB+RSQezMRRx5vRPEn88Sz7IIx8KHgT3FCBniWJUyke6o8/uXc3jBxIKTd7vdTsFJfkSo38NbCY/vPRsOPwt81KgLqeoBXc+sBjZsxLF4ZfgM7goqSqMRL1S7oOSrq6sdLodjH0rYfbyByPEOePwZ4CO8Liv3RCL70Wctr8+mA2NkT53P91iu92aCFYx8TU1NpbOi8gfs2R7iDYLxnXqYPg3c5Fm+Xygcbs/omXXATZGBBagQqNAe9Psf4d+ZiVwQ8qjqFVVl5dmi9ShvDEL90IieXtVDevic5ruOyYiAXYiA9YSxsZow0YnSKkKFjoAn8OAENsPGjKs9qnp5iSDuBXFLXsLjR4fSIy29vb2DU7UThW4d8n0zxjXtRVAYNaJnlocikWNTHZPvP1PPl2LLujM3cfbzwJXUyukQzxrZraptRCcbEDm60Wh4S0IE7McByVJQjf3yac+EfEm9ouxAcWu2TsS6koOplr6+vstWXf5IKBrejBR4ybIAlLpE1JE6j8eyh8h/dEKmS95e7w9sy57G+MkQ6sdYMrmiv79/gNdNR0YEbGKUvIIFQMRffRBtbkG0HQj6fHdcRafWmg55Gzy+BR5vtUzF2O96kjSH4nHNopsB0B0Ob6SEvcYvAPYS1UwQDyqLFcu5IZ/pTMUkjxfEoD/wLVY9+z02PXDL8RE9s0y9qMZNigIJcU37TZblfj7aUAMqURLXuqqq9sQHBi5NZbqpkBfh8a9BPLtDMz3wyImh9GhTLBab0uSmQfIQcNQ95pJkDVG3wtgdC1KFA+HaSodjdzKZ/Neou1Y7X/JC0K98BeIvWAdjp+jwUKN6/nyfVVd4JK4lunDrkwJhc6Gl1GGjwhqnLO3UNC2Rz8z5kKfw+EYQf5EfEKF+Wh+kDd0XYxd43WzKiIBfEAEjiIAm0zyUSFiU1XJF+feJy5evW3euR57C41+A+MumSbICY2dGmd6gnlPPWXRFABABP7llCXsA2mCcDjVAJoK4qryycsfAwEDSqOPb1yQPj38O4q/yL4F4aCiTXhqNRmMWXREBFMGjslOywUbToQeyyy4IrVVO53bUgEk/uZOSr/MHPsOd0hs8F4R6mI2ONKi9vRFeNxdyIqkddknOMhA2nyuy+wAqtEol8rbEYCLnZisneXj8UxB/00KGkUiGsqU90WiPRTeHACLgoNsp4eBDHzaagRS4RbCzle6ysq3xVIq/LiMW8ti5fYRVfMs4yFibsdgI05eqqhqy6OYBEE9qnSiCLhRB7tRHFzDR1oIasBU1wHTAMpHHjcmHIP4OzwXf8XMkk24IR6NneN18klEE97mc0gJwuN9oF+SFNlF8vNJR1YYacGVcN0Eet6XvY6Pw3rhi/Bc5fiEzShp7eiOnx7H5/IsI6EAELEIE3Gu0EymwyCbQZocktWEfMHa3MEa+zqe8KwjCB8bO/7f70kxvVGPqyRy6eQshAtpdsuTDN/9us5F0MQ4zTS5BaIsPDQ3jO+5/G+fjj82dIDF2CZeKjd3R6J8W3Y0BYFca+JJQssFqLuvSUqlmESHSiZywGzsgx+OZNFnWE4scN+I3WJshAnYjAm5FBNxptp16y+y2hICLEtOVMXJcI0xvDveGi/ofU7NxBZN0XIpuIIy0mUZkZNNZVf1kDAt6lZagEhjGnxbweh8wdbw5hOwdxHbwY/j9BpTM9xi4MGzFvZhpk3Bz8J5gkb19ym7cJr5w/wEmUjzJqoNVhwAAAABJRU5ErkJggg==";
+        
+        NSData *data = [[NSData alloc] initWithBase64EncodedString:base64Data options:NSDataBase64DecodingIgnoreUnknownCharacters];
+        previousImage = [[UIImage imageWithData:data scale:3] imageFlippedForRightToLeftLayoutDirection];
+    }
+    return previousImage;
+}
+
++ (UIImage *)toolbarNextImage
+{
+    static UIImage *nextImage = nil;
+    if (nextImage == nil) {
+        NSString *base64Data = @"iVBORw0KGgoAAAANSUhEUgAAAD8AAAAkCAYAAAA+TuKHAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAABWWlUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iWE1QIENvcmUgNS40LjAiPgogICA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogICAgICA8cmRmOkRlc2NyaXB0aW9uIHJkZjphYm91dD0iIgogICAgICAgICAgICB4bWxuczp0aWZmPSJodHRwOi8vbnMuYWRvYmUuY29tL3RpZmYvMS4wLyI+CiAgICAgICAgIDx0aWZmOk9yaWVudGF0aW9uPjE8L3RpZmY6T3JpZW50YXRpb24+CiAgICAgIDwvcmRmOkRlc2NyaXB0aW9uPgogICA8L3JkZjpSREY+CjwveDp4bXBtZXRhPgpMwidZAAAGp0lEQVRoBd1ZCWhcRRiemff25WrydmOtuXbfZlMo4lEpKkppm6TpZUovC4UqKlQoUhURqQcUBcWDIkhVUCuI9SpJa+2h0VZjUawUEUUUirLNXqmxSnc32WaT7O4bv0nd5R1bc+2maR8s7z9m5v+/+f/5Z94sIf89jW73Yp/bfUuWvwLfDp/H8zhwObLYmCCaPJ6FjLJPCWNHNU1bkFVeQW/Zp2l7KWUvNmlaB3DJAhvz1ntvI5R1EUpnUUKdEifHGuvr519BwKUmj/cDYNtwARNd5/NoH4GWKIhzlFKXCSzn/xCut/jD4V9N8suPYYj4ewC+2e46f55Rwp/geExKSmdzJn2l1WrXmuSXF8MQ8XfyAeeEn9KTyV3MHwq9RTh50IqLEjJHUkh3Y13dPKvuMuApIr6bUHKP1VeE+Y8MIa09Z8/+JQlltD/+Q7VaFcW6X2VsjFmbRRnbUFFZeai/v/+cUTeDaYqIv4GlfL/NR879I3qmORwOnxG6UfCCiMbjJ51VagKdlgs+91BaKVO6oVJVD8bj8WhOPkMJn1t7jTL6gNU9pHpgKJ1q7u3tjWR1OfBCEOuPf+9Sq4YwAW3ZBqNvSqsYpeuc5WUHYolE3KSbQYzP430FwB+yuoSCFtKHaXP4z3DIqDOBFwpkwHfVThXLgrYaG6IGOAmT1pZVVHw8MDDQb9TNBLrJre0E8EdtvnAeSRPeHOwN9lh1NvCiASbgG5fqRLDJEmMHsSU6GFuDGrAfNWDAqLuUNE5uL6A2bbf5wPkZrmdaAuGw36aDIC940TAajx1HBijIgEWmjpRWS4ytrnKq+1EDEibdJWAa3dqzjLGnrKaxxvt4OtXS09v7u1WX5S8KXjRABnQ7VbUCEV+Y7SDeWAJX4dfuLCnZFzt//rxRN500jqo74NvTVptY42fTnLcGI5FTVp2R/1/womEsHj/mwgxg27vd2BH8bCrLq0rKyjoTicSgUTcdNIrbkwD+nM2WOJ3qmaVI9d9sOotgTPCiPTLgi+oqdTbOAbea+lM6xyHLK8pnVXSiCCZNuiIyjZr2GArSS1YTOKie45n0UqT6L1ZdPn5c4EVHHIS6sA3WYLZvNg6E9L9GZmwZzgEdqAFDRl0xaET8EQB/2To21ngsQ0kbIv6zVXcxftzgxQDIgM+qVbUeGbDAPCCtxbfxUhdjHdGhoWGzrnAcIr4NwHflGbGf6PqyQCj0Yx7dRUUTAi9GwQQccapOL7bBm4yjIiPqSElpC5VYRzKZLPgE4M5hK0rt67CDZDM9A+k0XxmIhE6apONgJgxejBmLxw65VHUu/LjRaANeNZQpyhJZUToGBwdHjLqp0Ij4FgB/0wocaxw7DV8F4CcmM/6kwMMQRwYcrFad87DvXW8yTKlbkZVFSmlJB3bBlEk3CQYRvxfA3wbw0Vun7BAAPqjrmfaecPjbrGyib2sKTbS/LG5F4NhGe0d+fDiTuSMSiUx6F8Bn6V343N6TB3gSyb/aHwx22+2OX2KazfF3y7VMnw4FcUvCP8lJcgRtVph0yEu8pTnRBAiv270JwN+1AscQw5zr66YKXLgyVfBijBQc2YQ0PCIY4wPH2yQPERNTYpSPRSPid0qUvY/+1mU5QjJ8PVL96FhjjEdfCPDCzggyAKnPP7cZpWQFlsZ+yPGdMPaDiK/F6fEjbKeypXVK5/pGfyTYZZFPmi0UeOHAcCZI1+Oa6JjVG0SwHbcrnZDn7sytbQSPiLdLTBJXy+Z2nKcR8U09odDhfP0mKyskeBIggaERPb0WGfC1zSFK1gDcXsitER1t6m3wrkTEbRmC5ZTRCd+MiB+wjTlFwVSrfV7zdXV15aWy0oWKvNjWgJMOfyiAIklwYXLhwfd4G/47OAxnTMVRAKec3u0PB8SkFfyxFpSCGMBHTkpWHPsU2bEEKe8xDUrJdfhKnItzgiiEXKvXWhijR9CuzNgOwHWc1+87HQ5+aJQXki4KeOGgOOFJDkdnqeJowSGlweg00vsGHJAa1UpnTJKIAF5u1AM4R8S3APgeo7zQdFHS3uikz+VSSWXVlwBo+hoUbUR0ITfVHQEcEd+K4rbbOE4xaJPhYhg4HY3GcYG4HFB/so5vBT6q53TbdAAXtooe+SzghoaGakWSu2FwflZmfWMffxjAX7XKi8VPG3gBoKam5uoKpeQEDjBz7YD4dpwUd9rlxZMUPe2Nrvf19f2dTKdasap7jHIsiR3TDdxsfxq5xtpazad5g02al+Na6plpND0zTHk8Hp+4iLyU3vwLp0orLWXqrZQAAAAASUVORK5CYII=";
+        
+        NSData *data = [[NSData alloc] initWithBase64EncodedString:base64Data options:NSDataBase64DecodingIgnoreUnknownCharacters];
+        nextImage = [[UIImage imageWithData:data scale:3] imageFlippedForRightToLeftLayoutDirection];
+    }
+    return nextImage;
 }
 
 @end
@@ -422,14 +470,14 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     self.fw.innerKeyboardTarget.keyboardDistance = keyboardDistance;
 }
 
-- (CGFloat)innerReboundHeight
+- (CGFloat)innerReboundDistance
 {
-    return self.fw.innerKeyboardTarget.reboundHeight;
+    return self.fw.innerKeyboardTarget.reboundDistance;
 }
 
-- (void)setInnerReboundHeight:(CGFloat)reboundHeight
+- (void)setInnerReboundDistance:(CGFloat)reboundDistance
 {
-    self.fw.innerKeyboardTarget.reboundHeight = reboundHeight;
+    self.fw.innerKeyboardTarget.reboundDistance = reboundDistance;
 }
 
 - (BOOL)innerKeyboardResign
@@ -497,14 +545,14 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     self.base.innerKeyboardDistance = keyboardDistance;
 }
 
-- (CGFloat)reboundHeight
+- (CGFloat)reboundDistance
 {
-    return self.base.innerReboundHeight;
+    return self.base.innerReboundDistance;
 }
 
-- (void)setReboundHeight:(CGFloat)reboundHeight
+- (void)setReboundDistance:(CGFloat)reboundDistance
 {
-    self.base.innerReboundHeight = reboundHeight;
+    self.base.innerReboundDistance = reboundDistance;
 }
 
 - (BOOL)keyboardResign
@@ -602,6 +650,36 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     self.innerKeyboardTarget.keyboardToolbar = keyboardToolbar;
 }
 
+- (id)toolbarPreviousButton
+{
+    return self.innerKeyboardTarget.toolbarPreviousButton;
+}
+
+- (void)setToolbarPreviousButton:(id)toolbarPreviousButton
+{
+    self.innerKeyboardTarget.toolbarPreviousButton = toolbarPreviousButton;
+}
+
+- (id)toolbarNextButton
+{
+    return self.innerKeyboardTarget.toolbarNextButton;
+}
+
+- (void)setToolbarNextButton:(id)toolbarNextButton
+{
+    self.innerKeyboardTarget.toolbarNextButton = toolbarNextButton;
+}
+
+- (id)toolbarDoneButton
+{
+    return self.innerKeyboardTarget.toolbarDoneButton;
+}
+
+- (void)setToolbarDoneButton:(id)toolbarDoneButton
+{
+    self.innerKeyboardTarget.toolbarDoneButton = toolbarDoneButton;
+}
+
 - (UIResponder *)previousResponder
 {
     return self.innerKeyboardTarget.previousResponder;
@@ -623,20 +701,17 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
 }
 
 - (void)addToolbarWithTitle:(id)title
-             previousButton:(id)previousButton
-                 nextButton:(id)nextButton
-                rightButton:(id)rightButton
-                      block:(void (^)(id))block
+                  doneBlock:(void (^)(id sender))doneBlock
 {
-    [self.innerKeyboardTarget addToolbarWithTitle:title previousButton:previousButton nextButton:nextButton rightButton:rightButton block:block];
+    [self.innerKeyboardTarget addToolbarWithTitle:title doneBlock:doneBlock];
 }
 
 - (void)addToolbarWithTitleItem:(UIBarButtonItem *)titleItem
                    previousItem:(UIBarButtonItem *)previousItem
                        nextItem:(UIBarButtonItem *)nextItem
-                      rightItem:(UIBarButtonItem *)rightItem
+                       doneItem:(UIBarButtonItem *)doneItem
 {
-    [self.innerKeyboardTarget addToolbarWithTitleItem:titleItem previousItem:previousItem nextItem:nextItem rightItem:rightItem];
+    [self.innerKeyboardTarget addToolbarWithTitleItem:titleItem previousItem:previousItem nextItem:nextItem doneItem:doneItem];
 }
 
 @end
@@ -677,14 +752,14 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     self.fw.innerKeyboardTarget.keyboardDistance = keyboardDistance;
 }
 
-- (CGFloat)innerReboundHeight
+- (CGFloat)innerReboundDistance
 {
-    return self.fw.innerKeyboardTarget.reboundHeight;
+    return self.fw.innerKeyboardTarget.reboundDistance;
 }
 
-- (void)setInnerReboundHeight:(CGFloat)reboundHeight
+- (void)setInnerReboundDistance:(CGFloat)reboundDistance
 {
-    self.fw.innerKeyboardTarget.reboundHeight = reboundHeight;
+    self.fw.innerKeyboardTarget.reboundDistance = reboundDistance;
 }
 
 - (BOOL)innerKeyboardResign
@@ -752,14 +827,14 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     self.base.innerKeyboardDistance = keyboardDistance;
 }
 
-- (CGFloat)reboundHeight
+- (CGFloat)reboundDistance
 {
-    return self.base.innerReboundHeight;
+    return self.base.innerReboundDistance;
 }
 
-- (void)setReboundHeight:(CGFloat)reboundHeight
+- (void)setReboundDistance:(CGFloat)reboundDistance
 {
-    self.base.innerReboundHeight = reboundHeight;
+    self.base.innerReboundDistance = reboundDistance;
 }
 
 - (BOOL)keyboardResign
@@ -896,6 +971,36 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
     self.innerKeyboardTarget.keyboardToolbar = keyboardToolbar;
 }
 
+- (id)toolbarPreviousButton
+{
+    return self.innerKeyboardTarget.toolbarPreviousButton;
+}
+
+- (void)setToolbarPreviousButton:(id)toolbarPreviousButton
+{
+    self.innerKeyboardTarget.toolbarPreviousButton = toolbarPreviousButton;
+}
+
+- (id)toolbarNextButton
+{
+    return self.innerKeyboardTarget.toolbarNextButton;
+}
+
+- (void)setToolbarNextButton:(id)toolbarNextButton
+{
+    self.innerKeyboardTarget.toolbarNextButton = toolbarNextButton;
+}
+
+- (id)toolbarDoneButton
+{
+    return self.innerKeyboardTarget.toolbarDoneButton;
+}
+
+- (void)setToolbarDoneButton:(id)toolbarDoneButton
+{
+    self.innerKeyboardTarget.toolbarDoneButton = toolbarDoneButton;
+}
+
 - (UIResponder *)previousResponder
 {
     return self.innerKeyboardTarget.previousResponder;
@@ -917,20 +1022,17 @@ static UITapGestureRecognizer *fwStaticKeyboardGesture = nil;
 }
 
 - (void)addToolbarWithTitle:(id)title
-             previousButton:(id)previousButton
-                 nextButton:(id)nextButton
-                rightButton:(id)rightButton
-                      block:(void (^)(id))block
+                  doneBlock:(void (^)(id sender))doneBlock
 {
-    [self.innerKeyboardTarget addToolbarWithTitle:title previousButton:previousButton nextButton:nextButton rightButton:rightButton block:block];
+    [self.innerKeyboardTarget addToolbarWithTitle:title doneBlock:doneBlock];
 }
 
 - (void)addToolbarWithTitleItem:(UIBarButtonItem *)titleItem
                    previousItem:(UIBarButtonItem *)previousItem
                        nextItem:(UIBarButtonItem *)nextItem
-                      rightItem:(UIBarButtonItem *)rightItem
+                       doneItem:(UIBarButtonItem *)doneItem
 {
-    [self.innerKeyboardTarget addToolbarWithTitleItem:titleItem previousItem:previousItem nextItem:nextItem rightItem:rightItem];
+    [self.innerKeyboardTarget addToolbarWithTitleItem:titleItem previousItem:previousItem nextItem:nextItem doneItem:doneItem];
 }
 
 @end
