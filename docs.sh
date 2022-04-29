ac5d5698b33aeb03e4079e73bf4b16e469144426
wuyong <lingshizhuangzi@gmail.com>
2022-04-29 22:15:50 +0800

    fix: 修复docs.sh脚本路径问题

--- docs.sh
+++ docs.sh
@@ -10,13 +10,15 @@ mkdir "docs/$framework_name"
 
 umbrella_name="$framework_name-umbrella.h"
 umbrella_path=Example/Pods/Target\ Support\ Files
-cp "$umbrella_path/$framework_name/$umbrella_name" "docs/$framework_name/"
+lib_path="docs/$framework_name/"
+sdk_path=$(xcrun --show-sdk-path --sdk iphonesimulator)
+cp "$umbrella_path/$framework_name/$umbrella_name" $lib_path
 find "$framework_name/Classes" -type f ! -regex '*.h' -name '*.h' \
-    -exec cp {} "docs/$framework_name/" \;
+    -exec cp {} $lib_path \;
 
-sourcekitten doc -- -project _Pods.xcodeproj -target $framework_name > "docs/$framework_name/swift.json"
-sourcekitten doc --objc "docs/$framework_name/$umbrella_name" -- -x objective-c -isysroot $(xcrun --show-sdk-path --sdk iphonesimulator) -I $(pwd) -fmodules > "docs/$framework_name/objc.json"
-jazzy --sourcekitten-sourcefile "docs/$framework_name/swift.json","docs/$framework_name/objc.json"
+sourcekitten doc -- -project _Pods.xcodeproj -target $framework_name > "$lib_path/swift.json"
+sourcekitten doc --objc "$lib_path/$umbrella_name" -- -x objective-c -isysroot $sdk_path -I $lib_path -fmodules > "$lib_path/objc.json"
+jazzy --sourcekitten-sourcefile "$lib_path/swift.json","$lib_path/objc.json"
 
-rm -rf "docs/$framework_name"
+rm -rf $lib_path
 cp *.md docs/
