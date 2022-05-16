d6457d9f2e62377ec4688e6e62b1eda2eee94402
wuyong <lingshizhuangzi@gmail.com>
2022-05-16 18:05:09 +0800

    feat: 重构UIKit

--- FWFramework/Classes/Compatible/Toolkit/UIKit.swift
+++ FWFramework/Classes/Compatible/Toolkit/UIKit.swift
@@ -6,6 +6,40 @@
 //
 
 import UIKit
+#if FWMacroTracking
+import AdSupport
+#endif
+
+extension Wrapper where Base: UIDevice {
+    
+    /// 设置设备token原始Data，格式化并保存
+    public static func setDeviceTokenData(_ tokenData: Data?) {
+        Base.__fw.setDeviceTokenData(tokenData)
+    }
+
+    /// 获取设备Token格式化后的字符串
+    public static var deviceToken: String? {
+        return Base.__fw.deviceToken
+    }
+
+    /// 获取设备模型，格式："iPhone6,1"
+    public static var deviceModel: String? {
+        return Base.__fw.deviceModel
+    }
+
+    /// 获取设备IDFV(内部使用)，同账号应用全删除后会改变，可通过keychain持久化
+    public static var deviceIDFV: String? {
+        return Base.__fw.deviceIDFV
+    }
+
+#if FWMacroTracking
+    /// 获取设备IDFA(外部使用)，重置广告或系统后会改变，需先检测广告追踪权限，启用Tracking子模块后生效
+    public static var deviceIDFA: String {
+        return ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    }
+#endif
+    
+}
 
 extension Wrapper where Base: UIView {
     
