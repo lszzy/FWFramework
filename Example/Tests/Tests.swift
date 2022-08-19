9de11b436ca82fdeeeb745c11cf20a854d51c0f8
wuyong <lingshizhuangzi@gmail.com>
2022-08-19 14:51:16 +0800

    test: Runtime单元测试

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -15,6 +15,24 @@ class Tests: XCTestCase {
         XCTAssertEqual(loader.load("Hello "), "Hello Target")
     }
     
+    func testRuntime() {
+        fw.setProperty("Value", forName: "testRuntime")
+        XCTAssertEqual(fw.property(forName: "testRuntime") as? String, "Value")
+        
+        fw.setPropertyWeak(self, forName: "testRuntime2")
+        XCTAssert(fw.property(forName: "testRuntime2") is Tests)
+        
+        fw.tempObject = 1
+        XCTAssertEqual(fw.tempObject as? Int, 1)
+        
+        fw.bindObject(1, forKey: "testRuntime3")
+        XCTAssertEqual(fw.boundInt(forKey: "testRuntime3"), 1)
+        fw.removeBinding(forKey: "testRuntime3")
+        XCTAssertEqual(fw.boundInt(forKey: "testRuntime3"), 0)
+        
+        XCTAssertEqual(fw.invokeMethod(#selector(loaderAction(_:)), object: "Hello ") as? String, "Hello Target")
+    }
+    
 }
 
 // MARK: - Private
