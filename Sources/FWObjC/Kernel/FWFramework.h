f69dc708bf53287c987c4f82723d48e35ad44d1d
wuyong <lingshizhuangzi@gmail.com>
2022-08-22 10:43:50 +0800

    feat: 新增State等类

--- Sources/FWObjC/Kernel/FWFramework.h
+++ Sources/FWObjC/Kernel/FWFramework.h
@@ -6,11 +6,14 @@
 //
 
 #import "FWAutoloader.h"
+#import "FWConfiguration.h"
 #import "FWLoader.h"
 #import "FWLogger.h"
 #import "FWMessage.h"
 #import "FWPlugin.h"
 #import "FWProxy.h"
 #import "FWRuntime.h"
+#import "FWState.h"
 #import "FWSwizzle.h"
+#import "FWTask.h"
 #import "FWTest.h"
