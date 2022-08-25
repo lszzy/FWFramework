c1204ca4114bc7b50bfea064a7db80a0db05ee04
wuyong <lingshizhuangzi@gmail.com>
2022-08-25 12:32:03 +0800

    feat: 新增测试界面

--- Example/FWFramework/Classes/Module/Test/Index/TestController.swift
+++ Example/FWFramework/Classes/Module/Test/Index/TestController.swift
@@ -14,6 +14,7 @@ import Darwin
     var testData: [Any] = [
         ["Kernel", [
             ["Navigator", "TestWorkflowController"],
+            ["Promise", "TestPromiseController"],
         ]],
         ["Service", [
             ["Theme", "TestThemeController"],
