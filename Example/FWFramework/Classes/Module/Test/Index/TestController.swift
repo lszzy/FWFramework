c5b806249b9c98f4fd2431b2281fee20c4e24b00
wuyong <lingshizhuangzi@gmail.com>
2022-08-25 12:26:43 +0800

    feat: 新增测试界面

--- Example/FWFramework/Classes/Module/Test/Index/TestController.swift
+++ Example/FWFramework/Classes/Module/Test/Index/TestController.swift
@@ -38,6 +38,7 @@ import Darwin
             ["Controller", "TestSwiftController"],
             ["Bridge", "TestBridgeController"],
             ["Toolbar", "TestToolbarController"],
+            ["TabBar", "TestTabbarController"],
             ["SwiftUI", "TestSwiftUIController"],
         ]],
     ]
