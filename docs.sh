8c7c711dd18a4fb2f3c2524627dec0e495cb5d9a
wuyong <lingshizhuangzi@gmail.com>
2022-08-27 23:05:30 +0800

    chore: 4.0.0版本

--- docs.sh
+++ docs.sh
@@ -17,8 +17,9 @@ find "Sources/FWObjC" -type f ! -regex '*.h' -name '*.h' \
     -exec cp {} $lib_path \;
 
 sourcekitten doc -- -project _Pods.xcodeproj -target $framework_name > "$lib_path/swift.json"
-sourcekitten doc --objc "$lib_path/$umbrella_name" -- -x objective-c -isysroot $sdk_path -I $lib_path -fmodules > "$lib_path/objc.json"
+sourcekitten doc --objc "$lib_path/$umbrella_name" -- -x objective-c -isysroot $sdk_path -I $lib_path -fobjc-arc -fmodules > "$lib_path/objc.json"
 jazzy --sourcekitten-sourcefile "$lib_path/swift.json","$lib_path/objc.json"
 
 rm -rf $lib_path
+rm -rf Example/build/
 cp *.md docs/
