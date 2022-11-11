e1776948ae461022400d080051aaec41d65d84e2
wuyong <lingshizhuangzi@gmail.com>
2022-11-11 15:39:09 +0800

    refactor: 重构Autoloader

--- Sources/FWObjC/Kernel/FWBridge.m
+++ Sources/FWObjC/Kernel/FWBridge.m
@@ -6,3 +6,29 @@
 //
 
 #import "FWBridge.h"
+
+#pragma mark - __Autoloader
+
+@protocol __AutoloadProtocol <NSObject>
+@optional
+
++ (void)autoload;
+
+@end
+
+@interface __Autoloader () <__AutoloadProtocol>
+
+@end
+
+@implementation __Autoloader
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if ([__Autoloader respondsToSelector:@selector(autoload)]) {
+            [__Autoloader autoload];
+        }
+    });
+}
+
+@end
---
+++ Sources/include/FWObjC/FWBridge.h
@@ -0,0 +1,1 @@
+../../FWObjC/Kernel/FWBridge.h
