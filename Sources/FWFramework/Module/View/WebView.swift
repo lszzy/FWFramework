a5f01920c3a8a5a3cbac411dc85c9396ece30a48
wuyong <lingshizhuangzi@gmail.com>
2022-12-21 12:08:46 +0800

    refactor: 重构WebView

--- Sources/FWFramework/Module/View/WebView.swift
+++ Sources/FWFramework/Module/View/WebView.swift
@@ -10,47 +10,82 @@ import UIKit
 import FWObjC
 #endif
 
-extension Wrapper where Base: UIProgressView {
+@_spi(FW) @objc extension UIProgressView {
     
     /// 设置Web加载进度，0和1自动切换隐藏。可设置trackTintColor为clear，隐藏背景色
-    public var webProgress: Float {
-        get { return base.__fw_webProgress }
-        set { base.__fw_webProgress = newValue }
+    public var fw_webProgress: Float {
+        get {
+            return self.progress
+        }
+        set {
+            if newValue <= 0 {
+                self.alpha = 0
+            } else if newValue > 0 && newValue < 1.0 {
+                if self.alpha == 0 {
+                    self.progress = 0
+                    UIView.animate(withDuration: 0.2) {
+                        self.alpha = 1.0
+                    }
+                }
+            } else {
+                self.alpha = 1.0
+                UIView.animate(withDuration: 0.2) {
+                    self.alpha = 0
+                } completion: { _ in
+                    self.progress = 0
+                }
+            }
+            self.setProgress(newValue, animated: true)
+        }
     }
     
 }
 
-extension Wrapper where Base: WKWebView {
+@_spi(FW) @objc extension WKWebView {
     
     /// 设置Javascript桥接器强引用属性，防止使用过程中被释放
-    public var jsBridge: WebViewJsBridge? {
-        get { return base.__fw_jsBridge }
-        set { base.__fw_jsBridge = newValue }
+    public var fw_jsBridge: WebViewJsBridge? {
+        get { fw_property(forName: "fw_jsBridge") as? WebViewJsBridge }
+        set { fw_setProperty(newValue, forName: "fw_jsBridge") }
     }
 
     /// 获取当前UserAgent，未自定义时为默认，示例：Mozilla/5.0 (iPhone; CPU OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148
-    public var userAgent: String {
-        return base.__fw_userAgent
+    public var fw_userAgent: String {
+        if let userAgent = customUserAgent, !userAgent.isEmpty {
+            return userAgent
+        }
+        if let userAgent = fw_invokeGetter("userAgent") as? String, !userAgent.isEmpty {
+            return userAgent
+        }
+        return WKWebView.fw_browserUserAgent
     }
     
     /// 获取默认浏览器UserAgent，包含应用信息，示例：Mozilla/5.0 (iPhone; CPU OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari/605.1.15 Example/1.0.0
-    public static var browserUserAgent: String {
-        return Base.__fw_browserUserAgent
+    public static var fw_browserUserAgent: String {
+        let platformUserAgent = String(format: "Mozilla/5.0 (%@; CPU OS %@ like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)", UIDevice.current.model, UIDevice.current.systemVersion.replacingOccurrences(of: ".", with: "_"))
+        let userAgent = String(format: "%@ %@", platformUserAgent, fw_extensionUserAgent)
+        return userAgent
     }
 
     /// 获取默认浏览器扩展UserAgent，不含平台信息，可用于applicationNameForUserAgent，示例：Mobile/15E148 Safari/605.1.15 Example/1.0.0
-    public static var extensionUserAgent: String {
-        return Base.__fw_extensionUserAgent
+    public static var fw_extensionUserAgent: String {
+        let userAgent = String(format: "Mobile/15E148 Safari/605.1.15 %@/%@", UIApplication.fw_appExecutable, UIApplication.fw_appVersion)
+        return userAgent
     }
 
     /// 获取默认请求UserAgent，可用于网络请求，示例：Example/1.0.0 (iPhone; iOS 14.2; Scale/3.00)
-    public static var requestUserAgent: String {
-        return Base.__fw_requestUserAgent
+    public static var fw_requestUserAgent: String {
+        let userAgent = String(format: "%@/%@ (%@; iOS %@; Scale/%0.2f)", UIApplication.fw_appExecutable, UIApplication.fw_appVersion, UIDevice.current.model, UIDevice.current.systemVersion, UIScreen.main.scale)
+        return userAgent
     }
     
     /// 清空网页缓存，完成后回调。单个网页请求指定URLRequest.cachePolicy即可
     public static func fw_clearCache(_ completion: (() -> Void)? = nil) {
-        Base.__fw_clearCache(completion)
+        let dataTypes = WKWebsiteDataStore.allWebsiteDataTypes()
+        let sinceDate = Date(timeIntervalSince1970: 0)
+        WKWebsiteDataStore.default().removeData(ofTypes: dataTypes, modifiedSince: sinceDate) {
+            completion?()
+        }
     }
     
 }
