bdfadd84bbb29942fcf0d536d76bbb0ec04d7bb6
wuyong <lingshizhuangzi@gmail.com>
2023-01-19 21:51:57 +0800

    refactor: 重构Mediator

--- Package.sh
+++ Package.sh
@@ -13,7 +13,6 @@ ln -s ../../FWObjC/Service/Network/*.h ./
 ln -s ../../FWObjC/Service/Request/*.h ./
 ln -s ../../FWObjC/Service/Media/*.h ./
 ln -s ../../FWObjC/Service/Database/*.h ./
-ln -s ../../FWObjC/Module/App/*.h ./
 ln -s ../../FWObjC/Module/Controller/*.h ./
 ln -s ../../FWObjC/Module/View/*.h ./
 ln -s ../../FWObjC/Plugin/View/*.h ./
--- Sources/include/FWObjC/Router.h
+++
@@ -1,1 +0,0 @@
-../../FWObjC/Kernel/Router.h
