7753f13df35c7a760a51d1bdaff1f244fdef0382
wuyong <lingshizhuangzi@gmail.com>
2023-01-29 22:58:24 +0800

    refactor: 重构BadgeView

--- Sources/FWFramework/Module/View/BadgeView.swift
+++ Sources/FWFramework/Module/View/BadgeView.swift
@@ -6,9 +6,89 @@
 //
 
 import UIKit
-#if FWMacroSPM
-import FWObjC
-#endif
+
+/// 自带提醒灯样式
+public enum BadgeStyle: Int {
+    /// 小红点，(10)*(10)
+    case dot = 1
+    /// 小文本，同系统标签，(18+)*(18)，12号普通字体
+    case small
+    /// 大文本，同系统桌面，(24+)*(24)，14号普通字体
+    case big
+}
+
+/// 提醒灯视图，默认禁用userInteractionEnabled
+open class BadgeView: UIView {
+    
+    /// 提醒灯样式，默认nil自定义
+    open private(set) var badgeStyle: BadgeStyle?
+    
+    /// 提醒灯文本标签。可自定义样式
+    open private(set) var badgeLabel: UILabel?
+    
+    /// 提醒灯右上偏移值
+    open private(set) var badgeOffset: CGPoint = .zero
+
+    /// 初始化自带样式提醒灯。宽高自动布局，其它手工布局
+    public init(badgeStyle: BadgeStyle) {
+        super.init(frame: .zero)
+        self.badgeStyle = badgeStyle
+        switch badgeStyle {
+        case .small:
+            setup(badgeHeight: 18, badgeOffset: CGPoint(x: 7, y: 7), textInset: 5, textFont: UIFont.systemFont(ofSize: 12))
+        case .big:
+            setup(badgeHeight: 24, badgeOffset: CGPoint(x: 9, y: 9), textInset: 6, textFont: UIFont.systemFont(ofSize: 14))
+        default:
+            setup(dotSize: 10, badgeOffset: CGPoint(x: 3, y: 3))
+        }
+    }
+    
+    /// 初始化自定义红点提醒灯。宽高自动布局，其它手工布局
+    public init(dotSize: CGFloat, badgeOffset: CGPoint) {
+        super.init(frame: .zero)
+        setup(dotSize: dotSize, badgeOffset: badgeOffset)
+    }
+
+    /// 初始化自定义文本提醒灯。宽高自动布局，其它手工布局
+    public init(badgeHeight: CGFloat, badgeOffset: CGPoint, textInset: CGFloat, textFont: UIFont) {
+        super.init(frame: .zero)
+        setup(badgeHeight: badgeHeight, badgeOffset: badgeOffset, textInset: textInset, textFont: textFont)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup(dotSize: CGFloat, badgeOffset: CGPoint) {
+        self.badgeOffset = badgeOffset
+        
+        isUserInteractionEnabled = false
+        backgroundColor = UIColor.red
+        layer.cornerRadius = dotSize / 2
+        fw_setDimensions(CGSize(width: dotSize, height: dotSize))
+    }
+    
+    private func setup(badgeHeight: CGFloat, badgeOffset: CGPoint, textInset: CGFloat, textFont: UIFont) {
+        self.badgeOffset = badgeOffset
+        
+        isUserInteractionEnabled = false
+        backgroundColor = UIColor.red
+        layer.cornerRadius = badgeHeight / 2
+        fw_setDimension(.height, size: badgeHeight)
+        fw_setDimension(.width, size: badgeHeight, relation: .greaterThanOrEqual)
+        
+        let badgeLabel = UILabel()
+        self.badgeLabel = badgeLabel
+        badgeLabel.textColor = UIColor.white
+        badgeLabel.font = textFont
+        badgeLabel.textAlignment = .center
+        addSubview(badgeLabel)
+        badgeLabel.fw_alignCenter()
+        badgeLabel.fw_pinEdge(toSuperview: .right, inset: textInset, relation: .greaterThanOrEqual)
+        badgeLabel.fw_pinEdge(toSuperview: .left, inset: textInset, relation: .greaterThanOrEqual)
+    }
+    
+}
 
 @_spi(FW) extension UIView {
     
@@ -90,8 +170,8 @@ import FWObjC
             view.bringSubviewToFront(badgeView)
             
             // 自定义视图时默认偏移，否则固定偏移
-            badgeView.fw_pinEdge(toSuperview: .top, inset: badgeView.badgeStyle.rawValue == 0 ? -badgeView.badgeOffset.y : 0)
-            badgeView.fw_pinEdge(toSuperview: .right, inset: badgeView.badgeStyle.rawValue == 0 ? -badgeView.badgeOffset.x : 0)
+            badgeView.fw_pinEdge(toSuperview: .top, inset: badgeView.badgeStyle == nil ? -badgeView.badgeOffset.y : 0)
+            badgeView.fw_pinEdge(toSuperview: .right, inset: badgeView.badgeStyle == nil ? -badgeView.badgeOffset.x : 0)
         }
     }
 
@@ -174,7 +254,7 @@ import FWObjC
             view.addSubview(badgeView)
             view.bringSubviewToFront(badgeView)
             
-            badgeView.fw_pinEdge(toSuperview: .top, inset: badgeView.badgeStyle.rawValue == 0 ? -badgeView.badgeOffset.y : 2.0)
+            badgeView.fw_pinEdge(toSuperview: .top, inset: badgeView.badgeStyle == nil ? -badgeView.badgeOffset.y : 2.0)
             badgeView.fw_pinEdge(.left, toEdge: .right, ofView: imageView, offset: -badgeView.badgeOffset.x)
         }
     }
