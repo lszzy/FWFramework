c21be43c2c5ec2fd2334d4a98cd45d7aafa6c5f8
wuyong <lingshizhuangzi@gmail.com>
2023-02-02 11:15:06 +0800

    refactor: 重构WebView

--- Sources/FWFramework/Wrapper/Module/WebView+Wrapper.swift
+++ Sources/FWFramework/Wrapper/Module/WebView+Wrapper.swift
@@ -21,12 +21,6 @@ extension Wrapper where Base: UIProgressView {
 }
 
 extension Wrapper where Base: WKWebView {
-    
-    /// 设置Javascript桥接器强引用属性，防止使用过程中被释放
-    public var jsBridge: WebViewJsBridge? {
-        get { return base.fw_jsBridge }
-        set { base.fw_jsBridge = newValue }
-    }
 
     /// 获取当前UserAgent，未自定义时为默认，示例：Mozilla/5.0 (iPhone; CPU OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148
     public var userAgent: String {
@@ -51,6 +45,24 @@ extension Wrapper where Base: WKWebView {
     /// 清空网页缓存，完成后回调。单个网页请求指定URLRequest.cachePolicy即可
     public static func clearCache(_ completion: (() -> Void)? = nil) {
         Base.fw_clearCache(completion)
+    }
+    
+    /// 设置Javascript桥接器强引用属性，防止使用过程中被释放
+    public var jsBridge: WebViewJsBridge? {
+        get { return base.fw_jsBridge }
+        set { base.fw_jsBridge = newValue }
+    }
+    
+    /// 是否启用Javascript桥接器，需结合setupJsBridge使用
+    public var jsBridgeEnabled: Bool {
+        get { return base.fw_jsBridgeEnabled }
+        set { base.fw_jsBridgeEnabled = newValue }
+    }
+    
+    /// 自动初始化Javascript桥接器，jsBridgeEnabled开启时生效
+    @discardableResult
+    public func setupJsBridge() -> WebViewJsBridge? {
+        return base.fw_setupJsBridge()
     }
     
 }
