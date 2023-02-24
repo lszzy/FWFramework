80737ea025dd0fe6b20b6cdca22ccf675b7fa94d
wuyong <lingshizhuangzi@gmail.com>
2023-02-24 22:18:19 +0800

    refactor: 重构StatisticalView

--- Sources/FWFramework/Wrapper/Module/StatisticalView+Wrapper.swift
+++ Sources/FWFramework/Wrapper/Module/StatisticalView+Wrapper.swift
@@ -33,13 +33,21 @@ extension Wrapper where Base: UIView {
 // MARK: - UIView+StatisticalExposure
 extension Wrapper where Base: UIView {
     
-    
+    /// 设置并尝试自动绑定曝光事件统计，containerView参数为nil。如果对象发生变化(indexPath|name|object)，也会触发
+    public var statisticalExposure: StatisticalEvent? {
+        get { base.fw_statisticalExposure }
+        set { base.fw_statisticalExposure = newValue }
+    }
     
 }
 
 // MARK: - UIViewController+StatisticalExposure
 extension Wrapper where Base: UIViewController {
     
-    
+    /// 设置并尝试自动绑定曝光事件统计，containerView参数为nil
+    public var statisticalExposure: StatisticalEvent? {
+        get { base.fw_statisticalExposure }
+        set { base.fw_statisticalExposure = newValue }
+    }
     
 }
