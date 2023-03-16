5e957563bad7f384cbcf43e7dc3a978034c5ce2d
wuyong <lingshizhuangzi@gmail.com>
2023-03-16 21:25:41 +0800

    perf: 优化DrawerView滚动模式处理

--- Sources/FWObjC/Module/View/DrawerView.h
+++ Sources/FWObjC/Module/View/DrawerView.h
@@ -82,7 +82,7 @@ NS_SWIFT_NAME(DrawerView)
 /// 自定义滚动视图允许滚动的位置，默认nil时仅openPosition可滚动
 @property (nullable, nonatomic, copy) NSArray<NSNumber *> * (^scrollViewPositions)(UIScrollView *scrollView);
 
-/// 自定义滚动视图在各个位置的contentInset(从小到大)，数量必须和positions相同，默认nil时不处理
+/// 自定义滚动视图在各个位置的contentInset(从小到大，数量同positions)，默认nil时不处理。UITableView时也可使用tableFooterView等实现
 @property (nullable, nonatomic, copy) NSArray<NSValue *> * (^scrollViewInsets)(UIScrollView *scrollView);
 
 /// 设置抽屉效果视图到指定位置，如果位置发生改变，会触发抽屉callback回调
@@ -97,7 +97,7 @@ NS_SWIFT_NAME(DrawerView)
 /// 设置抽屉效果视图到指定索引位置(从小到大)，如果位置发生改变，会触发抽屉callback回调
 - (void)setPositionIndex:(NSInteger)index animated:(BOOL)animated;
 
-/// 如果scrollView已自定义delegate，需在scrollViewDidScroll手工调用本方法
+/// 如果scrollView已自定义delegate，需在scrollViewDidScroll手工调用本方法。仅处理手势拖动时的滚动效果
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView;
 
 @end
