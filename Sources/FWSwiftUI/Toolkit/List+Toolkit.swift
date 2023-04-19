4e9e4007e2e7e4068c18c8ec3cc5943bd48c7996
wuyong <lingshizhuangzi@gmail.com>
2023-04-19 21:19:28 +0800

    feat: SwiftUI输入框配置句柄

--- Sources/FWSwiftUI/Toolkit/List+Toolkit.swift
+++ Sources/FWSwiftUI/Toolkit/List+Toolkit.swift
@@ -12,35 +12,6 @@ import SwiftUI
 @available(iOS 13.0, *)
 extension View {
     
-    /// 重置Cell样式，左对齐并隐藏分割线、去除多余间距，可指定背景色
-    public func resetCellStyle(background: Color? = nil) -> some View {
-        self.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-            .then { view in
-                if #available(iOS 15.0, *) {
-                    return view.listRowInsets(.zero)
-                        .listRowSeparator(.hidden)
-                        .eraseToAnyView()
-                } else {
-                    return view.listRowInsets(EdgeInsets(top: -1, leading: -1, bottom: -1, trailing: -1))
-                        .eraseToAnyView()
-                }
-            }
-            .then(background, body: { view, color in
-                view.background(color)
-            })
-            .eraseToAnyView()
-    }
-    
-    /// 重置Header|Footer样式，左对齐并去除多余间距，可指定背景色
-    public func resetHeaderStyle(background: Color? = nil) -> some View {
-        self.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-            .listRowInsets(.zero)
-            .then(background, body: { view, color in
-                view.background(color)
-            })
-            .eraseToAnyView()
-    }
-    
     /// 重置List样式，去除多余间距等，可指定背景色
     public func resetListStyle(background: Color? = nil) -> some View {
         self.then(background) { view, color in
@@ -71,6 +42,35 @@ extension View {
             .eraseToAnyView()
     }
     
+    /// 重置Header|Footer样式，左对齐并去除多余间距，可指定背景色
+    public func resetHeaderStyle(background: Color? = nil) -> some View {
+        self.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            .listRowInsets(.zero)
+            .then(background, body: { view, color in
+                view.background(color)
+            })
+            .eraseToAnyView()
+    }
+    
+    /// 重置Cell样式，左对齐并隐藏分割线、去除多余间距，可指定背景色
+    public func resetCellStyle(background: Color? = nil) -> some View {
+        self.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            .then { view in
+                if #available(iOS 15.0, *) {
+                    return view.listRowInsets(.zero)
+                        .listRowSeparator(.hidden)
+                        .eraseToAnyView()
+                } else {
+                    return view.listRowInsets(EdgeInsets(top: -1, leading: -1, bottom: -1, trailing: -1))
+                        .eraseToAnyView()
+                }
+            }
+            .then(background, body: { view, color in
+                view.background(color)
+            })
+            .eraseToAnyView()
+    }
+    
     /// 配置List视图，仅调用一次，一般用于绑定下拉刷新、上拉追加等
     ///
     /// 注意：iOS16以上scrollView为UICollectionView，iOS16以下为UITableView
@@ -79,19 +79,17 @@ extension View {
     ) -> some View {
         if #available(iOS 16.0, *) {
             return introspectCollectionView { collectionView in
-                if !collectionView.fw_propertyBool(forName: "listViewConfigure") {
-                    collectionView.fw_setPropertyBool(true, forName: "listViewConfigure")
-                    
-                    configuration(collectionView)
-                }
+                guard !collectionView.fw_propertyBool(forName: "listViewConfigure") else { return }
+                collectionView.fw_setPropertyBool(true, forName: "listViewConfigure")
+                
+                configuration(collectionView)
             }
         } else {
             return introspectTableView { tableView in
-                if !tableView.fw_propertyBool(forName: "listViewConfigure") {
-                    tableView.fw_setPropertyBool(true, forName: "listViewConfigure")
-                    
-                    configuration(tableView)
-                }
+                guard !tableView.fw_propertyBool(forName: "listViewConfigure") else { return }
+                tableView.fw_setPropertyBool(true, forName: "listViewConfigure")
+                
+                configuration(tableView)
             }
         }
     }
@@ -101,11 +99,10 @@ extension View {
         _ configuration: @escaping (UIScrollView) -> Void
     ) -> some View {
         return introspectScrollView { scrollView in
-            if !scrollView.fw_propertyBool(forName: "scrollViewConfigure") {
-                scrollView.fw_setPropertyBool(true, forName: "scrollViewConfigure")
-                
-                configuration(scrollView)
-            }
+            guard !scrollView.fw_propertyBool(forName: "scrollViewConfigure") else { return }
+            scrollView.fw_setPropertyBool(true, forName: "scrollViewConfigure")
+            
+            configuration(scrollView)
         }
     }
     
