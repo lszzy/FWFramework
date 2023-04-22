2bece076c2f87fc28d837954c7b640ad24821fb0
wuyong <lingshizhuangzi@gmail.com>
2023-04-23 00:26:27 +0800

    fix: 修复轮询请求stop时未完全取消问题，网络请求新增isCancelled句柄处理请求取消情况

--- Sources/FWFramework/Toolkit/Foundation.swift
+++ Sources/FWFramework/Toolkit/Foundation.swift
@@ -640,9 +640,10 @@ extension Wrapper where Base: NSObject {
         completion: @escaping (Bool, Any?) -> Void,
         retryCount: Int,
         timeoutInterval: TimeInterval,
-        delayInterval: @escaping (Int) -> TimeInterval
+        delayInterval: @escaping (Int) -> TimeInterval,
+        isCancelled: (() -> Bool)? = nil
     ) {
-        Base.__fw_perform(block, completion: completion, retryCount: retryCount, timeoutInterval: timeoutInterval, delayInterval: delayInterval)
+        Base.__fw_perform(block, completion: completion, retryCount: retryCount, timeoutInterval: timeoutInterval, delayInterval: delayInterval, isCancelled: isCancelled)
     }
 
     /// 执行轮询block任务，返回任务Id可取消
