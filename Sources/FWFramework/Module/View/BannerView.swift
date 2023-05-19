4ca074fce9f327807668b8681742ffb759c01f6d
wuyong <lingshizhuangzi@gmail.com>
2023-05-19 10:32:18 +0800

    feat: Swift重写BannerView

---
+++ Sources/FWFramework/Module/View/BannerView.swift
@@ -0,0 +1,52 @@
+//
+//  BannerView.swift
+//  FWFramework
+//
+//  Created by wuyong on 2023/5/19.
+//
+
+import UIKit
+
+/*
+/// BannerView分页控件对齐方式枚举
+public enum BannerViewPageControlAlignment: Int {
+    /// 右对齐
+    case right
+    /// 居中对齐
+    case center
+}
+
+/// BannerView分页控件样式枚举
+public enum BannerViewPageControlStyle: Int {
+    /// 系统样式
+    case system
+    /// 自定义样式，可设置图片等
+    case custom
+    /// 不显示
+    case none
+}
+
+/// Banner视图事件代理
+@objc public protocol BannerViewDelegate {
+    
+    /// 选中指定index
+    @objc optional func bannerView(_ bannerView: BannerView, didSelectItemAt index: Int)
+    /// 监听bannerView滚动，快速滚动时也会回调
+    @objc optional func bannerView(_ bannerView: BannerView, didScrollTo index: Int)
+    /// 如果你需要自定义UICollectionViewCell样式，请实现此代理方法，默认的BannerViewCell也会调用
+    @objc optional func bannerView(_ bannerView: BannerView, customCell: UICollectionViewCell, for index: Int)
+    /// 如果你需要自定义UICollectionViewCell样式，请实现此代理方法返回你的自定义UICollectionViewCell的class
+    @objc optional func customCellClass(bannerView: BannerView) -> UICollectionViewCell.Type?
+    /// 如果你需要自定义UICollectionViewCell样式，请实现此代理方法返回你的自定义UICollectionViewCell的Nib
+    @objc optional func customCellNib(bannerView: BannerView) -> UINib?
+    
+}
+
+/// Banner视图
+///
+/// [SDCycleScrollView](https://github.com/gsdios/SDCycleScrollView)
+open class BannerView: UIView {
+    
+    
+}
+*/
