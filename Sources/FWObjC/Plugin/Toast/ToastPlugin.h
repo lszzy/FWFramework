dd24847e507bd72ffe0a9dec93c17ce20b72ccf7
wuyong <lingshizhuangzi@gmail.com>
2023-06-28 13:23:38 +0800

    feat: 支持获取当前正在显示的Toast视图

--- Sources/FWObjC/Plugin/Toast/ToastPlugin.h
+++ Sources/FWObjC/Plugin/Toast/ToastPlugin.h
@@ -32,8 +32,8 @@ NS_SWIFT_NAME(ToastPlugin)
 /// 隐藏加载吐司，可指定延迟隐藏从而实现连续的加载效果
 - (void)hideLoading:(BOOL)delayed inView:(UIView *)view;
 
-/// 是否正在显示加载吐司
-- (BOOL)isShowingLoading:(UIView *)view;
+/// 获取正在显示的加载吐司视图
+- (nullable UIView *)showingLoadingView:(UIView *)view;
 
 /// 显示进度条吐司，默认需手工隐藏，指定cancelBlock时点击会自动隐藏并调用之
 - (void)showProgressWithAttributedText:(nullable NSAttributedString *)attributedText progress:(CGFloat)progress cancelBlock:(nullable void (^)(void))cancelBlock inView:(UIView *)view;
@@ -41,8 +41,8 @@ NS_SWIFT_NAME(ToastPlugin)
 /// 隐藏进度条吐司
 - (void)hideProgress:(UIView *)view;
 
-/// 是否正在显示进度条吐司
-- (BOOL)isShowingProgress:(UIView *)view;
+/// 获取正在显示的进度条吐司视图
+- (nullable UIView *)showingProgressView:(UIView *)view;
 
 /// 显示指定样式消息吐司，可设置自动隐藏和允许交互，自动隐藏完成后回调
 - (void)showMessageWithAttributedText:(nullable NSAttributedString *)attributedText style:(__FWToastStyle)style autoHide:(BOOL)autoHide interactive:(BOOL)interactive completion:(nullable void (^)(void))completion inView:(UIView *)view;
@@ -50,8 +50,8 @@ NS_SWIFT_NAME(ToastPlugin)
 /// 隐藏消息吐司
 - (void)hideMessage:(UIView *)view;
 
-/// 是否正在显示消息吐司
-- (BOOL)isShowingMessage:(UIView *)view;
+/// 获取正在显示的消息吐司视图
+- (nullable UIView *)showingMessageView:(UIView *)view;
 
 @end
 
