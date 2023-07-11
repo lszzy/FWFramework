dd89f6a6b41b14bd79f85040fefb7bec45d78b9f
wuyong <lingshizhuangzi@gmail.com>
2023-07-11 10:19:24 +0800

    refactor: 重构BannerView方法和属性名称

--- Sources/FWObjC/Module/View/BannerView.h
+++ Sources/FWObjC/Module/View/BannerView.h
@@ -57,22 +57,17 @@ NS_SWIFT_NAME(BannerView)
 /** 初始轮播图（推荐使用） */
 + (instancetype)bannerViewWithFrame:(CGRect)frame delegate:(nullable id<__FWBannerViewDelegate>)delegate placeholderImage:(nullable UIImage *)placeholderImage;
 
-+ (instancetype)bannerViewWithFrame:(CGRect)frame imageURLStringsGroup:(nullable NSArray *)imageURLStringsGroup;
-
 /** 本地图片轮播初始化方式 */
-+ (instancetype)bannerViewWithFrame:(CGRect)frame imageNamesGroup:(nullable NSArray *)imageNamesGroup;
++ (instancetype)bannerViewWithFrame:(CGRect)frame imagesGroup:(nullable NSArray *)imagesGroup;
 
 /** 本地图片轮播初始化方式2,infiniteLoop:是否无限循环 */
-+ (instancetype)bannerViewWithFrame:(CGRect)frame shouldInfiniteLoop:(BOOL)infiniteLoop imageNamesGroup:(nullable NSArray *)imageNamesGroup;
++ (instancetype)bannerViewWithFrame:(CGRect)frame shouldInfiniteLoop:(BOOL)infiniteLoop imagesGroup:(nullable NSArray *)imagesGroup;
 
-/** 网络图片 url string 数组 */
-@property (nonatomic, strong, nullable) NSArray *imageURLStringsGroup;
+/** 图片数组，支持URL|String|UIImage */
+@property (nonatomic, strong, nullable) NSArray *imagesGroup;
 
 /** 每张图片对应要显示的文字数组 */
 @property (nonatomic, strong, nullable) NSArray *titlesGroup;
-
-/** 本地图片数组 */
-@property (nonatomic, strong, nullable) NSArray *localizationImageNamesGroup;
 
 /** 自动滚动间隔时间,默认2s */
 @property (nonatomic, assign) CGFloat autoScrollTimeInterval;
@@ -102,25 +97,25 @@ NS_SWIFT_NAME(BannerView)
 @property (nonatomic, weak, nullable) id<__FWBannerViewDelegate> delegate;
 
 /** block方式监听点击 */
-@property (nonatomic, copy, nullable) void (^clickItemOperationBlock)(NSInteger currentIndex);
+@property (nonatomic, copy, nullable) void (^didSelectItemBlock)(NSInteger currentIndex);
 
 /** block方式监听滚动，快速滚动时也会回调 */
-@property (nonatomic, copy, nullable) void (^itemDidScrollOperationBlock)(NSInteger currentIndex);
+@property (nonatomic, copy, nullable) void (^didScrollToItemBlock)(NSInteger currentIndex);
 
 /** 自定义cell句柄 */
 @property (nonatomic, copy, nullable) void (^customCellBlock)(UICollectionViewCell *cell, NSInteger index);
 
 /** 手工滚动到指定index，不使用动画 */
-- (void)makeScrollViewScrollToIndex:(NSInteger)index;
+- (void)scrollToIndex:(NSInteger)index;
 
 /** 手工滚动到指定index，可指定动画 */
-- (void)makeScrollViewScrollToIndex:(NSInteger)index animated:(BOOL)animated;
+- (void)scrollToIndex:(NSInteger)index animated:(BOOL)animated;
 
 /** 解决viewWillAppear时出现时轮播图卡在一半的问题，在控制器viewWillAppear时调用此方法 */
-- (void)adjustWhenControllerViewWillAppear;
+- (void)adjustWhenViewWillAppear;
 
 /** 轮播图片的ContentMode，默认为 UIViewContentModeScaleAspectFill */
-@property (nonatomic, assign) UIViewContentMode bannerImageViewContentMode;
+@property (nonatomic, assign) UIViewContentMode imageViewContentMode;
 
 /** 占位图，用于网络未加载到图片时 */
 @property (nonatomic, strong, nullable) UIImage *placeholderImage;
