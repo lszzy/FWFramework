56906cdc7e08717586b6e34282f1a29ef9e0524f
wuyong <lingshizhuangzi@gmail.com>
2023-08-14 14:04:06 +0800

    feat: 开始重写Bridge

--- Sources/FWObjC/ObjC.m
+++ Sources/FWObjC/ObjC.m
@@ -509,4 +509,14 @@
     return obj1 == obj2;
 }
 
++ (BOOL)tryCatch:(void (NS_NOESCAPE ^)(void))block exceptionHandler:(void (^)(NSException * _Nonnull))exceptionHandler {
+    @try {
+        if (block) block();
+        return YES;
+    } @catch (NSException *exception) {
+        if (exceptionHandler) exceptionHandler(exception);
+        return NO;
+    }
+}
+
 @end
