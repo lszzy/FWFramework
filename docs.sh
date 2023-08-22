584afb06b33ad757f996470f66ceed7ce327eb0f
wuyong <lingshizhuangzi@gmail.com>
2023-08-22 15:15:09 +0800

    fix: 优化生成文档脚本docs.sh

--- docs.sh
+++ docs.sh
@@ -8,14 +8,10 @@ mkdir docs
 mv CNAME docs/
 mkdir "docs/$framework_name"
 
-umbrella_name="$framework_name-umbrella.h"
-umbrella_path=Example/Pods/Target\ Support\ Files
+umbrella_name="$framework_name.h"
 lib_path="docs/$framework_name/"
 sdk_path=$(xcrun --show-sdk-path --sdk iphonesimulator)
-cp "$umbrella_path/$framework_name/$umbrella_name" $lib_path
 find "Sources/FWObjC" -type f ! -regex '*.h' -name '*.h' \
-    -exec cp {} $lib_path \;
-find "Sources/FWVendor" -type f ! -regex '*.h' -name '*.h' \
     -exec cp {} $lib_path \;
 
 sourcekitten doc -- -project _Pods.xcodeproj -target $framework_name > "$lib_path/swift.json"
