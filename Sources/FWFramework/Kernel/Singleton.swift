505c5ef529f22e51dc0b722627d091590b611377
wuyong <lingshizhuangzi@gmail.com>
2023-08-31 00:57:07 +0800

    feat: Singleton兼容AnyObject扩展

--- Sources/FWFramework/Kernel/Singleton.swift
+++ Sources/FWFramework/Kernel/Singleton.swift
@@ -8,6 +8,26 @@
 import Foundation
 
 /// 单例模式协议
+///
+/// 继承NSObject示例：
+/// ```swift
+/// class SingletonObject: NSObject, SingletonProtocol {
+///     required override init() {}
+/// }
+/// ```
+/// 不继承NSObject且不实现shared示例：
+/// ```swift
+/// class SingletonObject: WrapperCompatible, SingletonProtocol {
+///     required init() {}
+/// }
+/// ```
+/// 不继承NSObject且实现shared示例：
+/// ```swift
+/// final class SingletonObject: SingletonProtocol {
+///     static let shared = SingletonObject()
+///     required init() {}
+/// }
+/// ```
 public protocol SingletonProtocol {
     
     /// 单例对象
@@ -18,6 +38,13 @@ public protocol SingletonProtocol {
     
     /// 单例初始化钩子方法
     func setupSingleton()
+    
+}
+
+extension SingletonProtocol {
+    
+    /// 默认实现单例初始化钩子方法
+    public func setupSingleton() {}
     
 }
 
@@ -36,8 +63,5 @@ extension SingletonProtocol where Self: WrapperObject {
             }
         }
     }
-    
-    /// 默认实现单例初始化钩子方法
-    public func setupSingleton() {}
     
 }
