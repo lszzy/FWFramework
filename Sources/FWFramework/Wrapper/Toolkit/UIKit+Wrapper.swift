357efe2a00c7229ee6d8d781dec6ae1fe7ba152e
wuyong <lingshizhuangzi@gmail.com>
2023-08-30 12:56:39 +0800

    feat: UICollectionView新增layoutFrame方法

--- Sources/FWFramework/Wrapper/Toolkit/UIKit+Wrapper.swift
+++ Sources/FWFramework/Wrapper/Toolkit/UIKit+Wrapper.swift
@@ -983,6 +983,11 @@ extension Wrapper where Base: UICollectionView {
         base.fw_reloadDataWithoutAnimation()
     }
     
+    /// 计算指定indexPath的frame，可转换为指定视图坐标(如window)
+    public func layoutFrame(at indexPath: IndexPath, toView view: UIView? = nil) -> CGRect? {
+        return base.fw_layoutFrame(at: indexPath, toView: view)
+    }
+    
     /// 简单曝光方案，willDisplay调用即可，集合快速滑动、数据不变等情况不计曝光。如需完整曝光方案，请使用StatisticalView
     public func willDisplay(_ cell: UICollectionViewCell, at indexPath: IndexPath, key: AnyHashable? = nil, exposure: @escaping () -> Void) {
         base.fw_willDisplay(cell, at: indexPath, key: key, exposure: exposure)
