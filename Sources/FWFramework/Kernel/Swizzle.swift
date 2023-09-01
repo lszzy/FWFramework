168d7a974a8c043135a94efdc5e093ca4d670ba0
wuyong <lingshizhuangzi@gmail.com>
2023-09-01 11:30:02 +0800

    feat: 优化Runtime

--- Sources/FWFramework/Kernel/Swizzle.swift
+++ Sources/FWFramework/Kernel/Swizzle.swift
@@ -126,8 +126,7 @@ import FWObjC
         } else {
             guard let objectClass = object_getClass(target) else { return false }
             let swizzleIdentifier = fw_swizzleIdentifier(target, selector: selector, identifier: identifier ?? "")
-            let key = unsafeBitCast(Selector(swizzleIdentifier), to: UnsafeRawPointer.self)
-            objc_setAssociatedObject(target, key, true, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            NSObject.fw_setAssociatedObject(target, key: swizzleIdentifier, value: true, policy: .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             return ObjCBridge.swizzleInstanceMethod(objectClass, selector: selector, identifier: identifier ?? "", with: block)
         }
     }
