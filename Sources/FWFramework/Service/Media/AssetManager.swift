04eab7b548b55de6bf03428a074fad827f7946cf
wuyong <lingshizhuangzi@gmail.com>
2023-10-26 16:29:31 +0800

    feat: 重写ImagePicker

--- Sources/FWFramework/Service/Media/AssetManager.swift
+++ Sources/FWFramework/Service/Media/AssetManager.swift
@@ -10,29 +10,325 @@ import AVFoundation
 import MobileCoreServices
 import Photos
 
+// MARK: - Asset
+/// 资源类型枚举
+public enum AssetType: UInt {
+    case unknown = 0
+    case image
+    case video
+    case audio
+}
+
+/// 资源子类型枚举
+public enum AssetSubType: UInt {
+    case unknown = 0
+    case image
+    case livePhoto
+    case gif
+}
+
+/// 资源下载状态枚举
+public enum AssetDownloadStatus: UInt {
+    case succeed = 0
+    case downloading
+    case canceled
+    case failed
+}
+
+/// 相册里某一个资源的包装对象，该资源可能是图片、视频等
+///
+/// Asset 重写了 isEqual: 方法，只要两个 Asset 的 identifier 相同，则认为是同一个对象，以方便在数组、字典等容器中对大量 Asset 进行遍历查找等操作
+public class Asset: NSObject {
+    
+    /// 只读PHAsset对象
+    public let phAsset: PHAsset
+    /// 只读资源类型
+    public private(set) var assetType: AssetType = .unknown
+    /// 只读资源子类型
+    public private(set) var assetSubType: AssetSubType = .unknown
+    /// Asset 的标识，每个 Asset 的 identifier 都不同。只要两个 Asset 的 identifier 相同则认为它们是同一个 asset
+    public private(set) var identifier: String = ""
+    /// 从 iCloud 下载资源大图的状态
+    public private(set) var downloadStatus: AssetDownloadStatus = .succeed
+    /// 从 iCloud 下载资源大图的进度
+    public var downloadProgress: Double = 0
+    /// 从 iCloud 请求获得资源的大图的请求 ID
+    public var requestId: Int = 0
+    /// 自定义编辑后的图片，用于实现图片裁剪等功能，默认nil
+    public var editedImage: UIImage?
+    /// 自定义请求结果对象，用于保存请求结果场景，默认nil
+    public var requestObject: Any?
+    /// 自定义请求结果信息，用于保存请求结果场景，默认nil
+    public var requestInfo: [AnyHashable: Any]?
+    
+    private var phAssetInfo: [AnyHashable: Any]?
+    
+    private static let kAssetInfoImageData = "imageData"
+    private static let kAssetInfoOriginInfo = "originInfo"
+    private static let kAssetInfoDataUTI = "dataUTI"
+    private static let kAssetInfoOrientation = "orientation"
+    private static let kAssetInfoSize = "size"
+    
+    /// 初始化方法
+    public init(phAsset: PHAsset) {
+        self.phAsset = phAsset
+        super.init()
+        
+        switch phAsset.mediaType {
+        case .image:
+            self.assetType = .image
+            if (phAsset.fw_invokeGetter("uniformTypeIdentifier") as? String) == (kUTTypeGIF as String) {
+                self.assetSubType = .gif
+            } else {
+                if phAsset.mediaSubtypes.contains(.photoLive) {
+                    self.assetSubType = .livePhoto
+                } else {
+                    self.assetSubType = .image
+                }
+            }
+        case .video:
+            self.assetType = .video
+        case .audio:
+            self.assetType = .audio
+        default:
+            self.assetType = .unknown
+        }
+    }
+    
+    /// Asset 的原图（包含系统相册“编辑”功能处理后的效果）
+    public var originImage: UIImage? {
+        let imageRequestOptions = PHImageRequestOptions()
+        imageRequestOptions.deliveryMode = .highQualityFormat
+        imageRequestOptions.isNetworkAccessAllowed = true
+        imageRequestOptions.isSynchronous = true
+        
+        var resultImage: UIImage?
+        AssetManager.shared.phCachingImageManager().requestImageDataAndOrientation(for: phAsset, options: imageRequestOptions, resultHandler: { imageData, _, _, _ in
+            if let imageData = imageData {
+                resultImage = UIImage(data: imageData)
+            }
+        })
+        return resultImage
+    }
+    
+    /// Asset 的缩略图，size 指定返回的缩略图的大小，pt 为单位
+    public func thumbnailImage(size: CGSize) -> UIImage? {
+        let imageRequestOptions = PHImageRequestOptions()
+        imageRequestOptions.isNetworkAccessAllowed = true
+        imageRequestOptions.resizeMode = .fast
+        imageRequestOptions.isSynchronous = true
+        
+        // 在 PHImageManager 中，targetSize 等 size 都是使用 px 作为单位，因此需要对targetSize 中对传入的 Size 进行处理，宽高各自乘以 ScreenScale，从而得到正确的图片
+        var resultImage: UIImage?
+        AssetManager.shared.phCachingImageManager().requestImage(for: phAsset, targetSize: CGSize(width: size.width * UIScreen.main.scale, height: size.height * UIScreen.main.scale), contentMode: .aspectFill, options: imageRequestOptions) { result, _ in
+            resultImage = result
+        }
+        return resultImage
+    }
+    
+    /// Asset 的预览图，输出与当前设备屏幕大小相同尺寸的图片，如果图片原图小于当前设备屏幕的尺寸，则只输出原图大小的图片
+    public var previewImage: UIImage? {
+        let imageRequestOptions = PHImageRequestOptions()
+        imageRequestOptions.isNetworkAccessAllowed = true
+        imageRequestOptions.isSynchronous = true
+        
+        var resultImage: UIImage?
+        AssetManager.shared.phCachingImageManager().requestImage(for: phAsset, targetSize: CGSize(width: UIScreen.main.bounds.width * 2, height: UIScreen.main.bounds.height * 2), contentMode: .aspectFill, options: imageRequestOptions) { result, _ in
+            resultImage = result
+        }
+        return resultImage
+    }
+    
+    /**
+     异步请求 Asset 的原图，包含了系统照片“编辑”功能处理后的效果（剪裁，旋转和滤镜等），可能会有网络请求
+
+     - Parameters:
+       - completion: 请求完成后调用的闭包，包含请求的原图和图片信息。该闭包会被多次调用，第一次调用获取到的是低清图，然后不断调用直到获取到高清图。
+       - progressHandler: 处理请求进度的处理程序，在闭包中修改 UI 时需要手动放到主线程处理。
+
+     - Returns: 返回请求图片的请求 id
+     */
+    @discardableResult
+    public func requestOriginImage(completion: ((_ result: UIImage?, _ info: [String: Any]?, _ finished: Bool) -> Void)?, progressHandler: PHAssetImageProgressHandler? = nil) -> Int {
+        let imageRequestOptions = PHImageRequestOptions()
+        imageRequestOptions.isNetworkAccessAllowed = true
+        imageRequestOptions.progressHandler = progressHandler
+        
+        let imageRequestId = AssetManager.shared.phCachingImageManager().requestImageDataAndOrientation(for: phAsset, options: imageRequestOptions) { imageData, _, _, info in
+            var image: UIImage?
+            if let imageData = imageData {
+                image = UIImage(data: imageData)
+            }
+            completion?(image, info as? [String: Any], true)
+        }
+        return Int(imageRequestId)
+    }
+
+    /**
+     异步请求 Asset 的缩略图，不会产生网络请求
+
+     - Parameters:
+       - size: 指定返回的缩略图的大小
+       - completion: 请求完成后调用的闭包，包含请求的缩略图和图片信息。该闭包会被多次调用，第一次调用获取到的是低清图，然后不断调用直到获取到高清图，此时闭包中的第二个参数（图片信息）为 nil。
+
+     - Returns: 返回请求图片的请求 id
+     */
+    @discardableResult
+    public func requestThumbnailImage(size: CGSize, completion: ((_ result: UIImage?, _ info: [String: Any]?, _ finished: Bool) -> Void)?) -> Int {
+        let imageRequestOptions = PHImageRequestOptions()
+        imageRequestOptions.isNetworkAccessAllowed = true
+        imageRequestOptions.resizeMode = .fast
+        
+        // 在 PHImageManager 中，targetSize 等 size 都是使用 px 作为单位，因此需要对targetSize 中对传入的 Size 进行处理，宽高各自乘以 ScreenScale，从而得到正确的图片
+        let imageRequestId = AssetManager.shared.phCachingImageManager().requestImage(for: phAsset, targetSize: CGSize(width: size.width * UIScreen.main.scale, height: size.height * UIScreen.main.scale), contentMode: .aspectFill, options: imageRequestOptions) { result, info in
+        }
+        return Int(imageRequestId)
+    }
+
+    /**
+     异步请求 Asset 的预览图，可能会有网络请求
+
+     - Parameters:
+       - completion: 请求完成后调用的闭包，包含请求的预览图和图片信息。该闭包会被多次调用，第一次调用获取到的是低清图，然后不断调用直到获取到高清图。
+       - progressHandler: 处理请求进度的处理程序，在闭包中修改 UI 时需要手动放到主线程处理。
+
+     - Returns: 返回请求图片的请求 id
+     */
+    @discardableResult
+    public func requestPreviewImage(completion: ((_ result: UIImage?, _ info: [String: Any]?, _ finished: Bool) -> Void)?, progressHandler: PHAssetImageProgressHandler? = nil) -> Int {
+        let imageRequestOptions = PHImageRequestOptions()
+        imageRequestOptions.isNetworkAccessAllowed = true
+        imageRequestOptions.progressHandler = progressHandler
+        
+        let imageRequestId = AssetManager.shared.phCachingImageManager().requestImage(for: phAsset, targetSize: CGSize(width: UIScreen.main.bounds.width * 2, height: UIScreen.main.bounds.height * 2), contentMode: .aspectFill, options: imageRequestOptions) { result, info in
+            
+        }
+        return Int(imageRequestId)
+    }
+
+    /**
+     异步请求 Live Photo，可能会有网络请求
+
+     - Parameters:
+       - completion: 请求完成后调用的闭包，包含请求的 Live Photo 和相关信息。如果 assetType 不是 AssetTypeLivePhoto，则为 nil。
+       - progressHandler: 处理请求进度的处理程序，在闭包中修改 UI 时需要手动放到主线程处理。
+
+     - Returns: 返回请求 Live Photo 的请求 id
+     */
+    @discardableResult
+    public func requestLivePhoto(completion: ((_ livePhoto: PHLivePhoto?, _ info: [String: Any]?, _ finished: Bool) -> Void)?, progressHandler: PHAssetImageProgressHandler? = nil) -> Int {
+        let livePhotoRequestOptions = PHLivePhotoRequestOptions()
+        livePhotoRequestOptions.isNetworkAccessAllowed = true
+        livePhotoRequestOptions.progressHandler = progressHandler
+        
+        let livePhotoRequestId = AssetManager.shared.phCachingImageManager().requestLivePhoto(for: phAsset, targetSize: CGSize(width: UIScreen.main.bounds.width * 2, height: UIScreen.main.bounds.height * 2), contentMode: .aspectFill, options: livePhotoRequestOptions) { livePhoto, info in
+            
+        }
+        return Int(livePhotoRequestId)
+    }
+    
+    /**
+     异步请求 AVPlayerItem，可能会有网络请求
+
+     - Parameters:
+       - completion: 完成请求后调用的 block，参数中包含了请求的 AVPlayerItem 以及相关信息，若 assetType 不是 AssetTypeVideo 则为 nil
+       - progressHandler: 处理请求进度的 handler，不在主线程上执行，在 block 中修改 UI 时注意需要手工放到主线程处理。
+     
+     - Returns: 返回请求 AVPlayerItem 的请求 id
+     */
+    @discardableResult
+    public func requestPlayerItem(completion: ((_ playerItem: AVPlayerItem?, _ info: [String: Any]?) -> Void)?, progressHandler: PHAssetVideoProgressHandler? = nil) -> Int {
+        let videoRequestOptions = PHVideoRequestOptions()
+        videoRequestOptions.isNetworkAccessAllowed = true
+        videoRequestOptions.progressHandler = progressHandler
+        
+        let videoRequestId = AssetManager.shared.phCachingImageManager().requestPlayerItem(forVideo: phAsset, options: videoRequestOptions) { playerItem, info in
+            
+        }
+        return Int(videoRequestId)
+    }
+
+    /**
+     异步请求 视频文件URL，可能会有网络请求
+
+     - Parameters:
+       - outputURL: 视频输出文件URL路径，如果路径已存在会导出失败
+       - exportPreset: 导出视频选项配置
+       - completion: 完成请求后调用的 block，参数中包含了请求的 文件URL 以及相关信息，若 assetType 不是 AssetTypeVideo 则为 nil
+       - progressHandler: 处理请求进度的 handler，不在主线程上执行，在 block 中修改 UI 时注意需要手工放到主线程处理。
+     
+     - Returns: 返回请求 视频文件URL 的请求 id
+     */
+    @discardableResult
+    public func requestVideoURL(outputURL: URL, exportPreset: String, completion: ((_ videoURL: URL?, _ info: [String: Any]?) -> Void)?, progressHandler: PHAssetVideoProgressHandler? = nil) -> Int {
+        let videoRequestOptions = PHVideoRequestOptions()
+        videoRequestOptions.isNetworkAccessAllowed = true
+        videoRequestOptions.progressHandler = progressHandler
+        
+        let videoRequestId = AssetManager.shared.phCachingImageManager().requestExportSession(forVideo: phAsset, options: videoRequestOptions, exportPreset: exportPreset) { exportSession, info in
+            
+        }
+        return Int(videoRequestId)
+    }
+
+    /**
+     异步请求图片的 Data
+
+     - Parameter completion: 完成请求后调用的 block，参数中包含了请求的图片 Data（若 assetType 不是 AssetTypeImage 或 AssetTypeLivePhoto 则为 nil），该图片是否为 GIF 的判断值，以及该图片的文件格式是否为 HEIC
+     */
+    public func requestImageData(completion: ((_ imageData: Data?, _ info: [String: Any]?, _ isGIF: Bool, _ isHEIC: Bool) -> Void)?) {
+        
+    }
+    
+    /// 获取图片的 UIImageOrientation 值，仅 assetType 为 AssetTypeImage 或 AssetTypeLivePhoto 时有效
+    public var imageOrientation: UIImage.Orientation {
+        return .up
+    }
+    
+    /// 更新下载资源的结果
+    public func updateDownloadStatus(succeed: Bool) {
+        
+    }
+    
+    /// 获取 Asset 的体积（数据大小）
+    public func assetSize(completion: ((Int64) -> Void)?) {
+        
+    }
+    
+    /// 获取 Asset 的总时长（仅视频）
+    public var duration: TimeInterval {
+        return 0
+    }
+    
+}
+
+// MARK: - AssetGroup
+
 // MARK: - AssetManager
+
+// MARK: - LivePhoto
 /// [LivePhoto](https://github.com/LimitPoint/LivePhoto)
 public class LivePhoto {
     
     /// LivePhoto资源定义
-    public typealias LivePhotoResources = (pairedImage: URL, pairedVideo: URL)
+    public typealias Resources = (pairedImage: URL, pairedVideo: URL)
     
     /// 导出LivePhoto资源
-    public class func extractResources(from livePhoto: PHLivePhoto, completion: @escaping (LivePhotoResources?) -> Void) {
+    public class func extractResources(from livePhoto: PHLivePhoto, completion: @escaping (Resources?) -> Void) {
         queue.async {
             shared.extractResources(from: livePhoto, completion: completion)
         }
     }
     
     /// 生成LivePhoto对象
-    public class func generate(from imageURL: URL, videoURL: URL, progress: @escaping (CGFloat) -> Void, completion: @escaping (PHLivePhoto?, LivePhotoResources?) -> Void) {
+    public class func generate(from imageURL: URL, videoURL: URL, progress: @escaping (CGFloat) -> Void, completion: @escaping (PHLivePhoto?, Resources?) -> Void) {
         queue.async {
             shared.generate(from: imageURL, videoURL: videoURL, progress: progress, completion: completion)
         }
     }
     
     /// 保存LivePhoto资源到相册
-    public class func saveToLibrary(_ resources: LivePhotoResources, completion: @escaping (Bool, Error?) -> Void) {
+    public class func saveToLibrary(_ resources: Resources, completion: @escaping (Bool, Error?) -> Void) {
         PHPhotoLibrary.shared().performChanges({
             let creationRequest = PHAssetCreationRequest.forAsset()
             let options = PHAssetResourceCreationOptions()
@@ -57,7 +353,7 @@ public class LivePhoto {
         return fullDirectory
     }()
     
-    private func generate(from imageURL: URL, videoURL: URL, progress: @escaping (CGFloat) -> Void, completion: @escaping (PHLivePhoto?, LivePhotoResources?) -> Void) {
+    private func generate(from imageURL: URL, videoURL: URL, progress: @escaping (CGFloat) -> Void, completion: @escaping (PHLivePhoto?, Resources?) -> Void) {
         let assetIdentifier = UUID().uuidString
         guard let pairedImageURL = addAssetID(assetIdentifier, toImage: imageURL, saveTo: cacheDirectory.appendingPathComponent(assetIdentifier).appendingPathExtension("jpg")) else {
             DispatchQueue.main.async {
@@ -83,7 +379,7 @@ public class LivePhoto {
         }
     }
     
-    private func extractResources(from livePhoto: PHLivePhoto, to directoryURL: URL, completion: @escaping (LivePhotoResources?) -> Void) {
+    private func extractResources(from livePhoto: PHLivePhoto, to directoryURL: URL, completion: @escaping (Resources?) -> Void) {
         let assetResources = PHAssetResource.assetResources(for: livePhoto)
         let group = DispatchGroup()
         var keyPhotoURL: URL?
@@ -117,7 +413,7 @@ public class LivePhoto {
         }
     }
     
-    private func extractResources(from livePhoto: PHLivePhoto, completion: @escaping (LivePhotoResources?) -> Void) {
+    private func extractResources(from livePhoto: PHLivePhoto, completion: @escaping (Resources?) -> Void) {
         extractResources(from: livePhoto, to: cacheDirectory, completion: completion)
     }
     
