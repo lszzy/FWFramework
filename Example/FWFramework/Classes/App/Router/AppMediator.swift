34a98fef3de17431fd5e84f3bca1dfcfbdd1c677
wuyong <lingshizhuangzi@gmail.com>
2023-11-15 15:40:08 +0800

    feat: 重构Autoloader

--- Example/FWFramework/Classes/App/Router/AppMediator.swift
+++ Example/FWFramework/Classes/App/Router/AppMediator.swift
@@ -16,7 +16,7 @@ protocol AppModuleProtocol: ModuleProtocol {
 }
 
 @objc extension Autoloader {
-    static func loadAppModule() {
+    static func loadApp_AppModule() {
         Mediator.registerService(AppModuleProtocol.self, module: AppModule.self)
     }
 }
