3dce03a87809825be91c914a25eaa99fbc2531fe
wuyong <lingshizhuangzi@gmail.com>
2023-11-17 11:52:22 +0800

    feat: 新增网络插件

---
+++ Sources/FWFramework/Plugin/Network/NetworkPluginImpl.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkPluginImpl.swift
+//  FWFramework
+//
+//  Created by wuyong on 2023/11/17.
+//
+
+import Foundation
+
+/// 默认网络插件
+open class NetworkPluginImpl: NSObject, NetworkPlugin {
+    
+    // MARK: - Accessor
+    /// 单例模式
+    @objc(sharedInstance)
+    public static let shared = ViewPluginImpl()
+    
+    // MARK: - NetworkPlugin
+    
+    
+}
