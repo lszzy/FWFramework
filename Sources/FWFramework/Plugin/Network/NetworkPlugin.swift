2375a9cca05e1646d1c61b99e44cf646a61387af
wuyong <lingshizhuangzi@gmail.com>
2023-11-17 11:52:22 +0800

    feat: 新增网络插件

---
+++ Sources/FWFramework/Plugin/Network/NetworkPlugin.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkPlugin.swift
+//  FWFramework
+//
+//  Created by wuyong on 2023/11/17.
+//
+
+import Foundation
+
+// MARK: - NetworkPlugin
+/// 网络插件协议
+public protocol NetworkPlugin: AnyObject {
+    
+    
+    
+}
+
+extension NetworkPlugin {
+    
+    
+    
+}
