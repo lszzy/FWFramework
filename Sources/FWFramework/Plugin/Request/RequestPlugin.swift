5edd3203e481cad7b685e835873566c430e033df
wuyong <lingshizhuangzi@gmail.com>
2023-11-21 00:52:53 +0800

    feat: 重写Request

--- Sources/FWFramework/Plugin/Request/RequestPlugin.swift
+++ Sources/FWFramework/Plugin/Request/RequestPlugin.swift
@@ -14,11 +14,14 @@ public protocol RequestPlugin: AnyObject {
     /// 构建URLRequest方法
     func urlRequest(for request: HTTPRequest) throws -> NSMutableURLRequest
     
-    /// 开始数据请求，需设置requestTask
-    func startDataRequest(_ request: HTTPRequest, urlRequest: URLRequest, completionHandler: ((URLResponse, Any?, Error?) -> Void)?)
+    /// 构建数据任务，需设置requestTask
+    func dataTask(for request: HTTPRequest, urlRequest: URLRequest, completionHandler: ((URLResponse, Any?, Error?) -> Void)?)
     
-    /// 开始下载请求，支持断点续传，须设置requestTask
-    func startDownloadRequest(_ request: HTTPRequest, urlRequest: URLRequest?, resumeData: Data?, destination: String, completionHandler: ((URLResponse, URL?, Error?) -> Void)?)
+    /// 构建下载任务，支持断点续传，须设置requestTask
+    func downloadTask(for request: HTTPRequest, urlRequest: URLRequest?, resumeData: Data?, destination: String, completionHandler: ((URLResponse, URL?, Error?) -> Void)?)
+    
+    /// 开始请求
+    func startRequest(for request: HTTPRequest)
     
     /// 取消请求
     func cancelRequest(for request: HTTPRequest)
@@ -32,14 +35,19 @@ extension RequestPlugin {
         return try RequestPluginImpl.shared.urlRequest(for: request)
     }
     
-    /// 默认实现开始数据请求，需设置requestTask
-    public func startDataRequest(_ request: HTTPRequest, urlRequest: URLRequest, completionHandler: ((URLResponse, Any?, Error?) -> Void)?) {
-        RequestPluginImpl.shared.startDataRequest(request, urlRequest: urlRequest, completionHandler: completionHandler)
+    /// 默认实现构建数据任务，需设置requestTask
+    public func dataTask(for request: HTTPRequest, urlRequest: URLRequest, completionHandler: ((URLResponse, Any?, Error?) -> Void)?) {
+        RequestPluginImpl.shared.dataTask(for: request, urlRequest: urlRequest, completionHandler: completionHandler)
     }
     
-    /// 默认实现开始下载请求，支持断点续传，须设置requestTask
-    public func startDownloadRequest(_ request: HTTPRequest, urlRequest: URLRequest?, resumeData: Data?, destination: String, completionHandler: ((URLResponse, URL?, Error?) -> Void)?) {
-        RequestPluginImpl.shared.startDownloadRequest(request, urlRequest: urlRequest, resumeData: resumeData, destination: destination, completionHandler: completionHandler)
+    /// 默认实现构建下载任务，支持断点续传，须设置requestTask
+    public func downloadTask(for request: HTTPRequest, urlRequest: URLRequest?, resumeData: Data?, destination: String, completionHandler: ((URLResponse, URL?, Error?) -> Void)?) {
+        RequestPluginImpl.shared.downloadTask(for: request, urlRequest: urlRequest, resumeData: resumeData, destination: destination, completionHandler: completionHandler)
+    }
+    
+    /// 默认实现开始请求
+    public func startRequest(for request: HTTPRequest) {
+        RequestPluginImpl.shared.startRequest(for: request)
     }
     
     /// 默认实现取消请求
