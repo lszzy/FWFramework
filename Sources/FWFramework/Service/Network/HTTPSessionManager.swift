8e533b0667fb7e42fbc69d790c2061dc3c494831
wuyong <lingshizhuangzi@gmail.com>
2024-01-23 13:14:17 +0800

    feat: 重写Network

--- Sources/FWFramework/Service/Network/HTTPSessionManager.swift
+++ Sources/FWFramework/Service/Network/HTTPSessionManager.swift
@@ -12,5 +12,99 @@ import Foundation
 ///
 /// [AFNetworking](https://github.com/AFNetworking/AFNetworking)
 open class HTTPSessionManager: URLSessionManager {
+    open private(set) var baseURL: URL?
+    open var requestSerializer: HTTPRequestSerializer
+    open var responseSerializer: HTTPResponseSerializer
+    open var securityPolicy: SecurityPolicy
     
+    public init(baseURL: URL? = nil, sessionConfiguration: URLSessionConfiguration? = nil) {
+        
+    }
+    
+    open func get(
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        progress: ((Progress) -> Void)? = nil,
+        success: ((_ task: URLSessionDataTask, _ responseObject: Any?) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
+    
+    open func head(
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        success: ((_ task: URLSessionDataTask) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
+    
+    open func post(
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        progress: ((Progress) -> Void)? = nil,
+        success: ((_ task: URLSessionDataTask, _ responseObject: Any?) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
+    
+    open func post(
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        constructingBody block: ((MultipartFormData) -> Void)?,
+        progress: ((Progress) -> Void)? = nil,
+        success: ((_ task: URLSessionDataTask, _ responseObject: Any?) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
+    
+    open func put(
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        success: ((_ task: URLSessionDataTask, _ responseObject: Any?) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
+    
+    open func patch(
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        success: ((_ task: URLSessionDataTask, _ responseObject: Any?) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
+    
+    open func delete(
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        success: ((_ task: URLSessionDataTask, _ responseObject: Any?) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
+    
+    open func dataTask(
+        httpMethod: String,
+        url: String,
+        parameters: Any? = nil,
+        headers: [String: String]? = nil,
+        uploadProgress: ((Progress) -> Void)? = nil,
+        downloadProgress: ((Progress) -> Void)? = nil,
+        success: ((_ task: URLSessionDataTask, _ responseObject: Any?) -> Void)? = nil,
+        failure: ((_ task: URLSessionDataTask?, _ error: Error) -> Void)? = nil
+    ) -> URLSessionDataTask? {
+        
+    }
 }*/
