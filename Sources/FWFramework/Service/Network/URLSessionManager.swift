1a2916a1820bdcd187af2dc3de46068131fa4915
wuyong <lingshizhuangzi@gmail.com>
2024-01-24 11:43:03 +0800

    feat: 重写Network

--- Sources/FWFramework/Service/Network/URLSessionManager.swift
+++ Sources/FWFramework/Service/Network/URLSessionManager.swift
@@ -179,4 +179,170 @@ open class URLSessionManager: NSObject, NSCopying, URLSessionDelegate, URLSessio
     open func setDownloadTaskDidResumeBlock(_ block: ((_ session: URLSession, _ downloadTask: URLSessionDownloadTask, _ fileOffset: Int64, _ expectedTotalBytes: Int64) -> Void)?) {
         
     }
+}
+
+fileprivate class URLSessionManagerTaskDelegate: NSObject, URLSessionTaskDelegate, URLSessionDataDelegate, URLSessionDownloadDelegate {
+    static let processingQueue = DispatchQueue(label: "site.wuyong.networking.session.manager.processing", attributes: .concurrent)
+    static let completionGroup = DispatchGroup()
+    
+    weak var manager: URLSessionManager?
+    private var mutableData: Data? = Data()
+    private var uploadProgress = Progress(parent: nil, userInfo: nil)
+    private var downloadProgress = Progress(parent: nil, userInfo: nil)
+    private var downloadFileURL: URL?
+    private var sessionTaskMetrics: URLSessionTaskMetrics?
+    private var downloadTaskDidFinishDownloading: ((_ session: URLSession, _ downloadTask: URLSessionDownloadTask, _ location: URL) -> URL?)?
+    private var uploadProgressBlock: ((Progress) -> Void)?
+    private var downloadProgressBlock: ((Progress) -> Void)?
+    private var completionHandler: ((_ response: URLResponse, _ responseObject: Any?, _ error: Error?) -> Void)?
+    
+    init(task: URLSessionTask) {
+        super.init()
+        
+        for progress in [uploadProgress, downloadProgress] {
+            progress.totalUnitCount = NSURLSessionTransferSizeUnknown
+            progress.isCancellable = true
+            progress.cancellationHandler = { [weak task] in
+                task?.cancel()
+            }
+            progress.isPausable = true
+            progress.pausingHandler = { [weak task] in
+                task?.suspend()
+            }
+            progress.resumingHandler = { [weak task] in
+                task?.resume()
+            }
+            
+            progress.addObserver(self, forKeyPath: "fractionCompleted", options: .new, context: nil)
+        }
+    }
+    
+    deinit {
+        downloadProgress.removeObserver(self, forKeyPath: "fractionCompleted")
+        uploadProgress.removeObserver(self, forKeyPath: "fractionCompleted")
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if let progress = object as? Progress {
+            if progress == downloadProgress {
+                downloadProgressBlock?(progress)
+            } else if progress == uploadProgress {
+                uploadProgressBlock?(progress)
+            }
+        }
+    }
+    
+    // MARK: - URLSessionTaskDelegate
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        let error = (task.fw_property(forName: "authenticationChallengeError") as? Error) ?? error
+        let manager = self.manager
+        var responseObject: Any?
+        
+        var userInfo: [AnyHashable: Any] = [:]
+        userInfo[URLSessionManager.networkingTaskDidCompleteResponseSerializerKey] = manager?.responseSerializer
+        
+        var data: Data?
+        if mutableData != nil {
+            data = mutableData
+            mutableData = nil
+        }
+        
+        if let sessionTaskMetrics = sessionTaskMetrics {
+            userInfo[URLSessionManager.networkingTaskDidCompleteSessionTaskMetrics] = sessionTaskMetrics
+        }
+        if let downloadFileURL = downloadFileURL {
+            userInfo[URLSessionManager.networkingTaskDidCompleteAssetPathKey] = downloadFileURL
+        } else if let data = data {
+            userInfo[URLSessionManager.networkingTaskDidCompleteResponseDataKey] = data
+        }
+        
+        if let error = error {
+            userInfo[URLSessionManager.networkingTaskDidCompleteErrorKey] = error
+            
+            let queue = manager?.completionQueue ?? .main
+            let group = manager?.completionGroup ?? Self.completionGroup
+            queue.async(group: group) {
+                self.completionHandler?(task.response ?? HTTPURLResponse(), responseObject, error)
+                
+                DispatchQueue.main.async {
+                    NotificationCenter.default.post(name: URLSessionManager.networkingTaskDidCompleteNotification, object: task, userInfo: userInfo)
+                }
+            }
+        } else {
+            Self.processingQueue.async {
+                let taskInfo = manager?.userInfo(for: task)
+                if taskInfo != nil, task.response != nil, let responseSerializer = manager?.responseSerializer as? HTTPResponseSerializer {
+                    responseSerializer.setUserInfo(taskInfo, for: task.response)
+                }
+                
+                var serializationError: Error?
+                responseObject = manager?.responseSerializer.responseObject(for: task.response, data: data, error: &serializationError)
+                
+                if self.downloadFileURL != nil {
+                    responseObject = self.downloadFileURL
+                }
+                if responseObject != nil {
+                    userInfo[URLSessionManager.networkingTaskDidCompleteSerializedResponseKey] = responseObject
+                }
+                if serializationError != nil {
+                    userInfo[URLSessionManager.networkingTaskDidCompleteErrorKey] = serializationError
+                }
+                
+                let queue = manager?.completionQueue ?? .main
+                let group = manager?.completionGroup ?? Self.completionGroup
+                queue.async(group: group) {
+                    self.completionHandler?(task.response ?? HTTPURLResponse(), responseObject, serializationError)
+                    
+                    DispatchQueue.main.async {
+                        NotificationCenter.default.post(name: URLSessionManager.networkingTaskDidCompleteNotification, object: task, userInfo: userInfo)
+                    }
+                }
+            }
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
+        sessionTaskMetrics = metrics
+    }
+    
+    // MARK: - URLSessionDataDelegate
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        downloadProgress.totalUnitCount = dataTask.countOfBytesExpectedToReceive
+        downloadProgress.completedUnitCount = dataTask.countOfBytesReceived
+        
+        mutableData?.append(data)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
+        uploadProgress.totalUnitCount = task.countOfBytesExpectedToSend
+        uploadProgress.completedUnitCount = task.countOfBytesSent
+    }
+    
+    // MARK: - URLSessionDownloadDelegate
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        downloadProgress.totalUnitCount = totalBytesExpectedToWrite
+        downloadProgress.completedUnitCount = totalBytesWritten
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) {
+        downloadProgress.totalUnitCount = expectedTotalBytes
+        downloadProgress.completedUnitCount = fileOffset
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        downloadFileURL = nil
+        
+        if downloadTaskDidFinishDownloading != nil {
+            downloadFileURL = downloadTaskDidFinishDownloading?(session, downloadTask, location)
+            if let downloadFileURL = downloadFileURL {
+                
+                do {
+                    try FileManager.default.moveItem(at: location, to: downloadFileURL)
+                    NotificationCenter.default.post(name: URLSessionManager.urlSessionDownloadTaskDidMoveFileSuccessfullyNotification, object: downloadTask, userInfo: nil)
+                } catch let fileManagerError {
+                    NotificationCenter.default.post(name: URLSessionManager.urlSessionDownloadTaskDidFailToMoveFileNotification, object: downloadTask, userInfo: (fileManagerError as NSError).userInfo)
+                }
+            }
+        }
+    }
 }*/
