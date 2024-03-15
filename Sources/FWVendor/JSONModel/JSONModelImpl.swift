49dd3a452c4d90e4e11a090206428eb69dda5a3c
wuyong <lingshizhuangzi@gmail.com>
2024-03-15 20:13:46 +0800

    refactor: 开始重构JSONModel

---
+++ Sources/FWVendor/JSONModel/JSONModelImpl.swift
@@ -0,0 +1,18 @@
+//
+//  JSONModelImpl.swift
+//  FWFramework
+//
+//  Created by wuyong on 2022/8/23.
+//
+
+import Foundation
+#if FWMacroSPM
+import FWFramework
+#endif
+
+// MARK: - Autoloader+JSONModelImpl
+@objc extension Autoloader {
+    
+    static func loadVendor_JSONModel() {}
+    
+}
