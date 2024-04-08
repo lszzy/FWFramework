af4219569c249fbd4fbcd18df4e2d371da4e2379
wuyong <lingshizhuangzi@gmail.com>
2024-04-08 17:02:09 +0800

    chore: 5.2.1版本

--- Sources/FWFramework/Kernel/Wrapper.swift
+++ Sources/FWFramework/Kernel/Wrapper.swift
@@ -127,4 +127,8 @@ extension URL: WrapperCompatible {}
 extension Array: WrapperCompatible {}
 extension Set: WrapperCompatible {}
 extension Dictionary: WrapperCompatible {}
+extension CGFloat: WrapperCompatible {}
+extension CGPoint: WrapperCompatible {}
+extension CGSize: WrapperCompatible {}
+extension CGRect: WrapperCompatible {}
 extension NSObject: WrapperCompatible {}
