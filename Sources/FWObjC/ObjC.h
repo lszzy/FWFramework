cc1b1d29b646fc552ebd8ce3e409fcd9665ffa3f
wuyong <lingshizhuangzi@gmail.com>
2024-04-16 17:12:01 +0800

    feat: 移除swizzleDealloc，重构LifecycleState实现

--- Sources/FWObjC/ObjC.h
+++ Sources/FWObjC/ObjC.h
@@ -18,8 +18,6 @@ NS_SWIFT_NAME(ObjCBridge)
 
 + (BOOL)swizzleInstanceMethod:(Class)originalClass selector:(SEL)originalSelector identifier:(nullable NSString *)identifier withBlock:(id (^)(__unsafe_unretained Class targetClass, SEL originalCMD, IMP (^originalIMP)(void)))block;
 
-+ (BOOL)swizzleDeallocMethod:(Class)originalClass identifier:(nullable NSString *)identifier withBlock:(void (^)(__kindof NSObject *__unsafe_unretained object))block;
-
 + (BOOL)exchangeInstanceMethod:(Class)originalClass originalSelector:(SEL)originalSelector swizzleSelector:(SEL)swizzleSelector;
 
 + (BOOL)exchangeInstanceMethod:(Class)originalClass originalSelector:(SEL)originalSelector swizzleSelector:(SEL)swizzleSelector withBlock:(id)swizzleBlock;
