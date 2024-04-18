f4e8e60f32727acee43a19252ba4282a38c68c9a
wuyong <lingshizhuangzi@gmail.com>
2024-04-18 23:34:03 +0800

    docs: 优化文档脚本文件

--- docs.sh
+++ docs.sh
@@ -8,15 +8,9 @@ mkdir docs
 mv CNAME docs/
 mkdir "docs/$framework_name"
 
-umbrella_name="$framework_name.h"
 lib_path="docs/$framework_name/"
-sdk_path=$(xcrun --show-sdk-path --sdk iphonesimulator)
-find "Sources/FWObjC" -type f ! -regex '*.h' -name '*.h' \
-    -exec cp {} $lib_path \;
-
 sourcekitten doc -- -project _Pods.xcodeproj -target $framework_name > "$lib_path/swift.json"
-sourcekitten doc --objc "$lib_path/$umbrella_name" -- -x objective-c -isysroot $sdk_path -I $lib_path -fobjc-arc -fmodules > "$lib_path/objc.json"
-jazzy --include-spi-declarations --sourcekitten-sourcefile "$lib_path/swift.json","$lib_path/objc.json"
+jazzy --include-spi-declarations --sourcekitten-sourcefile "$lib_path/swift.json"
 
 rm -rf $lib_path
 rm -rf Example/build/
