fa6aed98557c206e684f81eb31685ab2c29125f9
wuyong <lingshizhuangzi@gmail.com>
2024-04-19 21:15:58 +0800

    feat: 重构Error异常处理

--- Sources/FWFramework/Service/Basic/Language.swift
+++ Sources/FWFramework/Service/Basic/Language.swift
@@ -69,13 +69,14 @@ extension Wrapper where Base: Bundle {
     }
     
     /// 读取应用当前语言，如果localizedLanguage存在则返回，否则返回systemLanguage
-    public static var currentLanguage: String {
+    public static var currentLanguage: String? {
         return Base.fw_currentLanguage
     }
 
-    /// 读取当前系统语言，兼容应用设置且不含区域，示例：zh-Hans
-    public static var systemLanguage: String {
-        return Base.fw_systemLanguage
+    /// 读取当前系统语言，不满足需求时可自定义，兼容应用设置且不含区域，示例：zh-Hans
+    public static var systemLanguage: String? {
+        get { return Base.fw_systemLanguage }
+        set { Base.fw_systemLanguage = newValue }
     }
 
     /// 读取或设置自定义本地化语言，未自定义时为空。(语言值对应本地化文件存在才会立即生效，如zh-Hans|en)，为空时清空自定义，会触发通知。默认只处理mainBundle语言，如果需要处理三方SDK和系统组件语言，详见Bundle分类
@@ -208,23 +209,33 @@ extension Notification.Name {
     
     /// 读取指定语言显示名称，可指定本地化标识(默认当前语言)
     public static func fw_languageName(for language: String, localeIdentifier: String? = nil) -> String {
-        let locale: NSLocale = NSLocale(localeIdentifier: localeIdentifier ?? fw_currentLanguage)
+        guard let localeIdentifier = localeIdentifier ?? fw_currentLanguage else { return "" }
+        let locale: NSLocale = NSLocale(localeIdentifier: localeIdentifier)
         return locale.displayName(forKey: NSLocale.Key.identifier, value: language) ?? ""
     }
     
     /// 读取应用当前语言，如果localizedLanguage存在则返回，否则返回systemLanguage
-    public static var fw_currentLanguage: String {
+    public static var fw_currentLanguage: String? {
         return fw_localizedLanguage ?? fw_systemLanguage
     }
 
-    /// 读取当前系统语言，兼容应用设置且不含区域，示例：zh-Hans
+    /// 读取当前系统语言，不满足需求时可自定义，兼容应用设置且不含区域，示例：zh-Hans
     ///
     /// 备注：
     /// 1. Bundle.main.preferredLocalizations只包含语言信息，只返回App支持的语言，示例：zh-Hans；注意localizedLanguage重置为nil后需下次启动才能获取到当前系统语言
     /// 2. Locale.preferredLanguages包含语言和区域信息，可能返回App不支持的语言，示例：zh-Hans-CN；注意localizedLanguage重置为nil后无需下次启动即可获取到当前系统语言
-    public static var fw_systemLanguage: String {
-        return Bundle.main.preferredLocalizations.first ?? ""
+    public static var fw_systemLanguage: String? {
+        get {
+            if let language = _fw_systemLanguage {
+                return language
+            }
+            return Bundle.main.preferredLocalizations.first
+        }
+        set {
+            _fw_systemLanguage = newValue
+        }
     }
+    private static var _fw_systemLanguage: String?
 
     /// 读取或设置自定义本地化语言，未自定义时为空。(语言值对应本地化文件存在才会立即生效，如zh-Hans|en)，为空时清空自定义，会触发通知。默认只处理mainBundle语言，如果需要处理三方SDK和系统组件语言，详见Bundle分类
     public static var fw_localizedLanguage: String? {
