77f0e6c45d3721a039b32fcb6ac53f531bf6c59a
wuyong <lingshizhuangzi@gmail.com>
2024-04-26 10:38:35 +0800

    test: 新增SwiftUI测试Target

---
+++ Example/FWFramework-SwiftUI/FWFramework_SwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  FWFramework_SwiftUIApp.swift
+//  FWFramework-SwiftUI
+//
+//  Created by wuyong on 2024/4/26.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct FWFramework_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
