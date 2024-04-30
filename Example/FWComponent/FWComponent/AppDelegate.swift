7b789d1f95d60112512b7e5f2391493a571efc4f
wuyong <lingshizhuangzi@gmail.com>
2024-04-30 13:29:58 +0800

    feat: 新增FWComponent测试项目

--- Example/FWComponent/FWComponent/AppDelegate.swift
+++ Example/FWComponent/FWComponent/AppDelegate.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import FWFramework
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Autoloader.autoload()
+        Mediator.setupAllModules()
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- Example/FWComponent/FWComponent/ViewController.swift
+++ Example/FWComponent/FWComponent/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import FWFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "FWFramework"
     }
-
 
 }
 
