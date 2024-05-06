22ea3a43c52f032fa4aa82a88ec0de83ecc1a5ba
wuyong <lingshizhuangzi@gmail.com>
2024-05-06 11:44:25 +0800

    perf: 优化代码依赖关系

--- Sources/FWFramework/Module/App/AppDelegate.swift
+++ Sources/FWFramework/Module/App/AppDelegate.swift
@@ -1,5 +1,5 @@
 //
-//  Responder.swift
+//  AppDelegate.swift
 //  FWFramework
 //
 //  Created by wuyong on 2022/8/23.
@@ -7,7 +7,6 @@
 
 import UIKit
 
-// MARK: - AppResponder
 /// AppDelegate基类
 open class AppResponder: UIResponder, UIApplicationDelegate {
     
@@ -223,40 +222,5 @@ open class AppResponder: UIResponder, UIApplicationDelegate {
         return false
          */
     }
-    
-}
-
-// MARK: - SceneResponder
-/// SceneDelegate基类
-open class SceneResponder: UIResponder, UIWindowSceneDelegate {
-    
-    /// 场景主window
-    open var window: UIWindow?
-    
-    /// 初始化根控制器，子类重写
-    open func setupController() {
-        /*
-        window?.rootViewController = TabBarController()
-         */
-    }
-    
-    // MARK: - UIWindowSceneDelegate
-    open func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        if let windowScene = scene as? UIWindowScene {
-            window = UIWindow(windowScene: windowScene)
-            window?.makeKeyAndVisible()
-            setupController()
-        }
-    }
-    
-    open func sceneDidDisconnect(_ scene: UIScene) {}
-    
-    open func sceneDidBecomeActive(_ scene: UIScene) {}
-    
-    open func sceneWillResignActive(_ scene: UIScene) {}
-    
-    open func sceneWillEnterForeground(_ scene: UIScene) {}
-    
-    open func sceneDidEnterBackground(_ scene: UIScene) {}
     
 }
