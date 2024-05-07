d3328289e44608bbe2d81a4b9dccbc6e828600f8
wuyong <lingshizhuangzi@gmail.com>
2024-05-07 17:05:16 +0800

    feat: 重构Wrapper相关实现

--- Sources/FWFramework/Toolkit/BarStyle.swift
+++ Sources/FWFramework/Toolkit/BarStyle.swift
@@ -71,65 +71,187 @@ extension Wrapper where Base: UIViewController {
 
     /// 状态栏是否隐藏，默认prefersStatusBarHidden，设置后才会生效
     public var statusBarHidden: Bool {
-        get { return base.fw_statusBarHidden }
-        set { base.fw_statusBarHidden = newValue }
+        get {
+            if let hidden = propertyNumber(forName: "statusBarHidden") {
+                return hidden.boolValue
+            }
+            return base.prefersStatusBarHidden
+        }
+        set {
+            setPropertyNumber(NSNumber(value: newValue), forName: "statusBarHidden")
+            base.setNeedsStatusBarAppearanceUpdate()
+        }
     }
 
     /// 当前导航栏设置，优先级高于style，设置后会在viewWillAppear:自动应用生效
     public var navigationBarAppearance: NavigationBarAppearance? {
-        get { return base.fw_navigationBarAppearance }
-        set { base.fw_navigationBarAppearance = newValue }
+        get {
+            return property(forName: "navigationBarAppearance") as? NavigationBarAppearance
+        }
+        set {
+            setProperty(newValue, forName: "navigationBarAppearance")
+            if base.isViewLoaded && base.view.window != nil {
+                updateNavigationBarStyle(false, isAppeared: true)
+            }
+        }
     }
 
     /// 当前导航栏样式，默认default，设置后才会在viewWillAppear:自动应用生效
     public var navigationBarStyle: NavigationBarStyle {
-        get { return base.fw_navigationBarStyle }
-        set { base.fw_navigationBarStyle = newValue }
+        get {
+            if let style = propertyNumber(forName: "navigationBarStyle") {
+                return .init(rawValue: style.intValue)
+            }
+            return .default
+        }
+        set {
+            setPropertyNumber(NSNumber(value: newValue.rawValue), forName: "navigationBarStyle")
+            if base.isViewLoaded && base.view.window != nil {
+                updateNavigationBarStyle(false, isAppeared: true)
+            }
+        }
     }
 
     /// 导航栏是否隐藏，默认isNavigationBarHidden，设置后才会在viewWillAppear:自动应用生效
     public var navigationBarHidden: Bool {
-        get { return base.fw_navigationBarHidden }
-        set { base.fw_navigationBarHidden = newValue }
+        get {
+            if let hidden = propertyNumber(forName: "navigationBarHidden") {
+                return hidden.boolValue
+            }
+            return base.navigationController?.isNavigationBarHidden ?? true
+        }
+        set {
+            setNavigationBarHidden(newValue, animated: false)
+            // 直接设置navigtionBar.isHidden不会影响右滑关闭手势
+            // base.navigationController?.navigationBar.isHidden = true
+        }
     }
 
     /// 动态隐藏导航栏，如果当前已经viewWillAppear:时立即执行
     public func setNavigationBarHidden(_ hidden: Bool, animated: Bool) {
-        base.fw_setNavigationBarHidden(hidden, animated: animated)
+        setPropertyNumber(NSNumber(value: hidden), forName: "navigationBarHidden")
+        if base.isViewLoaded && base.view.window != nil {
+            updateNavigationBarStyle(false, isAppeared: true)
+        }
     }
     
     /// 是否允许修改导航栏样式，默认未设置时child控制器不能修改
     public var allowsBarAppearance: Bool {
-        get { return base.fw_allowsBarAppearance }
-        set { base.fw_allowsBarAppearance = newValue }
+        get {
+            if let number = propertyNumber(forName: "allowsBarAppearance") {
+                return number.boolValue
+            }
+            return !isChild
+        }
+        set {
+            setPropertyNumber(NSNumber(value: newValue), forName: "allowsBarAppearance")
+        }
+    }
+    
+    private var currentNavigationBarAppearance: NavigationBarAppearance? {
+        // 1. 检查VC是否自定义appearance
+        if let appearance = navigationBarAppearance {
+            return appearance
+        }
+        // 2. 检查VC是否自定义style
+        if let style = propertyNumber(forName: "navigationBarStyle") {
+            return NavigationBarAppearance.appearance(for: .init(rawValue: style.intValue))
+        }
+        // 3. 检查NAV是否自定义appearance
+        if let appearance = base.navigationController?.fw.navigationBarAppearance {
+            return appearance
+        }
+        // 4. 检查NAV是否自定义style
+        if let style = base.navigationController?.fw.propertyNumber(forName: "navigationBarStyle") {
+            return NavigationBarAppearance.appearance(for: .init(rawValue: style.intValue))
+        }
+        return nil
+    }
+    
+    fileprivate func updateNavigationBarStyle(_ animated: Bool, isAppeared: Bool) {
+        // 含有导航栏且不是导航栏控制器，如果是child控制器且允许修改时才处理
+        guard let navigationController = base.navigationController,
+              !(base is UINavigationController) else { return }
+        if !allowsBarAppearance { return }
+        
+        // navigationBarHidden设置即生效，动态切换导航栏不突兀，一般在viewWillAppear:中调用
+        if let hidden = propertyNumber(forName: "navigationBarHidden"),
+           navigationController.isNavigationBarHidden != hidden.boolValue {
+            navigationController.setNavigationBarHidden(hidden.boolValue, animated: animated)
+        }
+        
+        // 获取当前用于显示的appearance，未设置时不处理
+        guard let appearance = currentNavigationBarAppearance else { return }
+        
+        // 配合导航栏appearance初始化返回按钮或左侧按钮
+        if appearance.backImage != nil {
+            base.navigationItem.backBarButtonItem = UIBarButtonItem(image: UIImage(), style: .plain, target: nil, action: nil)
+        }
+        if appearance.leftBackImage != nil && property(forName: #function) == nil {
+            setPropertyBool(true, forName: #function)
+            if navigationController.children.count > 1 && base.navigationItem.leftBarButtonItem == nil {
+                leftBarItem = appearance.leftBackImage
+            }
+        }
+        
+        // 应用当前导航栏appearance
+        navigationController.navigationBar.fw.applyBarAppearance(appearance)
+        
+        // 标记转场导航栏样式需要刷新
+        if isAppeared {
+            NavigationBarAppearance.appearanceChanged?(base)
+        }
     }
 
     /// 标签栏是否隐藏，默认为true，立即生效。如果tabBar一直存在，则用tabBar包裹navBar；如果tabBar只存在主界面，则用navBar包裹tabBar
     public var tabBarHidden: Bool {
-        get { return base.fw_tabBarHidden }
-        set { base.fw_tabBarHidden = newValue }
+        get {
+            return base.tabBarController?.tabBar.isHidden ?? true
+        }
+        set {
+            base.tabBarController?.tabBar.isHidden = newValue
+        }
     }
     
     /// 工具栏是否隐藏，默认为true。需设置toolbarItems，立即生效
     public var toolBarHidden: Bool {
-        get { return base.fw_toolBarHidden }
-        set { base.fw_toolBarHidden = newValue }
+        get {
+            return base.navigationController?.isToolbarHidden ?? true
+        }
+        set {
+            base.navigationController?.isToolbarHidden = newValue
+        }
     }
     
     /// 动态隐藏工具栏。需设置toolbarItems，立即生效
     public func setToolBarHidden(_ hidden: Bool, animated: Bool) {
-        base.fw_setToolBarHidden(hidden, animated: animated)
+        base.navigationController?.setToolbarHidden(hidden, animated: animated)
     }
 
     /// 设置视图布局Bar延伸类型，None为不延伸(Bar不覆盖视图)，Top|Bottom为顶部|底部延伸，All为全部延伸
     public var extendedLayoutEdge: UIRectEdge {
-        get { return base.fw_extendedLayoutEdge }
-        set { base.fw_extendedLayoutEdge = newValue }
+        get {
+            return base.edgesForExtendedLayout
+        }
+        set {
+            base.edgesForExtendedLayout = newValue
+            base.extendedLayoutIncludesOpaqueBars = true
+        }
     }
     
     /// 自适应Bar延伸类型，兼容顶部和底部栏safeArea布局方式，需在viewDidLoad及之后调用生效。开启兼容模式时仅在iOS14及以下生效
     public func adjustExtendedLayout(compatible: Bool = false) {
-        base.fw_adjustExtendedLayout(compatible: compatible)
+        base.extendedLayoutIncludesOpaqueBars = true
+        // iOS15+系统默认使用safeArea布局时自适应标签栏，开启兼容模式时仅处理iOS14及以下
+        var shouldAdjust = true
+        if compatible, #available(iOS 15.0, *) {
+            shouldAdjust = false
+        }
+        if shouldAdjust, !base.hidesBottomBarWhenPushed,
+           base.tabBarController != nil, isHead {
+            // 注意移除bottom后无法实现标签栏半透明效果，可换成bottomBarHeight布局方式等
+            base.edgesForExtendedLayout.remove(.bottom)
+        }
     }
 }
 
@@ -197,195 +319,6 @@ open class NavigationBarAppearance: NSObject {
     
 }
 
-@_spi(FW) extension UIViewController {
-
-    /// 状态栏是否隐藏，默认prefersStatusBarHidden，设置后才会生效
-    public var fw_statusBarHidden: Bool {
-        get {
-            if let hidden = fw.propertyNumber(forName: "fw_statusBarHidden") {
-                return hidden.boolValue
-            }
-            return self.prefersStatusBarHidden
-        }
-        set {
-            fw.setPropertyNumber(NSNumber(value: newValue), forName: "fw_statusBarHidden")
-            self.setNeedsStatusBarAppearanceUpdate()
-        }
-    }
-
-    /// 当前导航栏设置，优先级高于style，设置后会在viewWillAppear:自动应用生效
-    public var fw_navigationBarAppearance: NavigationBarAppearance? {
-        get {
-            return fw.property(forName: "fw_navigationBarAppearance") as? NavigationBarAppearance
-        }
-        set {
-            fw.setProperty(newValue, forName: "fw_navigationBarAppearance")
-            if self.isViewLoaded && self.view.window != nil {
-                self.fw_updateNavigationBarStyle(false, isAppeared: true)
-            }
-        }
-    }
-
-    /// 当前导航栏样式，默认default，设置后才会在viewWillAppear:自动应用生效
-    public var fw_navigationBarStyle: NavigationBarStyle {
-        get {
-            if let style = fw.propertyNumber(forName: "fw_navigationBarStyle") {
-                return .init(rawValue: style.intValue)
-            }
-            return .default
-        }
-        set {
-            fw.setPropertyNumber(NSNumber(value: newValue.rawValue), forName: "fw_navigationBarStyle")
-            if self.isViewLoaded && self.view.window != nil {
-                self.fw_updateNavigationBarStyle(false, isAppeared: true)
-            }
-        }
-    }
-
-    /// 导航栏是否隐藏，默认isNavigationBarHidden，设置后才会在viewWillAppear:自动应用生效
-    public var fw_navigationBarHidden: Bool {
-        get {
-            if let hidden = fw.propertyNumber(forName: "navigationBarHidden") {
-                return hidden.boolValue
-            }
-            return navigationController?.isNavigationBarHidden ?? true
-        }
-        set {
-            self.fw_setNavigationBarHidden(newValue, animated: false)
-            // 直接设置navigtionBar.isHidden不会影响右滑关闭手势
-            // self.navigationController?.navigationBar.isHidden = true
-        }
-    }
-
-    /// 动态隐藏导航栏，如果当前已经viewWillAppear:时立即执行
-    public func fw_setNavigationBarHidden(_ hidden: Bool, animated: Bool) {
-        fw.setPropertyNumber(NSNumber(value: hidden), forName: "navigationBarHidden")
-        if self.isViewLoaded && self.view.window != nil {
-            self.fw_updateNavigationBarStyle(false, isAppeared: true)
-        }
-    }
-    
-    /// 是否允许修改导航栏样式，默认未设置时child控制器不能修改
-    public var fw_allowsBarAppearance: Bool {
-        get {
-            if let number = fw.propertyNumber(forName: "fw_allowsBarAppearance") {
-                return number.boolValue
-            }
-            return !fw_isChild
-        }
-        set {
-            fw.setPropertyNumber(NSNumber(value: newValue), forName: "fw_allowsBarAppearance")
-        }
-    }
-    
-    private var fw_currentNavigationBarAppearance: NavigationBarAppearance? {
-        // 1. 检查VC是否自定义appearance
-        if let appearance = self.fw_navigationBarAppearance {
-            return appearance
-        }
-        // 2. 检查VC是否自定义style
-        if let style = fw.propertyNumber(forName: "fw_navigationBarStyle") {
-            return NavigationBarAppearance.appearance(for: .init(rawValue: style.intValue))
-        }
-        // 3. 检查NAV是否自定义appearance
-        if let appearance = self.navigationController?.fw_navigationBarAppearance {
-            return appearance
-        }
-        // 4. 检查NAV是否自定义style
-        if let style = self.navigationController?.fw.propertyNumber(forName: "fw_navigationBarStyle") {
-            return NavigationBarAppearance.appearance(for: .init(rawValue: style.intValue))
-        }
-        return nil
-    }
-    
-    fileprivate func fw_updateNavigationBarStyle(_ animated: Bool, isAppeared: Bool) {
-        // 含有导航栏且不是导航栏控制器，如果是child控制器且允许修改时才处理
-        guard let navigationController = self.navigationController,
-              !(self is UINavigationController) else { return }
-        if !fw_allowsBarAppearance { return }
-        
-        // fw_navigationBarHidden设置即生效，动态切换导航栏不突兀，一般在viewWillAppear:中调用
-        if let hidden = fw.propertyNumber(forName: "navigationBarHidden"),
-           navigationController.isNavigationBarHidden != hidden.boolValue {
-            navigationController.setNavigationBarHidden(hidden.boolValue, animated: animated)
-        }
-        
-        // 获取当前用于显示的appearance，未设置时不处理
-        guard let appearance = self.fw_currentNavigationBarAppearance else { return }
-        
-        // 配合导航栏appearance初始化返回按钮或左侧按钮
-        if appearance.backImage != nil {
-            self.navigationItem.backBarButtonItem = UIBarButtonItem(image: UIImage(), style: .plain, target: nil, action: nil)
-        }
-        if appearance.leftBackImage != nil && fw.property(forName: "fw_updateNavigationBarStyle") == nil {
-            fw.setPropertyBool(true, forName: "fw_updateNavigationBarStyle")
-            if navigationController.children.count > 1 && self.navigationItem.leftBarButtonItem == nil {
-                self.fw_leftBarItem = appearance.leftBackImage
-            }
-        }
-        
-        // 应用当前导航栏appearance
-        navigationController.navigationBar.fw.applyBarAppearance(appearance)
-        
-        // 标记转场导航栏样式需要刷新
-        if isAppeared {
-            NavigationBarAppearance.appearanceChanged?(self)
-        }
-    }
-
-    /// 标签栏是否隐藏，默认为true，立即生效。如果tabBar一直存在，则用tabBar包裹navBar；如果tabBar只存在主界面，则用navBar包裹tabBar
-    public var fw_tabBarHidden: Bool {
-        get {
-            return self.tabBarController?.tabBar.isHidden ?? true
-        }
-        set {
-            self.tabBarController?.tabBar.isHidden = newValue
-        }
-    }
-    
-    /// 工具栏是否隐藏，默认为true。需设置toolbarItems，立即生效
-    public var fw_toolBarHidden: Bool {
-        get {
-            return self.navigationController?.isToolbarHidden ?? true
-        }
-        set {
-            self.navigationController?.isToolbarHidden = newValue
-        }
-    }
-    
-    /// 动态隐藏工具栏。需设置toolbarItems，立即生效
-    public func fw_setToolBarHidden(_ hidden: Bool, animated: Bool) {
-        self.navigationController?.setToolbarHidden(hidden, animated: animated)
-    }
-
-    /// 设置视图布局Bar延伸类型，None为不延伸(Bar不覆盖视图)，Top|Bottom为顶部|底部延伸，All为全部延伸
-    public var fw_extendedLayoutEdge: UIRectEdge {
-        get {
-            return self.edgesForExtendedLayout
-        }
-        set {
-            self.edgesForExtendedLayout = newValue
-            self.extendedLayoutIncludesOpaqueBars = true
-        }
-    }
-    
-    /// 自适应Bar延伸类型，兼容顶部和底部栏safeArea布局方式，需在viewDidLoad及之后调用生效。开启兼容模式时仅在iOS14及以下生效
-    public func fw_adjustExtendedLayout(compatible: Bool = false) {
-        self.extendedLayoutIncludesOpaqueBars = true
-        // iOS15+系统默认使用safeArea布局时自适应标签栏，开启兼容模式时仅处理iOS14及以下
-        var shouldAdjust = true
-        if compatible, #available(iOS 15.0, *) {
-            shouldAdjust = false
-        }
-        if shouldAdjust, !self.hidesBottomBarWhenPushed,
-           self.tabBarController != nil, fw_isHead {
-            // 注意移除bottom后无法实现标签栏半透明效果，可换成bottomBarHeight布局方式等
-            self.edgesForExtendedLayout.remove(.bottom)
-        }
-    }
-    
-}
-
 // MARK: - FrameworkAutoloader+BarStyle
 extension FrameworkAutoloader {
     
@@ -427,7 +360,7 @@ extension FrameworkAutoloader {
             swizzleSignature: (@convention(block) (UIViewController, Bool) -> Void).self
         ) { store in { selfObject, animated in
             store.original(selfObject, store.selector, animated)
-            selfObject.fw_updateNavigationBarStyle(animated, isAppeared: false)
+            selfObject.fw.updateNavigationBarStyle(animated, isAppeared: false)
         }}
     }
     
