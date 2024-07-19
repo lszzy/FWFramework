8f621f28e341e1de2e028f22fac9adad598b9627
wuyong <lingshizhuangzi@gmail.com>
2024-07-19 16:09:51 +0800

    chore: 兼容Swift6配置

---
+++ Sources/FWPlugin/Macros/Package@Swift-6.0.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:6.0
+
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "FWPluginMacros",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "FWPluginMacros",
+            targets: ["FWPluginMacros"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+    ],
+    targets: [
+        .macro(
+            name: "FWMacroMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ],
+            path: "FWMacroMacros"
+        ),
+        .target(
+            name: "FWPluginMacros",
+            dependencies: ["FWMacroMacros"],
+            path: "FWPluginMacros"
+        ),
+    ]
+)
