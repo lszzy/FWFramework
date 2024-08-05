7948f44701a0323be9dd61af9a3a411f9bd98497
wuyong <lingshizhuangzi@gmail.com>
2024-08-05 10:30:59 +0800

    test: 优化测试用例

--- Example/FWFramework/Classes/Module/Test/Toolkit/TestCollectionController.swift
+++ Example/FWFramework/Classes/Module/Test/Toolkit/TestCollectionController.swift
@@ -334,15 +334,15 @@ class TestCollectionController: UIViewController, CollectionViewControllerProtoc
         
         app.showImagePreview(imageURLs: pictureUrls, imageInfos: nil, currentIndex: indexPath.row) { [weak self] index in
             guard let self = self else { return nil }
-            if let cell = self.collectionView.cellForItem(at: IndexPath(row: index, section: 0)) as? TestCollectionDynamicLayoutCell {
+            if let cell = self.collectionView.cellForItem(at: IndexPath(row: index, section: 1)) as? TestCollectionDynamicLayoutCell {
                 return cell.myImageView
             } else {
-                let cellFrame = self.collectionView.app.layoutCellFrame(for: IndexPath(row: index, section: 0))
+                let cellFrame = self.collectionView.app.layoutCellFrame(for: IndexPath(row: index, section: 1))
                 let windowFrame = self.collectionView.convert(cellFrame, to: self.view.window)
                 return NSValue(cgRect: windowFrame)
             }
         } placeholderImage: { [weak self] index in
-            let cell = self?.collectionView.cellForItem(at: IndexPath(row: index, section: 0)) as? TestCollectionDynamicLayoutCell
+            let cell = self?.collectionView.cellForItem(at: IndexPath(row: index, section: 1)) as? TestCollectionDynamicLayoutCell
             return cell?.myImageView.image
         }
     }
