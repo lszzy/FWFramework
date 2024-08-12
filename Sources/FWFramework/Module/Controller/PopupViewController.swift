082e74191081b85827b3d4850110683d20d166e1
wuyong <lingshizhuangzi@gmail.com>
2024-08-12 21:24:37 +0800

    feat: 移除completionSpeed配置属性

--- Sources/FWFramework/Module/Controller/PopupViewController.swift
+++ Sources/FWFramework/Module/Controller/PopupViewController.swift
@@ -24,10 +24,8 @@ open class PopupConfiguration {
     open var centerAnimation = false
     /// 中心弹窗时是否执行alert动画，默认true，否则fade动画，仅centerAnimation生效
     open var alertAnimation = true
-    /// 动画持续时间，必须大于0，默认同completionSpeed为0.35秒
+    /// 动画持续时间，必须大于0，默认0.35秒
     open var animationDuration: TimeInterval = 0.35
-    /// 动画完成速度，默认0.35
-    open var completionSpeed: CGFloat = 0.35
     /// 是否启用交互pan手势进行pop|dismiss，默认false，仅animationEdge生效
     open var interactEnabled = false
     /// 是否启用screenEdge交互手势进行pop|dismiss，默认false，仅animationEdge为left|right时生效
@@ -154,7 +152,6 @@ internal extension ViewControllerManager {
         }
         
         modalTransition.transitionDuration = popupConfiguration.animationDuration
-        modalTransition.completionSpeed = popupConfiguration.completionSpeed
         modalTransition.presentationBlock = { [weak popupController] presented, presenting in
             let presentation = PresentationController(presentedViewController: presented, presenting: presenting)
             presentation.showDimming = popupController?.popupConfiguration.showDimming ?? true
