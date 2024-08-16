b13b6d4a6849ccc4620907a6f6d8ee60f0ca745c
wuyong <lingshizhuangzi@gmail.com>
2024-08-17 01:08:27 +0800

    feat: 新增AttributedText富文本组件

--- Example/FWFramework/Classes/Module/Test/Module/TestSwiftUIController.swift
+++ Example/FWFramework/Classes/Module/Test/Module/TestSwiftUIController.swift
@@ -128,6 +128,8 @@ struct TestSwiftUIContent: View {
     @State var topSize: CGSize = .zero
     @State var contentOffset: CGPoint = .zero
     @State var shouldRefresh: Bool = false
+    @State var attributedColor = UIColor.app.randomColor
+    @State var attributedChanged: Bool = false
     
     @State var moreItems: [String] = []
     
@@ -177,6 +179,46 @@ struct TestSwiftUIContent: View {
                         .hostingViewInitialize { hostingView in
                             hostingView.backgroundColor = UIColor.app.randomColor
                         }
+                        
+                        AttributedText(NSMutableAttributedString({
+                            "我是富文本，"
+                            
+                            NSAttributedString(string: "我可以变色，", attributes: [
+                                .foregroundColor: attributedColor,
+                                .init("URL"): 1,
+                            ])
+                            
+                            if attributedChanged {
+                                "我是动态内容，"
+                            }
+                            
+                            NSAttributedString(string: "我可以点击，", attributes: [
+                                .foregroundColor: UIColor.blue,
+                                .init("URL"): 2,
+                            ])
+                            
+                            "我可以跳转"
+                            
+                            NSAttributedString(string: "https://www.baidu.com", attributes: [
+                                .foregroundColor: UIColor.blue,
+                                .link: APP.safeURL("https://www.baidu.com"),
+                            ])
+                        })) { link in
+                            if let url = link as? URL {
+                                Router.openURL(url)
+                            } else if let value = link as? Int {
+                                if value == 1 {
+                                    attributedColor = UIColor.app.randomColor
+                                } else if value == 2 {
+                                    attributedChanged.toggle()
+                                }
+                            }
+                        }
+                        .font(APP.font(16))
+                        .foregroundColor(Color.black)
+                        .lineHeight(24)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 16)
                     }
                     .padding(.top, 16)
                     .captureSize(in: $topSize)
---
+++ Sources/FWSwiftUI/Plugin/AttributedText.swift
@@ -0,0 +1,185 @@
+//
+//  AttributedText.swift
+//  FWFramework
+//
+//  Created by wuyong on 2024/8/16.
+//
+
+#if canImport(SwiftUI)
+import UIKit
+import SwiftUI
+#if FWMacroSPM
+@_spi(FW) import FWFramework
+#endif
+
+// MARK: - AttributedText
+/// 富文本，兼容NSAttributedString
+public struct AttributedText: View {
+    
+    // MARK: - Accessor
+    @State private var textSize: CGSize?
+    private var viewModel = ViewModel()
+    
+    private let attributedText: NSAttributedString
+    private let clickedOnLink: ((Any?) -> Void)?
+    
+    // MARK: - Lifecycle
+    public init(_ attributedText: AttributedStringParameter, clickedOnLink: ((Any?) -> Void)? = nil) {
+        self.attributedText = attributedText.attributedStringValue
+        self.clickedOnLink = clickedOnLink
+    }
+    
+    // MARK: - Public
+    public func font(_ font: UIFont) -> Self {
+        configure({ $0.viewModel.font = font })
+    }
+
+    public func foregroundColor(_ foregroundColor: UIColor) -> Self {
+        configure({ $0.viewModel.foregroundColor = foregroundColor })
+    }
+
+    @_disfavoredOverload
+    public func foregroundColor(_ foregroundColor: Color?) -> Self {
+        configure({ $0.viewModel.foregroundColor = foregroundColor?.toUIColor() })
+    }
+    
+    public func lineHeight(_ lineHeight: CGFloat) -> Self {
+        configure({ $0.viewModel.lineHeight = lineHeight })
+    }
+    
+    public func baselineAdjustment(_ baselineAdjustment: UIBaselineAdjustment) -> Self {
+        configure({ $0.viewModel.baselineAdjustment = baselineAdjustment })
+    }
+    
+    public func adjustsFontSizeToFitWidth(_ adjustsFontSizeToFitWidth: Bool) -> Self {
+        configure({ $0.viewModel.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth })
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            ViewRepresentable(
+                attributedText: attributedText,
+                maxLayoutWidth: geometry.size.width - geometry.safeAreaInsets.leading - geometry.safeAreaInsets.trailing,
+                viewModel: viewModel,
+                clickedOnLink: clickedOnLink
+            )
+        }
+        .frame(idealWidth: textSize?.width, idealHeight: textSize?.height)
+        .fixedSize(horizontal: false, vertical: true)
+        .onReceive(viewModel.$textSize) { size in
+            textSize = size
+        }
+    }
+    
+}
+
+extension AttributedText {
+    
+    class ViewModel {
+        @Published var textSize: CGSize?
+        var font: UIFont?
+        var foregroundColor: UIColor?
+        var lineHeight: CGFloat?
+        var baselineAdjustment: UIBaselineAdjustment?
+        var adjustsFontSizeToFitWidth: Bool?
+    }
+    
+    struct ViewRepresentable: UIViewRepresentable {
+        var attributedText: NSAttributedString
+        var maxLayoutWidth: CGFloat
+        var viewModel: ViewModel
+        var clickedOnLink: ((Any?) -> Void)?
+        
+        // MARK: - UIViewRepresentable
+        typealias UIViewType = ViewLabel
+        
+        func makeUIView(context: Context) -> ViewLabel {
+            ViewLabel()
+        }
+        
+        func updateUIView(_ label: ViewLabel, context: Context) {
+            if let lineHeight = viewModel.lineHeight {
+                label.fw.setFont(viewModel.font, lineHeight: lineHeight)
+            } else if let font = viewModel.font {
+                label.font = font
+            }
+            if let foregroundColor = viewModel.foregroundColor { label.textColor = foregroundColor }
+            if let baselineAdjustment = viewModel.baselineAdjustment { label.baselineAdjustment = baselineAdjustment }
+            if let adjustsFontSizeToFitWidth = viewModel.adjustsFontSizeToFitWidth { label.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth }
+            
+            label.allowsDefaultTighteningForTruncation = context.environment.allowsTightening
+            label.minimumScaleFactor = context.environment.minimumScaleFactor
+            label.numberOfLines = context.environment.lineLimit ?? 0
+            label.textAlignment = textAlignment(from: context.environment.multilineTextAlignment)
+            label.lineBreakMode = lineBreakMode(from: context.environment.truncationMode)
+            label.isUserInteractionEnabled = context.environment.isEnabled
+            label.clickedOnLink = clickedOnLink
+            
+            label.attributedText = attributedText
+            label.maxLayoutWidth = maxLayoutWidth
+            viewModel.textSize = label.intrinsicContentSize
+        }
+        
+        // MARK: - Private
+        func textAlignment(from textAlignment: TextAlignment) -> NSTextAlignment {
+            switch textAlignment {
+            case .leading:
+                return .left
+            case .center:
+                return .center
+            case .trailing:
+                return .right
+            @unknown default:
+                return .left
+            }
+        }
+        
+        func lineBreakMode(from truncationMode: Text.TruncationMode) -> NSLineBreakMode {
+            switch truncationMode {
+            case .head:
+                return .byTruncatingHead
+            case .tail:
+                return .byTruncatingTail
+            case .middle:
+                return .byTruncatingMiddle
+            @unknown default:
+                return .byWordWrapping
+            }
+        }
+    }
+    
+    class ViewLabel: UILabel {
+        var maxLayoutWidth: CGFloat = 0 {
+            didSet {
+                guard maxLayoutWidth != oldValue else { return }
+                invalidateIntrinsicContentSize()
+            }
+        }
+        
+        var clickedOnLink: ((Any?) -> Void)?
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            backgroundColor = .clear
+            fw.addLinkGesture { [weak self] link in
+                self?.clickedOnLink?(link)
+            }
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            guard maxLayoutWidth > 0 else {
+                return super.intrinsicContentSize
+            }
+            
+            return sizeThatFits(CGSize(width: maxLayoutWidth, height: .greatestFiniteMagnitude))
+        }
+    }
+    
+}
+
+#endif
