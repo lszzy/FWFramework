666cc8588c5f286ab6b41cd892188f7bec27b283
wuyong <lingshizhuangzi@gmail.com>
2024-08-29 22:03:23 +0800

    feat: 新增EventViewProtocol通用事件视图协议

--- Example/FWFramework/Classes/Module/Test/Module/TestSwiftController.swift
+++ Example/FWFramework/Classes/Module/Test/Module/TestSwiftController.swift
@@ -222,7 +222,19 @@ class SwiftTestRequestView: UIView, EventViewProtocol {
     private var emptyView: PlaceholderView? {
         app.showingEmptyView as? PlaceholderView
     }
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        didInitialize()
+        setupSubviews()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func setupSubviews() {
         app.showEmptyView(text: nil, action: "刷新") { [weak self] _ in
             self?.triggerEvent(EventName.refresh)
