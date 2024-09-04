bae22a9a301e31ec1e36849f0e7fd76e8344df86
wuyong <lingshizhuangzi@gmail.com>
2024-09-04 14:19:23 +0800

    feat: DrawerView新增draggingAreaBlock配置和springAnimation配置

--- Example/FWFramework/Classes/Module/Test/Module/TestDrawerController.swift
+++ Example/FWFramework/Classes/Module/Test/Module/TestDrawerController.swift
@@ -101,6 +101,7 @@ class TestDrawerController: UIViewController, ViewControllerProtocol, UINavigati
             guard let drawerView = self?.bottomView.app.drawerView else { return }
             drawerView.scrollViewFilter = { _ in true }
             drawerView.scrollViewPositions = nil
+            drawerView.draggingAreaBlock = nil
             self?.toggleMenu()
         } customize: { gesture in
             gesture.highlightedAlpha = UIButton.app.highlightedAlpha
@@ -119,6 +120,13 @@ class TestDrawerController: UIViewController, ViewControllerProtocol, UINavigati
                     drawerView.middlePosition
                 ]
             }
+            drawerView.draggingAreaBlock = { position in
+                if position == drawerView.openPosition ||
+                    position == drawerView.middlePosition {
+                    return CGRect(x: 0, y: 0, width: APP.screenWidth, height: 50)
+                }
+                return .zero
+            }
             self?.toggleMenu()
         } customize: { gesture in
             gesture.highlightedAlpha = UIButton.app.highlightedAlpha
@@ -132,6 +140,7 @@ class TestDrawerController: UIViewController, ViewControllerProtocol, UINavigati
             guard let drawerView = self?.bottomView.app.drawerView else { return }
             drawerView.scrollViewFilter = { _ in false }
             drawerView.scrollViewPositions = nil
+            drawerView.draggingAreaBlock = nil
             self?.toggleMenu()
         } customize: { gesture in
             gesture.highlightedAlpha = UIButton.app.highlightedAlpha
@@ -162,7 +171,7 @@ class TestDrawerController: UIViewController, ViewControllerProtocol, UINavigati
             .right,
             positions: [-APP.screenWidth / 2.0, 0],
             kickbackHeight: 25
-        )
+        ).springAnimation = false
     }
 
     func toggleMenu() {
