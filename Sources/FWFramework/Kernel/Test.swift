95392f2039d49915cd1690cf9c5cfd95c8b99a88
wuyong <lingshizhuangzi@gmail.com>
2024-09-09 16:33:01 +0800

    feat: 重构Test，支持自定义testSuite并手动调用

--- Sources/FWFramework/Kernel/Test.swift
+++ Sources/FWFramework/Kernel/Test.swift
@@ -10,100 +10,128 @@ import Foundation
 // 调试环境开启，正式环境关闭
 #if DEBUG
 
+// MARK: - TestSuite
+/// 可扩展测试套件，默认automatic
+public struct TestSuite: RawRepresentable, Equatable, Hashable {
+    public typealias RawValue = String
+
+    /// 自动测试套件，启动时自动调用
+    public static let automatic: TestSuite = .init("automatic")
+    /// 手动测试套件，需手动调用执行
+    public static let manual: TestSuite = .init("manual")
+
+    public var rawValue: String
+
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+
+    public init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
 // MARK: - TestCase
 /// 单元测试用例基类，所有单元测试用例必须继承。注意测试方法需标记objc，让OC可以访问
 ///
-/// 调试模式下自动执行，按模块单元测试命名格式：TestCase_module_name
+/// 测试类命名建议模块+单元格式：TestCase_module_name，测试方法命名规则如下：
+/// 同步测试：test开头无参方法，无需调用assertFinished
+/// 异步测试：testAsync开头无参方法，必须调用assertFinished
 open class TestCase: NSObject {
-    
+    // MARK: - Accessor
     fileprivate var assertError: NSError?
-    fileprivate var isAssertAsync = false
-    private var assertSemaphore: DispatchSemaphore?
-    
+    fileprivate var assertCompletion: (() -> Void)?
+
+    // MARK: - Lifecycle
     /// 初始化方法
-    required public override init() {
+    override public required init() {
         super.init()
     }
-    
+
+    // MARK: - Public
+    /// 所属测试套件，默认automatic
+    open class func testSuite() -> TestSuite {
+        .automatic
+    }
+
     /// 测试初始化，每次执行测试方法开始都会调用
     open func setUp() {}
-    
+
     /// 测试收尾，每次执行测试方法结束都会调用
     open func tearDown() {}
-    
-    /// 执行同步断言
+
+    /// 执行断言，异步断言完成时必须调用assertFinished
     ///
     /// - Parameters:
-    ///   - value: 断言表达式
+    ///   - value: 断言值
+    ///   - expression: 断言表达式
     ///   - file: 文件名，默认传参
     ///   - line: 行数，默认传参
-    open func assertTrue(_ value: Bool, _ expression: String = "", file: String = #file, line: Int = #line) {
-        guard !value else { return }
-        
+    @discardableResult
+    open func assertTrue(_ value: Bool, _ expression: String = "", file: String = #file, line: Int = #line) -> Bool {
+        guard !value else { return true }
+
         assertError = NSError(domain: "FWTest", code: 0, userInfo: [
             "expression": expression,
             "file": !file.isEmpty ? (file as NSString).lastPathComponent : "",
             "line": "\(line)"
         ])
-        isAssertAsync = false
+        return false
     }
-    
-    /// 异步断言开始
-    open func assertBegin() {
-        assertSemaphore = DispatchSemaphore(value: 0)
+
+    /// 异步断言结束，异步断言完成时必须调用assertFinished
+    open func assertFinished() {
+        assertCompletion?()
     }
-    
-    /// 执行异步断言并退出，一个异步周期仅支持一次异步断言
-    ///
-    /// - Parameters:
-    ///   - value: 断言表达式
-    ///   - file: 文件名，默认传参
-    ///   - line: 行数，默认传参
-    open func assertAsync(_ value: Bool, _ expression: String = "", file: String = #file, line: Int = #line) {
-        assertTrue(value, expression, file: file, line: line)
-        isAssertAsync = true
-        assertSemaphore?.signal()
-    }
-    
-    /// 异步断言结束
-    open func assertEnd() {
-        assertSemaphore?.wait()
-    }
-    
 }
 
 // MARK: - UnitTest
-/// 单元测试启动器
-fileprivate class UnitTest: CustomDebugStringConvertible {
-    
-    private static let shared = UnitTest()
-    
-    private var testCases: [AnyClass] = []
+/// 单元测试启动器，调试模式启动时自动执行automatic测试套件
+public class UnitTest: CustomDebugStringConvertible {
+    // MARK: - Accessor
+    private var testSuite: TestSuite = .manual
+    private var testCases: [AnyClass]?
     private var testLogs: String = ""
+
+    // MARK: - Lifecycle
+    /// 指定测试套件初始化
+    public init(testSuite: TestSuite) {
+        self.testSuite = testSuite
+    }
     
-    static func runTests() {
-        DispatchQueue.main.async {
-            let unitTest = UnitTest.shared
-            unitTest.testCases.append(contentsOf: unitTest.testSuite())
-            guard unitTest.testCases.count > 0 else { return }
-            
-            let queue = DispatchQueue(label: "site.wuyong.queue.test.async")
-            queue.async {
-                unitTest.run()
-                Logger.debug(group: Logger.fw.moduleName, "%@", unitTest.debugDescription)
-            }
+    /// 指定测试用例初始化
+    public init(testCases: [TestCase.Type]) {
+        self.testCases = testCases
+    }
+
+    // MARK: - Public
+    /// 执行单元测试，完成时回调是否成功
+    public func runTests(completion: ((Bool) -> Void)? = nil) {
+        let testCases = testCases ?? testCases(of: testSuite)
+        guard !testCases.isEmpty else {
+            completion?(true)
+            return
+        }
+
+        let queue = DispatchQueue(label: "site.wuyong.queue.test.\(testSuite.rawValue)")
+        queue.async {
+            let result = self.runTestCases(testCases)
+            completion?(result)
         }
     }
-    
-    private func testSuite() -> [AnyClass] {
+
+    /// 测试结果调试信息
+    public var debugDescription: String { testLogs }
+
+    // MARK: - Private
+    private func testCases(of testSuite: TestSuite) -> [AnyClass] {
         let testCases = NSObject.fw.allSubclasses(TestCase.self)
-            .sorted { obj1, obj2 in
-                return NSStringFromClass(obj1) < NSStringFromClass(obj2)
-            }
+            .filter { ($0 as? TestCase.Type)?.testSuite() == testSuite }
+            .sorted { NSStringFromClass($0) < NSStringFromClass($1) }
         return testCases
     }
-    
-    private func testMethods(_ clazz: AnyClass) -> [String] {
+
+    private func testMethods(of clazz: AnyClass) -> [String] {
         var methodNames: [String] = []
         let selectorNames = NSObject.fw.classMethods(clazz)
         for selectorName in selectorNames {
@@ -113,14 +141,13 @@ fileprivate class UnitTest: CustomDebugStringConvertible {
         }
         return methodNames
     }
-    
-    private func run() {
-        let testCases = self.testCases
+
+    private func runTestCases(_ testCases: [AnyClass]) -> Bool {
         var failedCount: UInt = 0
         var succeedCount: UInt = 0
         var testLog = ""
+        let testSemaphore = DispatchSemaphore(value: 0)
         let beginTime = Date().timeIntervalSince1970
-        self.testCases.removeAll()
 
         for classType in testCases {
             let classTime = Date().timeIntervalSince1970
@@ -130,13 +157,12 @@ fileprivate class UnitTest: CustomDebugStringConvertible {
                 .replacingOccurrences(of: "_", with: ".")
             var formatMethod = ""
             var formatMessage = ""
-            let selectorNames = testMethods(classType)
+            let selectorNames = testMethods(of: classType)
             var testCasePassed = true
-            let totalTestCount: UInt = UInt(selectorNames.count)
+            let totalTestCount = UInt(selectorNames.count)
             var currentTestCount: UInt = 0
             var assertError: NSError?
-            var assertAsync = false
-            
+
             if let testClass = classType as? TestCase.Type, selectorNames.count > 0 {
                 let testCase = testClass.init()
                 for selectorName in selectorNames {
@@ -145,22 +171,30 @@ fileprivate class UnitTest: CustomDebugStringConvertible {
                     let selector = NSSelectorFromString(selectorName)
                     if testCase.responds(to: selector) {
                         testCase.setUp()
-                        testCase.perform(selector)
+                        if selectorName.hasPrefix("testAsync") {
+                            testCase.assertCompletion = {
+                                testSemaphore.signal()
+                            }
+                            testCase.perform(selector)
+                            testSemaphore.wait()
+                        } else {
+                            testCase.assertCompletion = nil
+                            testCase.perform(selector)
+                        }
                         testCase.tearDown()
-                        
+
                         assertError = testCase.assertError
-                        assertAsync = testCase.isAssertAsync
                         if assertError != nil { break }
                     }
                 }
             }
-            
-            if let assertError = assertError {
+
+            if let assertError {
                 let expression = assertError.userInfo["expression"] as? String ?? ""
-                formatMessage = String(format: "- assert%@(%@); (%@ - %@ #%@)", assertAsync ? "Async" : "True", !expression.isEmpty ? expression : "false", formatMethod, assertError.userInfo["file"] as? String ?? "", assertError.userInfo["line"] as? String ?? "")
+                formatMessage = String(format: "- assertTrue(%@); (%@ - %@ #%@)", !expression.isEmpty ? expression : "false", formatMethod, assertError.userInfo["file"] as? String ?? "", assertError.userInfo["line"] as? String ?? "")
                 testCasePassed = false
             }
-            
+
             let time = Date().timeIntervalSince1970 - classTime
             let succeedTestCount = testCasePassed ? currentTestCount : (currentTestCount - 1)
             let classPassRate: Float = totalTestCount > 0 ? (Float(succeedTestCount) / Float(totalTestCount) * 100.0) : 100.0
@@ -173,27 +207,28 @@ fileprivate class UnitTest: CustomDebugStringConvertible {
                 testLog += String(format: "     %@\n", formatMessage)
             }
         }
-        
+
         let totalTime = Date().timeIntervalSince1970 - beginTime
         let totalCount = succeedCount + failedCount
         let passRate: Float = totalCount > 0 ? (Float(succeedCount) / Float(totalCount) * 100.0) : 100.0
-        let totalLog = String(format: "   %@: (%lu/%lu) (%.0f%%) (%.003fs)\n", failedCount < 1 ? "✔️" : "❌", succeedCount, totalCount, passRate, totalTime)
-        self.testLogs = String(format: "\n========== TEST  ==========\n%@%@========== TEST  ==========", testLog, totalCount > 0 ? totalLog : "")
+        let totalLog = String(format: "   %@: (%lu/%lu) (%.0f%%) (%.003fs)\n", failedCount < 1 ? "✅" : "⚠️", succeedCount, totalCount, passRate, totalTime)
+        let suiteName = testSuite != .automatic ? "." + testSuite.rawValue : ""
+        testLogs = String(format: "\n========== TEST%@ ==========\n%@%@========== TEST%@ ==========", suiteName, testLog, totalCount > 0 ? totalLog : "", suiteName)
+        return failedCount < 1
     }
-    
-    var debugDescription: String {
-        return testLogs
-    }
-    
 }
 
 // MARK: - FrameworkAutoloader+Test
 extension FrameworkAutoloader {
-    
     @objc static func loadKernel_Test() {
-        UnitTest.runTests()
+        DispatchQueue.main.async {
+            let unitTest = UnitTest(testSuite: .automatic)
+            unitTest.runTests { _ in
+                guard !unitTest.debugDescription.isEmpty else { return }
+                Logger.debug(group: Logger.fw.moduleName, "%@", unitTest.debugDescription)
+            }
+        }
     }
-    
 }
 
 #endif
