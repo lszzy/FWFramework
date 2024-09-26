9e613b0f70ea6e43006f579fd344b8ab701c53bf
wuyong <lingshizhuangzi@gmail.com>
2024-09-26 22:03:30 +0800

    perf: 减少nonisolated(unsafe)使用

--- Sources/FWFramework/Plugin/Toast/ToastPlugin.swift
+++ Sources/FWFramework/Plugin/Toast/ToastPlugin.swift
@@ -529,7 +529,7 @@ extension ToastPlugin {
 // MARK: - FrameworkAutoloader+ToastPlugin
 extension FrameworkAutoloader {
     @objc static func loadPlugin_ToastPlugin() {
-        RequestContextAccessory.showErrorBlock = { @MainActor @Sendable context, error in
+        RequestConfig.shared.showErrorBlock = { @MainActor @Sendable context, error in
             if let viewController = context as? UIViewController {
                 viewController.fw.showMessage(error: error)
             } else if let view = context as? UIView {
@@ -539,7 +539,7 @@ extension FrameworkAutoloader {
             }
         }
 
-        RequestContextAccessory.showLoadingBlock = { @MainActor @Sendable context in
+        RequestConfig.shared.showLoadingBlock = { @MainActor @Sendable context in
             if let viewController = context as? UIViewController {
                 viewController.fw.showLoading()
             } else if let view = context as? UIView {
@@ -547,7 +547,7 @@ extension FrameworkAutoloader {
             }
         }
 
-        RequestContextAccessory.hideLoadingBlock = { @MainActor @Sendable context in
+        RequestConfig.shared.hideLoadingBlock = { @MainActor @Sendable context in
             if let viewController = context as? UIViewController {
                 viewController.fw.hideLoading()
             } else if let view = context as? UIView {
