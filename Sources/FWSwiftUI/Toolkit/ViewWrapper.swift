68cf29e305d90a31b608585687ac8acb2154ed47
wuyong <lingshizhuangzi@gmail.com>
2024-09-30 11:23:37 +0800

    feat: 标记Sendable，兼容Swift 6

--- Sources/FWSwiftUI/Toolkit/ViewWrapper.swift
+++ Sources/FWSwiftUI/Toolkit/ViewWrapper.swift
@@ -11,34 +11,34 @@ import SwiftUI
 // MARK: - ViewWrapper
 /// 通用UIView包装器
 public struct ViewWrapper<T: UIView>: UIViewRepresentable {
-    var maker: (() -> T)?
-    var updater: ((T) -> Void)?
+    var maker: (@MainActor @Sendable () -> T)?
+    var updater: (@MainActor @Sendable (T) -> Void)?
 
     /// 指定makeUIView闭包初始化
-    public init(_ maker: (() -> T)? = nil) {
+    public init(_ maker: (@MainActor @Sendable () -> T)? = nil) {
         self.maker = maker
     }
 
     /// 指定updateUIView闭包初始化
-    public init(updater: @escaping (T) -> Void) {
+    public init(updater: @escaping @MainActor @Sendable (T) -> Void) {
         self.updater = updater
     }
 
     /// 指定makeUIView闭包和updateUIView闭包初始化
-    public init(_ maker: @escaping () -> T, updater: @escaping (T) -> Void) {
+    public init(_ maker: @escaping @MainActor @Sendable () -> T, updater: @escaping @MainActor @Sendable (T) -> Void) {
         self.maker = maker
         self.updater = updater
     }
 
     /// 设置makeUIView闭包
-    public func maker(_ maker: @escaping () -> T) -> ViewWrapper<T> {
+    public func maker(_ maker: @escaping @MainActor @Sendable () -> T) -> ViewWrapper<T> {
         var result = self
         result.maker = maker
         return result
     }
 
     /// 设置updateUIView闭包
-    public func updater(_ updater: @escaping (T) -> Void) -> ViewWrapper<T> {
+    public func updater(_ updater: @escaping @MainActor @Sendable (T) -> Void) -> ViewWrapper<T> {
         var result = self
         result.updater = updater
         return result
@@ -60,34 +60,34 @@ public struct ViewWrapper<T: UIView>: UIViewRepresentable {
 // MARK: - ViewControllerWrapper
 /// 通用UIViewController包装器
 public struct ViewControllerWrapper<T: UIViewController>: UIViewControllerRepresentable {
-    var maker: (() -> T)?
-    var updater: ((T) -> Void)?
+    var maker: (@MainActor @Sendable () -> T)?
+    var updater: (@MainActor @Sendable (T) -> Void)?
 
     /// 指定makeUIViewController闭包初始化
-    public init(_ maker: (() -> T)? = nil) {
+    public init(_ maker: (@MainActor @Sendable () -> T)? = nil) {
         self.maker = maker
     }
 
     /// 指定updateUIViewController闭包初始化
-    public init(updater: @escaping (T) -> Void) {
+    public init(updater: @escaping @MainActor @Sendable (T) -> Void) {
         self.updater = updater
     }
 
     /// 指定makeUIViewController闭包和updateUIViewController闭包初始化
-    public init(_ maker: @escaping () -> T, updater: @escaping (T) -> Void) {
+    public init(_ maker: @escaping @MainActor @Sendable () -> T, updater: @escaping @MainActor @Sendable (T) -> Void) {
         self.maker = maker
         self.updater = updater
     }
 
     /// 设置makeUIViewController闭包
-    public func maker(_ maker: @escaping () -> T) -> ViewControllerWrapper<T> {
+    public func maker(_ maker: @escaping @MainActor @Sendable () -> T) -> ViewControllerWrapper<T> {
         var result = self
         result.maker = maker
         return result
     }
 
     /// 设置updateUIViewController闭包
-    public func updater(_ updater: @escaping (T) -> Void) -> ViewControllerWrapper<T> {
+    public func updater(_ updater: @escaping @MainActor @Sendable (T) -> Void) -> ViewControllerWrapper<T> {
         var result = self
         result.updater = updater
         return result
