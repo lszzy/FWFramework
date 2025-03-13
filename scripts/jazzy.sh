58d95574084d2dfc19924d42caf785d6f9d31d3c
wuyong <lingshizhuangzi@gmail.com>
2025-03-14 00:57:37 +0800

    docs: 优化文档生成脚本

--- scripts/jazzy.sh
+++ scripts/jazzy.sh
@@ -1,15 +1,33 @@
 #! /bin/bash
 
-framework_name=FWFramework
+# Documentation:
+# This script generates jazzy docs for CocoaPods Framework.
 
+# Usage:
+# jazzy.sh
+# e.g. `bash scripts/jazzy.sh`
+
+# Exit immediately if a command exits with non-zero status
+set -e
+
+# Use the script folder to refer to other scripts.
+FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_PACKAGE_NAME="$FOLDER/package_name.sh"
+
+# Get package name
+PACKAGE_NAME=$("$SCRIPT_PACKAGE_NAME") || { echo "Failed to get package name"; exit 1; }
+LIB_PATH="docs/$PACKAGE_NAME/"
+
+# Create package directory
 rm -rf docs
 mkdir docs
-mkdir "docs/$framework_name"
+mkdir "docs/$PACKAGE_NAME"
 
-lib_path="docs/$framework_name/"
-sourcekitten doc -- -project Example/Pods/Pods.xcodeproj -target $framework_name > "$lib_path/swift.json"
-jazzy --include-spi-declarations --sourcekitten-sourcefile "$lib_path/swift.json"
+# Generate jazzy docs
+sourcekitten doc -- -project Example/Pods/Pods.xcodeproj -target $PACKAGE_NAME > "$LIB_PATH/swift.json"
+jazzy --include-spi-declarations --sourcekitten-sourcefile "$LIB_PATH/swift.json"
 
-rm -rf $lib_path
+# Clean build directory
+rm -rf $LIB_PATH
 rm -rf Example/build/
 cp *.md docs/
